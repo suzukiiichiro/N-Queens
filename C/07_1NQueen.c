34ef8184ffa2008edf688982603be91553d65941
suzukiiichiro <suzuki.iichiro@gmail.com>
2017-05-29 11:14:18 +0900

    add

--- C/07_1NQueen.c
+++ C/07_1NQueen.c
@@ -1,27 +1,93 @@
+/**
+
+  Cで学ぶアルゴリズムとデータ構造  
+  一般社団法人  共同通信社  情報技術局  鈴木  維一郎(suzuki.iichiro@kyodonews.jp)
+ 
+  Java版はこちらを
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Java
+  Lua版
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Lua
+  Bash版
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Bash
+ 
+
+  再帰  Nクイーン問題
+  https://ja.wikipedia.org/wiki/エイト・クイーン
+ 
+  N-Queens問題とは
+     Nクイーン問題とは、「8列×8行のチェスボードに8個のクイーンを、互いに効きが
+     当たらないように並べよ」という８クイーン問題のクイーン(N)を、どこまで大き
+     なNまで解を求めることができるかという問題。
+     クイーンとは、チェスで使われているクイーンを指し、チェス盤の中で、縦、横、
+     斜めにどこまでも進むことができる駒で、日本の将棋でいう「飛車と角」を合わ
+     せた動きとなる。８列×８行で構成される一般的なチェスボードにおける8-Queens
+     問題の解は、解の総数は92個である。比較的単純な問題なので、学部レベルの演
+     習問題として取り上げられることが多い。
+     8-Queens問題程度であれば、人力またはプログラムによる「力まかせ探索」でも
+     解を求めることができるが、Nが大きくなると解が一気に爆発し、実用的な時間で
+     は解けなくなる。
+     現在すべての解が判明しているものは、2004年に電気通信大学で264CPU×20日をか
+     けてn=24を解決し世界一に、その後2005 年にニッツァ大学でn=25、2016年にドレ
+     スデン工科大学でn=27の解を求めることに成功している。
+ 
+
+
+    ステップ
+  
+   １．ブルートフォース（力まかせ探索） NQueen1()
+   ２．バックトラック                   NQueen2()
+   ３．配置フラグ（制約テスト高速化）   NQueen3()
+   ４．ビットマップ                     NQueen4()
+   ５．対称解除法(回転と斜軸）          NQueen5()
+   ６．マルチスレッド                   NQueen6()
+
+
+
+ 1. ブルートフォース　力任せ探索
+ 　全ての可能性のある解の候補を体系的に数え上げ、それぞれの解候補が問題の解と
+   なるかをチェックする方法
+   (※)各行に１個の王妃を配置する組み合わせを再帰的に列挙組み合わせを生成するだ
+   けであって8王妃問題を解いているわけではありません
+
+  実行結果
+  :
+  :
+  16777207: 7 7 7 7 7 7 6 6
+  16777208: 7 7 7 7 7 7 6 7
+  16777209: 7 7 7 7 7 7 7 0
+  16777210: 7 7 7 7 7 7 7 1
+  16777211: 7 7 7 7 7 7 7 2
+  16777212: 7 7 7 7 7 7 7 3
+  16777213: 7 7 7 7 7 7 7 4
+  16777214: 7 7 7 7 7 7 7 5
+  16777215: 7 7 7 7 7 7 7 6
+  16777216: 7 7 7 7 7 7 7 7
+**/
+
 #include <stdio.h>
 #include <time.h>
+
 #define MINSIZE 0
 #define MAXSIZE 8
-int c=1 ; //# c:count
-int pos[MAXSIZE];
-void NQueen1(int i,int s)
-{
-  int j ; //# s:size
-  for(j=0;j<s;j++){
-      pos[i]=j ;
-      if ((i==s-1)){ 
-        printf("%d",c++);
-        for(int x=0;x<s;x++){
-          printf("%d",pos[x]);
+
+int iCount=1 ; //# c:count
+int aBoard[MAXSIZE];
+
+void NQueen1(int iMin,int iMax) {
+  for(int i=0;i<iMax;i++){
+      aBoard[iMin]=i ;
+      if (iMin==iMax-1){ 
+        printf("%d: ",iCount++);
+        for(int j=0;j<iMax;j++){
+          printf("%d ",aBoard[j]);
         }
         printf("\n");
       }else{
-        NQueen1(i+1,s);
+        NQueen1(iMin+1,iMax);
       }
   }  
 }
-
-int main(void)
-{
-NQueen1(MINSIZE,MAXSIZE);
+int main(void) {
+  NQueen1(MINSIZE,MAXSIZE);
 }
+
--- C/07_2NQueen.c
+++ C/07_2NQueen.c
@@ -1,31 +1,72 @@
+/**
+
+  Cで学ぶアルゴリズムとデータ構造  
+  一般社団法人  共同通信社  情報技術局  鈴木  維一郎(suzuki.iichiro@kyodonews.jp)
+ 
+  Java版はこちらを
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Java
+  Lua版
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Lua
+  Bash版
+  https://github.com/suzukiiichiro/AI_Algorithm_N-Queen/Bash
+ 
+  ステップバイステップでＮ−クイーン問題を最適化
+   １．ブルートフォース（力まかせ探索） NQueen1()
+ <>２．配置フラグ（制約テスト高速化）   NQueen2()
+   ３．バックトラック                   NQueen3()
+   ４．ビットマップ                     NQueen4()
+   ５．ユニーク解                       NQueen5()
+
+  ２．配置フラグ（制約テスト高速化）
+   パターンを生成し終わってからチェックを行うのではなく、途中で制約を満たさな
+   い事が明らかな場合は、それ以降のパターン生成を行わない。
+  「手を進められるだけ進めて、それ以上は無理（それ以上進めても解はない）という
+  事がわかると一手だけ戻ってやり直す」という考え方で全ての手を調べる方法。
+  (※)各行列に一個の王妃配置する組み合わせを再帰的に列挙分枝走査を行っても、組
+  み合わせを列挙するだけであって、8王妃問題を解いているわけではありません。
+ 
+  実行結果
+  :
+  :
+  40312: 7 6 5 4 2 1 3 0
+  40313: 7 6 5 4 2 3 0 1
+  40314: 7 6 5 4 2 3 1 0
+  40315: 7 6 5 4 3 0 1 2
+  40316: 7 6 5 4 3 0 2 1
+  40317: 7 6 5 4 3 1 0 2
+  40318: 7 6 5 4 3 1 2 0
+  40319: 7 6 5 4 3 2 0 1
+  40320: 7 6 5 4 3 2 1 0
+ */
+
 #include <stdio.h>
 #include <time.h>
+
 #define MINSIZE 0
 #define MAXSIZE 8
-int c=1 ; //# c:count
-int pos[MAXSIZE];
-int flag_a[MAXSIZE];//C$B8@8l$N(Bboolean $B$N;H$$J}ITL@(B
-void NQueen2(int i,int s)
-{
-  int j ; //# s:size
-  for(j=0;j<s;j++){
-    if(! flag_a[j]){
-      pos[i]=j ;
-      if(i==s-1){
-        printf("%d",c++);
-        for(i=0;i<s;i++){
-          printf("%d",pos[i]);
+
+int iCount=1 ; //# c:count
+int aBoard[MAXSIZE];
+int fA[MAXSIZE];
+
+void NQueen2(int iMin,int iMax) {
+  for(int i=0;i<iMax;i++){
+    if(! fA[i]){
+      aBoard[iMin]=i ;
+      if(iMin==iMax-1){
+        printf("%d: ",iCount++);
+        for(int j=0;j<iMax;j++){
+          printf("%d ",aBoard[j]);
         }
         printf("\n");
       }else{
-        flag_a[j]=1;         
-        NQueen2(i+1,s);
-        flag_a[j]=0; 
+        fA[i]=1;         
+        NQueen2(iMin+1,iMax);
+        fA[i]=0; 
       }
     }
   }
 }
-int main(void)
-{
-NQueen2(MINSIZE,MAXSIZE);
+int main(void) {
+  NQueen2(MINSIZE,MAXSIZE);
 }
