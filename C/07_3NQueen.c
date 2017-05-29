3675f95d09daba399e070740d2c88175b630cda2
suzukiiichiro <suzuki.iichiro@gmail.com>
2017-05-29 13:31:43 +0900

    07_3NQueen.c

--- C/07_3NQueen.c
+++ C/07_3NQueen.c
@@ -1,64 +1,62 @@
 #include <stdio.h>
 #include <time.h>
+
 #define MAXSIZE 12
+
 // 日本語
 
-int T=1 ;
-int U;
-int fa[MAXSIZE];
-int fb[MAXSIZE];
-int fc[MAXSIZE];
-int pos[MAXSIZE];
-void TimeFormat(clock_t utime, char *form)
-{
-    int  dd, hh, mm;
-    float ftime, ss;
+int iTotal=1 ;
+int iUnique=0;
+int iSize;
+int colChk [2*MAXSIZE-1];
+int diagChk[2*MAXSIZE-1];
+int antiChk[2*MAXSIZE-1];
+int aBoard[MAXSIZE];
 
-    ftime = (float)utime / CLOCKS_PER_SEC;
-
-    mm = (int)ftime / 60;
-    ss = ftime - (float)(mm * 60);
-    dd = mm / (24 * 60);
-    mm = mm % (24 * 60);
-    hh = mm / 60;
-    mm = mm % 60;
-
-    if (dd) sprintf(form, "%4d %02d:%02d:%05.2f", dd, hh, mm, ss);
-    else if (hh) sprintf(form, "     %2d:%02d:%05.2f", hh, mm, ss);
-    else if (mm) sprintf(form, "        %2d:%05.2f", mm, ss);
-    else sprintf(form, "           %5.2f", ss);
+void TimeFormat(clock_t utime, char *form) {
+    int dd,hh,mm;
+    float ftime,ss;
+    ftime=(float)utime/CLOCKS_PER_SEC;
+    mm=(int)ftime/60;
+    ss=ftime-(float)(mm * 60);
+    dd=mm/(24*60);
+    mm=mm%(24*60);
+    hh=mm/60;
+    mm=mm%60;
+    if (dd) sprintf(form,"%4d %02d:%02d:%05.2f",dd,hh,mm,ss);
+    else if (hh) sprintf(form, "     %2d:%02d:%05.2f",hh,mm,ss);
+    else if (mm) sprintf(form, "        %2d:%05.2f",mm,ss);
+    else sprintf(form, "           %5.2f",ss);
 }
-void NQueen3(int i,int s){
-  int j; //# s:size
-  for(j=0;j<s;j++){
-    if(fa[j]==0 && fb[i+j]==0 && fc[i-j+s-1]==0){
-      pos[i]=j ;
-      if(i==s-1){
-        T++;
-      }else{
-        fa[j]=1;
-        fb[i+j]=1; 
-        fc[i-j+s-1]=1; 
-        NQueen3(i+1,s); 
-        fa[j]=0;           
-        fb[i+j]=0;   
-        fc[i-j+s-1]=0; 
-      }          
+void NQueen3(int row){
+  if(row==iSize){
+    iTotal++;
+  }else{
+    for(int col=0;col<iSize;col++){
+      aBoard[row]=col ;
+      if(colChk[col]==0 && diagChk[row-col+(iSize-1)]==0 && antiChk[row+col]==0){
+        colChk[col]=diagChk[row-aBoard[row]+iSize-1]=antiChk[row+aBoard[row]]=1; 
+        NQueen3(row+1); 
+        colChk[col]=diagChk[row-aBoard[row]+iSize-1]=antiChk[row+aBoard[row]]=0; 
+      }
+    }  
+  }
+}
+int main(void) {
+  clock_t st; 
+  char t[20];
+  printf("%s\n"," N:        Total       Unique        hh:mm:ss.ms");
+  for(int i=2;i<=MAXSIZE;i++){
+    iSize=i;
+    iTotal=0; 
+    iUnique=0; 
+    for(int j=0;j<iSize;j++){
+      aBoard[j]=j;
     }
-  }  
-}
-
-int main(void)
-{
-  //# m: max mi:min s:size st:starttime t:time
-  int m=MAXSIZE; int mi=2; int s=mi; clock_t st; char t[20];
-  printf("%s"," N:        Total       Unique        hh:mm:ss.");
-  printf("\n");
-  for(s=mi;s<=m;s++){
-    T=0; U=0; st=clock();
-    NQueen3(0,s);
-    TimeFormat(clock() - st, t);
-    printf("%2d:%13d%13d%s\n",s,T,U,t) ;
+    st=clock();
+    NQueen3(0);
+    TimeFormat(clock()-st,t);
+    printf("%2d:%13d%16d%s\n",iSize,iTotal,iUnique,t) ;
   } 
 }
 
