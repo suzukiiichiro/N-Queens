f3b6c407ce902da73b4f6b7b1bca2a0c95f835ab
anikotaro <rmakotaro@gmail.com>
2017-06-03 23:56:59 +0900

    round

--- C/round.c
+++ C/round.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h> 
-int iSize=3;
+#include <stdlib.h>
+
+int iSize=10;
+int rh(int a,int sz){
+  int tmp;
+  int i;
+  sz=sz-1;
+  /* ビット入替 */
+  tmp = 0;
+  for( i = 0; i <= sz; i++ )
+  {
+    if( a & ( 1 << i ) )
+    {
+      tmp |= ( 1 << ( sz - i ) );
+    }
+  }
+  a = tmp;
+  return tmp;
+}
+void revHorzBitmap(int abefore[],int aafter[]){
+for(int i=0;i< iSize;i++) {
+ int score=abefore[i];
+ aafter[i]=rh(score,iSize);
+}
+
+}
 
 void dtob(int score,int size) {
   int bit = 1, i;
@@ -20,22 +45,33 @@ void dtob(int score,int size) {
   }
   printf("\n");
 }
+void rotateBitmap90(int abefore[],int aafter[]){
+  for(int i=0;i<iSize;i++) {
+    int t = 0;
+    for (int j = 0; j < iSize; j++)
+        t |= ((abefore[j] >> i) & 1) << j; // x[j] の i ビット目を
+    aafter[i] = t;                        // y[i] の j ビット目にする
+  }
+}
 int main(void) {
 int x[iSize];
 int y[iSize]; // x が元のデータ、y が回転後のデータ.
+int z[iSize]; // x が元のデータ、y が回転後のデータ.
+//int z[iSize];
 printf("元の配列\n");
 for(int i=0;i<iSize;i++) {
-      x[i] = pow(1,i); // load, and, store, 各32回
+      x[i] = pow(1,i)+rand()%2; // load, and, store, 各32回
       dtob(x[i],iSize);
 }
 printf("90度回転したもの\n");
-for(int i=0;i<iSize;i++) {
-    int t = 0;
-    for (int j = 0; j < iSize; j++)
-        t |= ((x[j] >> i) & 1) << j; // x[j] の i ビット目を
-    y[i] = t;                        // y[i] の j ビット目にする
-}
+rotateBitmap90(x,y);
 for(int i=0;i< iSize;i++) {
       dtob(y[i],iSize);
+}
+printf("左右反転\n");
+  /* ビット数を取得 */
+revHorzBitmap(y,z);
+for(int i=0;i< iSize;i++) {
+      dtob(z[i],iSize);
 }
 }
