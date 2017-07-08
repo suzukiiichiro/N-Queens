43984faa77c2350e063a5dc8ca1d9848fc672569
Cloud User <centos@ip-172-31-1-59.us-west-2.compute.internal>
2017-07-08 15:00:48 +0000

    add

--- C/amzSCP.sh
+++ C/amzSCP.sh
@@ -2,4 +2,4 @@
 
 #ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29 ;
 #sudo scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile ec2-user@34.210.150.129:/home/ec2-user/N-Queens/ ;
-sudo scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile centos@34.212.246.131:/home/centos/N-Queens/ ;
+sudo scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile centos@34.212.246.131:/home/centos/GitHub/N-Queens/ ;
