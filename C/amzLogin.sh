f2e9f91c459eba9fffd6071bac4a6bf58c90742a
suzuki.iichiro <suzukiiichiro@suzukiiichiro.local>
2017-07-08 20:38:53 +0900

    add

--- C/amzLogin.sh
+++ C/amzLogin.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29
+#ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29
+sudo ssh -i ~/.ssh/ani.txt ec2-user@34.210.150.129
--- C/amzSCP.sh
+++ C/amzSCP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29 ;
-scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile ubuntu@35.160.218.29:/home/ubuntu/N-Queens/ ;
+scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile ec2-user@34.210.150.129:/home/ec2-user/N-Queens/ ;
