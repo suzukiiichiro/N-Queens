45bdea678ccba751f845c1e0e420b04d15e7fe81
suzuki.iichiro <suzukiiichiro@suzukiiichiro.local>
2017-07-09 02:43:46 +0900

    rm

--- C/amzGET.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29 ;
-#sudo scp -i ~/.ssh/ani.txt ec2-user@34.210.150.129:/home/ec2-user/N-Queens/out . ;
-sudo scp -i ~/.ssh/ani.txt centos@34.212.246.131:/home/centos/GitHub/N-Queens/out . ;
