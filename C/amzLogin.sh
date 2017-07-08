64b95743c16f283234bf84ceda251fe3269bfacd
suzuki.iichiro <suzukiiichiro@suzukiiichiro.local>
2017-07-08 23:19:22 +0900

    add

--- C/amzLogin.sh
+++ C/amzLogin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29
-sudo ssh -i ~/.ssh/ani.txt ec2-user@34.210.150.129
+#sudo ssh -i ~/.ssh/ani.txt ec2-user@34.210.150.129
+sudo ssh -i ~/.ssh/ani.txt centos@34.212.246.131
