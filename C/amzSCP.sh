7ee3becca7cfd3ca9ca56f6b72b166364df1869e
suzukiiichiro <suzuki@localhost.localdomain>
2017-07-11 18:30:41 +0900

    add

--- C/amzSCP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ssh -i ~/.ssh/ani.txt ubuntu@35.160.218.29 ;
-#sudo scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile ec2-user@34.210.150.129:/home/ec2-user/N-Queens/ ;
-sudo scp -i ~/.ssh/ani.txt 07_17NQueen.c Makefile centos@34.212.246.131:/home/centos/GitHub/N-Queens/C/ ;
