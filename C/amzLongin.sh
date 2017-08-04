50b19f47020d96941eda5f4ebed8568b8325cd74
suzukiiichiro <suzuki.iichiro@gmail.com>
2017-08-04 14:03:36 +0900

    amzLongin.sh

--- C/amzLongin.sh
+++ C/amzLongin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo ssh -i ~/.ssh/ani.txt centos@34.212.246.131
+sudo ssh -i ani.txt -o 'StrictHostKeyChecking no' centos@34.213.119.28
+#sudo ssh -i ani.txt -o 'StrictHostKeyChecking no' centos@13.58.146.146
+#sudo ssh -i ani.txt -o 'StrictHostKeyChecking no' centos@52.14.178.37
