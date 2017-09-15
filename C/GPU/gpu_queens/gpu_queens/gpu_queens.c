5b19f3d99daea45b9bba80fed082104fa772164d
suzukiiichiro <suzuki.iichiro@gmail.com>
2017-09-15 18:27:27 +0900

    rm

--- C/GPU/gpu_queens/gpu_queens/gpu_queens.c
+++
@@ -1,9 +0,0 @@
-//
-//  gpu_queens.c
-//  gpu_queens
-//
-//  Created by suzuki.iichiro on 2017/09/14.
-//  Copyright © 2017年 suzuki.iichiro. All rights reserved.
-//
-
-#include <stdio.h>
--- C/GPU/gpu_queens/gpu_queens/queen_kernel.c
+++
@@ -1,9 +0,0 @@
-//
-//  queen_kernel.c
-//  gpu_queens
-//
-//  Created by suzuki.iichiro on 2017/09/14.
-//  Copyright © 2017年 suzuki.iichiro. All rights reserved.
-//
-
-#include "queen_kernel.h"
