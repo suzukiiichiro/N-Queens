cf7bb77cca6159468d610b412a43161c48558347
anikotaro <rmakotaro@gmail.com>
2017-09-20 11:13:06 +0900

    kensho

---
+++ C/gpu_queens/kensho.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <time.h>
+
+#define MAX 27
+
+long Total=1 ;        //合計解
+long Unique=0;        //ユニーク解
+enum { PLACE, REMOVE, DONE };
+const int si=13;
+const int msk=(1<<si)-1;
+typedef struct{
+	int BOUND1;
+  int id;
+  int aB[si];
+  long lTotal;
+  char step;
+  char y;
+  char startCol;
+  int bm;
+  int down;
+  int right;
+  int left;
+}local; 
+void place(local *l) {
+  //int index=get_global_id(0);
+  int aB[si];
+  for (int i=0; i < si; i++)
+    aB[i]=l->aB[i];
+  int BOUND1=l->BOUND1;
+  long lTotal=l->lTotal;
+  char step=l->step;
+  int y=l->y;
+  int startCol =l->startCol;
+  int bm    =l->bm;
+  int down    =l->down;
+  int right     =l->right;
+  int left     =l->left;
+  int i=1;
+  printf("BOUND1: %d\n",BOUND1);
+  while (i!=0) {
+  	i++;
+    if (step==REMOVE) {
+      if (y==startCol) {
+        step=DONE;
+        l->lTotal=lTotal;
+        break;
+      }
+      bm=aB[--y];
+    }
+    int bit;
+    bit=bm&-bm;
+    //if(y==0){
+    //  if(bm & (1<<BOUND1)){
+    //    bit=1<<BOUND1;
+        //aB[y]=bit;
+    //  }else{
+    //    step=DONE;
+    //    break;
+    //  }
+   // }else{
+    //  bit=bm&-bm;
+      //aB[y]=bit;
+    //}
+    //
+    //
+    //
+    //
+    //qint  bit=bm & -bm;
+          down ^= bit;
+          right  ^= bit<<y;
+          left  ^= bit<<(si-1-y);
+    if (step==PLACE) {
+      aB[y++]=bm;
+      if (y==si) {
+        lTotal += 1;
+        step=REMOVE;
+      } else {
+        bm=msk & ~(down | (right>>y) | (left>>((si-1)-y)));
+        if (bm==0)
+          step=REMOVE;
+//
+      }
+    } else {
+      //if(y>0){
+      bm ^= bit;
+      //}
+      if (bm==0)
+        step=REMOVE;
+      else
+        step=PLACE;
+    }
+  }
+  // Save kernel state for next round.
+}
+int main(void) {
+  local l[si];              //構造体 local型 
+  for(int i=0; i < si; i++){
+		l[i].BOUND1=i;
+    l[i].id=i;
+    l[i].bm=(1<<si)-1;
+  }
+  for(int i=0; i < si; i++){
+    place(&l[i]);
+  }
+  for(int i=0; i < si; i++){
+    printf("%d: %ld\n",l[i].id,l[i].lTotal);
+	}
+}
