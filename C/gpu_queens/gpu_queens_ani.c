df00adc1ed576b45bece658a3eb1348f552680ba
anikotaro <rmakotaro@gmail.com>
2017-09-22 19:09:16 +0900

    j

--- C/gpu_queens/gpu_queens_ani.c
+++ C/gpu_queens/gpu_queens_ani.c
@@ -1,279 +1,394 @@
-
 #include "stdio.h"
 #include "string.h"
-#include "OpenCL/cl.h"
+#ifdef __APPLE__ // MacOSであるかを確認
+#include "OpenCL/cl.h" //MacOSの場合はインクルード
+#else
+#include <CL/cl.h> //Windows/Unix/Linuxの場合はインクルード
+#endif
+#define PROGRAM_FILE "./queen_kernel.c" //カーネルソースコード
+#define FUNC "place" //カーネル関数の名称を設定
 
-const int32_t numQueens = 8;
-const int32_t spread = 1024;
-
+const int32_t si=15;
+//const int32_t spread=1024;
+const int32_t spread=si;
 typedef int64_t qint;
-
-enum { Place, Remove, Done };
-
-struct queenState
-{
+enum { Place,Remove,Done };
+struct queenState {
+	int BOUND1;
+  int j;
   int id;
-  qint masks[numQueens];
-  uint64_t solutions;
+  qint aB[si];
+  uint64_t lTotal;
   char step;
-  char col;
-  char startCol;
-  qint mask;
-  qint rook;
-  qint add;
-  qint sub;
+  int y;
+  int startCol;
+  qint bm;
+  qint down;
+  qint right;
+  qint left;
 } __attribute__((packed));
-
-void get_queens_code(char ** buffer)
-{
+/**
+ * カーネルコードの読み込み
+ */
+void get_queens_code(char ** buffer){
   char prefix[256];
   int prefixLength =
-    snprintf(prefix, 256, "#define OPENCL_STYLE\n#define NUM_QUEENS %d\n", numQueens);
-
-  FILE * f = fopen("./queen_kernel.c", "rb");
-
-  if (!f)
-  {
-    *buffer = NULL;
+    snprintf(prefix,256,"#define OPENCL_STYLE\n#define si %d\n",si);
+  FILE * f=fopen(PROGRAM_FILE,"rb");
+  if(!f){
+    *buffer=NULL;
     return;
   }
-
-  long fileLength = 0;
-  fseek(f, 0, SEEK_END);
-  fileLength = ftell(f);
-  fseek(f, 0, SEEK_SET);
-
-  long totalLength = prefixLength + fileLength + 1;
-  *buffer = malloc(totalLength);
-
-  strcpy(*buffer, prefix);
-
-  if (buffer)
-    fread(*buffer + prefixLength, 1, fileLength, f);
-
+  long fileLength=0;
+  fseek(f,0,SEEK_END);
+  fileLength=ftell(f);
+  fseek(f,0,SEEK_SET);
+  long totalLength=prefixLength + fileLength + 1;
+  *buffer=malloc(totalLength);
+  strcpy(*buffer,prefix);
+  if(buffer)
+    fread(*buffer + prefixLength,1,fileLength,f);
   fclose(f);
-
   // Replace BOM with space
-  (*buffer)[prefixLength] = ' ';
-  (*buffer)[prefixLength + 1] = ' ';
-  (*buffer)[prefixLength + 2] = ' ';
+ (*buffer)[prefixLength]=' ';
+ (*buffer)[prefixLength + 1]=' ';
+ (*buffer)[prefixLength + 2]=' ';
 }
-
-int all_tasks_done(struct queenState * tasks, size_t num_tasks)
-{
-  for (int i = 0; i < num_tasks; i++)
-    if (tasks[i].step == Done)
+int all_tasks_done(struct queenState * tasks,size_t num_tasks){
+  for(int i=0; i <(int)num_tasks; i++)
+    if(tasks[i].step==Done)
       return 1;
-
   return 0;
 }
-
-int main()
-{
+/**
+main()OpenCL 主な流れ 
+clGetPlatformIDs();         // プラットフォーム一覧を取得
+clGetDeviceIDs();           // デバイス一覧を取得
+clCreateContext();          // コンテキストの作成
+clCreateProgramWithSource();// ソースコードからカーネルプログラム作成
+clBuildProgram();						// カーネルプログラムのビルド
+clGetProgramBuildInfo();		// プログラムのビルド情報を取得
+clCreateKernel();						// カーネルの作成
+clCreateCommandQueue();     // コマンドキュー作成
+clCreateBuffer();           // 書き込み・読み込みメモリバッファの作成
+clSetKernelArg();           // カーネル引数の設定
+clEnqueueWriteBuffer();     // メモリバッファへの書き込み
+clEnqueueNDRangeKernel();   // カーネル実行
+clEnqueueReadBuffer();      // メモリバッファから結果読み出し
+clFinish();                 // 実行が終わるまで待機
+// それぞれの終了処理
+clReleaseMemObject();
+clReleaseKernel();
+clReleaseProgram();
+clReleaseCommandQueue();
+clReleaseContext();
+*/
+#define BUFFER_SIZE 4096
+int main(){
+  /* OpneCL APIの変数 */
   cl_int status;
 
-  printf("Looking up first platform.\n");
+	char value[BUFFER_SIZE];
+	size_t size;
+  /**
+	プラットフォーム一覧を取得
+	clGetPlatformIDs()使用できるプラットフォームの数とID一覧を取得する関数
+	戻り値　CL_SUCCESS 成功 CL_INVALID_VALUE 失敗
+	PLATFORM : 見つかったプラットフォームの最大取得数
+	platforms : 見つかったプラットフォームの一覧が代入されるポインタ
+	&platformCount : 使用できるプラットフォームの数が代入されるポインタ  
+  */
+
+  /** プラットフォームオブジェクトの宣言 */
   cl_platform_id platform;
-//【1】：プラットフォームIDの取得
-//cl_int clGetPlatformIDs (cl_uint num_entries, cl_platform_id *platforms, cl_uint *num_platforms)
-//platforms 内に返された cl_platform_id の値は、特定のOpenCLプラットフォームを指定するのに用いることができます。
-  status = clGetPlatformIDs(1, &platform, NULL);
-
-  if (status != CL_SUCCESS)
-  {
-    return 1;
+  /** OpenCLデバイスのプラットフォームの特定*/
+  status=clGetPlatformIDs(1,&platform,NULL);
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't get platform ID.");
+    return 1; 
   }
-
-  printf("Determining number of devices on platform.\n");
+	status=clGetPlatformInfo(platform,CL_PLATFORM_NAME,BUFFER_SIZE,value,&size);
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't get platform info.");
+    return 2; 
+  }else{
+    printf("CL_PLATFORM_NAME:%s\n",value);
+  }
+	status=clGetPlatformInfo(platform,CL_PLATFORM_VERSION,BUFFER_SIZE,value,&size);	
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't get platform info.");
+    return 3; 
+  }else{
+    printf("CL_PLATFORM_VERSION:%s\n",value);
+  }
+	// デバイス一覧を取得
+	// clGetDeviceIds()使用できるデバイスの数とID一覧を取得する関数
+	// platforms[0]clGetPlatformIDs関数で取得したプラットフォームのID
+	// CL_DEVICE_TYPE_CPU：ホストプロセッサを指定する
+	// CL_DEVICE_TYPE_GPU：GPUデバイスを指定する。
+	// CL_DEVICE_TYPE_ACCELERATOR：OpenCL専用デバイスを指定する
+	// CL_DEVICE_TYPE_CUSTOM：OpenCL C言語で実装されたプログラムに対応していないデバイスを指定する。
+	// CL_DEVICE_TYPE_DEFAULT：システム上で設定されているデフォルトのOpenCLデバイス。
+	// この指定はCL_DEVICE_TYPE_CUSTOMと併用できない。
+	// CL_DEVICE_TYPE_ALL：C言語で実装されたプログラムに対応していないデバイスを除いたす
+	// べての使用可能なOpenCLデバイスを指定する。
+	// DEVICE_MAX : 取得するデバイスの制限数。
+	// devices : 見つかったOpenCLデバイスID一覧を取得するためのポインタ。
+	// &deviceCount : 第３引数device_typeに適合したOpenCLデバイスの数を取得するためのポインタ。
+	//デバイスのカウント
   cl_uint num_devices;
-  //【2】：デバイスIDの取得 
-  //cl_int clGetDeviceIDs[1] (cl_platform_id platform, cl_device_type device_type, cl_uint num_entries, cl_device_id *devices, cl_uint *num_devices)
-  //CL_DEVICE_TYPE_ALL  システム上の有効なOpneCLデバイス全てです。
-  status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+ 	status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_GPU,0,NULL,&num_devices);
+	if(status==CL_DEVICE_NOT_FOUND){
+ 		status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_ALL,0,NULL,&num_devices);
+		printf("CL_DEVICE_TYPE_ALL\n");
+	}
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't get device count.");
+    return 4; 
+  }else{
+	  printf("CL_DEVICE COUNT:%d\n",num_devices);
+  }
+	//デバイスIDの取得
 
-  if (status != CL_SUCCESS)
-    return 2;
+  /** デバイスオブジェクトの宣言 */
+ 	cl_device_id * devices=malloc(num_devices * sizeof(cl_device_id));
+	status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_GPU,num_devices,devices,NULL);
+	if(status==CL_DEVICE_NOT_FOUND){
+		status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_ALL,num_devices,devices,NULL);
+		printf("CL_DEVICE_TYPE_ALL\n");
+	}else{
+		printf("CL_DEVICE_TYPE_GPU\n");
+	}
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't get platform device count.");
+    return 5; 
+  }else{
+    printf("CL_DEVICE INFO\n");
+  }
+	for(int didx=0;didx<(int)num_devices;didx++){
+		status=clGetDeviceInfo(devices[didx],CL_DEVICE_NAME,BUFFER_SIZE,value,&size);	
+    if(status!=CL_SUCCESS){
+      printf("Couldn't get device name.");
+      return 6;
+    }else{
+      printf(" +CL_DEVICE_NAME:%s\n",value);
+    }
+		status=clGetDeviceInfo(devices[didx],CL_DEVICE_VERSION,BUFFER_SIZE,value,&size);
+    if(status!=CL_SUCCESS){
+      printf("Couldn's get device version.");
+      return 7;
+    }else{
+      printf("  CL_DEVICE_VERSION:%s\n",value);
+    }
+	}
+	// コンテキストの作成
+	// clCreateContext()ひとつ以上のデバイスで使用するためのコンテキストを作成する。
+	// nullptr コンテキストプロパティを指定する。
+	// 各プロパティ名にはそれぞれに対応した要求される値が続く。この一覧の終端には0がつけ
+	// られる。引数porpertiesには、処理依存のプラットフォームの場合に限りNULLを指定する
+	// ことができる。
+	// 1 : 第３引数devicesで指定されたデバイスの数
+	// devices : 一意に定まる、clGetDeviceIDs関数で取得されたデバイス、また
+	// はclCreateleftDevicesで作成されたサブデバイス。
+	// nullptr : アプリケーションによって登録することが可能なコールバック関数。
+	// nullptr : 引数pfn_notifyで設定したコールバック関数が呼び出されたとき、データが
+	// 渡されるポインタ。この引数はNULLにした場合、無視される
+	// &err エラーが発生した場合、そのエラーに合わせたエラーコードが返される。
 
-  printf("Getting device IDs.\n");
-  cl_device_id * devices = malloc(num_devices * sizeof(cl_device_id));
-  status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
-
-  printf("%d devices detected.\n", num_devices);
-
-  if (status != CL_SUCCESS)
-    return 3;
-//【3】：コンテキストの作成
-//cl_context clCreateContext (const cl_context_properties *properties, cl_uint num_devices, const cl_device_id *devices,
-//OpenCLコンテキストを作成します。OpenCLコンテキストは、1つ以上のデバイスと関連付けられて作成されます。コンテキストは、コマンドキューやメモリ、プログラム、カーネルなどといったオブジェクトを扱うために、また、コンテキスト内の指定された1つ以上のデバイス上でカーネルを実行するために用いられます。
-  printf("Creating context.\n");
-  cl_context context =
-    clCreateContext(NULL, num_devices, devices, NULL, NULL, &status);
-
-  if (status != CL_SUCCESS)
-    return 4;
-
-  printf("Loading kernel code.\n");
-
+  /** コンテキストオブジェクトの宣言 */
+  cl_context context=clCreateContext(NULL,num_devices,devices,NULL,NULL,&status);
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't creating context.\n");
+    return 8; 
+  }else{
+    printf("Creating context.\n");
+  }
+  //カーネルコードの読み込み
   char * code;
-  //カーネルのコードを読み込む
   get_queens_code(&code);
-
-  if (code == NULL)
-  {
+  if(code==NULL){
     printf("Couldn't load the code.");
+    return 9;
+  }else{
+    printf("Loading kernel code.\n");
   }
-
-  printf("Creating program.\n");
-  //【4】：実装・コンパイルしたプログラムを読み込む
-  //cl_program clCreateProgramWithSource (cl_context context, cl_uint count, const char **strings, const size_t *lengths, cl_int *errcode_ret)
-  //指定したOpenCLコンテキストについてプログラムオブジェクトを作成し、strings 配列内のソースコードをそのプログラムオブジェクト内に読み込みます。context と関連付けられたデバイスがプログラムオブジェクトと関連付けられます。
-  cl_program program =
-    clCreateProgramWithSource(context, 1, (const char **) &code, NULL, &status);
-
+	/** プログラムオブジェクトの宣言 */
+  cl_program program=clCreateProgramWithSource(context,1,(const char **)&code,NULL,&status);
   free(code);
-
-  if (status != CL_SUCCESS)
-    return 5;
-
-  printf("Building program.\n");
-  //【5】：読み込んだプログラムをビルドする
-  //特定のデバイス用の実行可能プログラムを、プログラムソースもしくはバイナリからビルド（コンパイルとリンク）します。
-  status = clBuildProgram(program, num_devices, devices, NULL, NULL, NULL);
-
-  if (status != CL_SUCCESS)
-  {
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't creating program.");
+    return 10; 
+  }else{
+    printf("Creating program.\n");
+  }
+	// プログラムのビルド
+	// clBuildProgram()カーネルオブジェクトを作成する。
+	// program    実行ファイルを作成するもとになるプログラム
+	// kernel_name    __kernelで事前に指定する関数名。
+	// errcode_ret    実行結果に関連づけられたエラーコードを格納するポインタ。
+  status=clBuildProgram(program,num_devices,devices,NULL,NULL,NULL);
+  if(status!=CL_SUCCESS){
     char log[2048];
-    status = clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG,
-                                   2048, log, NULL);
-
-    printf("%s", log);
-    return 6;
+   	status=clGetProgramBuildInfo(program,devices[0],CL_PROGRAM_BUILD_LOG,2048,log,NULL);
+    printf("%s",log);
+    printf("Couldn't building program.");
+    return 11;
+  }else{
+    printf("Building program.\n");
   }
+	/** カーネルオブジェクトの宣言 */
+  cl_kernel kernel=clCreateKernel(program,FUNC,&status);
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't creating kernel.");
+    return 12; 
+  }else{
+    printf("Creating kernel.\n");
+  }
+	// コマンドキューの作成
+	// clCreateCommandQueue()指定したデバイスのコマンドキューを作成する。
+	// context    OpenCLコンテキスト。
+	// device    第１引数のcontextに関連づけられたデバイス。
+	// properties    コマンドキューに適用するプロパティのリスト。
+	// errcode_ret    エラーコードを格納する変数。
 
-  printf("Creating kernel.\n");
-  //【6】：カーネルの作成
-  //cl_kernel clCreateKernel (cl_program program, const char *kernel_name, cl_int *errcode_ret)
-  //OpenCL の処理を実行するための OpenCL カーネルを生成します。カーネルの概念が分からないときは、カーネルはプログラムとデバイス間をやりとりするためのもの、あるいは、関数と引数がセットになったもの、程度に覚えておけば十分だと思います。
-  //対象のプログラムと実行する関数名を引数に指定している
-  cl_kernel kernel = clCreateKernel(program, "place", &status);
+  /** コマンドキューオブジェクトの宣言 */
+ 	cl_command_queue cmd_queue=clCreateCommandQueue(context,devices[0],0,&status);
+	/*
+ 	cl_command_queue cmd_queue[num_devices];
+  for(int i=0;i<num_devices;i++){
+    cmd_queue[i]=clCreateCommandQueue(context,devices[i],0,&status);
+  }
+	*/
 
-  if (status != CL_SUCCESS)
-    return 1;
-  //【7】：コマンドキューの作成
-  //OpenCLではCPU側がこの「コマンドキュー」を作成しておいて、作成したコンテキストで使用する
-//「各種OpenCLのコマンド」をここに格納しておく仕組みになっている。
-  printf("Creating command queue.\n");
-  cl_command_queue cmd_queue =
-    clCreateCommandQueue(context, devices[1], 0, &status);
-
-  if (status != CL_SUCCESS)
-    return 1;
-
+  if(status!=CL_SUCCESS){ 
+    printf("Couldn't creating command queue.");
+    return 13; 
+  }else{
+    printf("Creating command queue.\n");
+  }
+  /**
+   * 初期化
+   */
   // List of in-progress tasks
-  struct queenState inProgress[spread] = { 0 };
-
-  for (int i = 0; i < spread; i++)
-  {
-    struct queenState s = { 0 };
-    s.id = i;
-    s.mask = (1 << numQueens) - 1;
-    inProgress[i] = s;
+  struct queenState inProgress[spread*spread]={ 0 };
+  for(int i=0; i < spread; i++){
+    for(int j=0; j < spread; j++){
+      struct queenState s={ 0 };
+      s.BOUND1=i; //BOUND1の初期化
+      s.id=i;
+      s.bm=(1<<si)-1;
+      for (int k=0; k < si; k++){
+        s.aB[k]=k;
+      }
+      s.lTotal=0;
+      s.step=0;
+      s.y=0;
+      s.j=j;
+      s.startCol =2;
+      s.down=0;
+      s.right=0;
+      s.left=0;
+      inProgress[i*spread+j]=s;
+    }  
   }
-
-  printf("Starting computation of Q(%d)\n", numQueens);
-
-  while (!all_tasks_done(inProgress, spread))
-  {
+	// デバイスメモリを確保しつつデータをコピー
+	// clCreateBuffer()バッファオブジェクトを作成する。
+	// context バッファオブジェクトを作成するために必要なOpenCLコンテキスト。
+	// flags    「バッファオブジェクトをどのようなメモリ領域に割り当てるか」「メモリ領域
+	// をどのように使用するか」のような割り当てやusageに関する情報を指定するビットフィールド。
+	// CL_MEM_READ_WRITE カーネルにメモリ領域へのRead/Writeを許可する設定。
+	// CL_MEM_USE_HOST_PTR デバイスメモリ内でhost_ptrを指定することにより、OpsnCL処理に
+	// バッファをキャッシュすることを許可する。
+	// size    割り当てられたバッファメモリオブジェクトのバイトサイズ
+	// host_ptr    アプリケーションにより既に割り当てられているバッファデータへのポインタ。
+	// errcode_ret    実行結果に関連づけられたエラーコードを格納するポインタ。
+  printf("Starting computation of Q(%d)\n",si);
+  while(!all_tasks_done(inProgress,spread)){
     printf("loop\n");
-    
-    cl_mem buffer =
-      //【8】：GPU上にメモリ確保する
-      //OpenCLではバッファオブジェクトを経由して、GPUへデータを転送する。
-      //そのため、まずはバッファオブジェクトを作成する。
-      //cl_mem clCreateBuffer(cl_context context,//OpenCLのコンテキストを指定
-      //      cl_mem_flags flags,//フラグ
-      //            size_t size, //確保するメモリのバイトサイズ
-      //                  void *host_ptr,//CPU側のデータへのポインタ
-      //                        cl_int *errcode_ret)//エラーコードを拾う時に使用
-      clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(inProgress), NULL, &status);
-
-    if (status != CL_SUCCESS)
-    {
+    //printf("14\n");
+    cl_mem buffer=clCreateBuffer(context,CL_MEM_READ_WRITE,sizeof(inProgress),NULL,&status);
+    if(status!=CL_SUCCESS){
       printf("Couldn't create buffer.\n");
-
-      return 1;
+      return 14;
     }
-    //【9】：【6】で作成したカーネルの引数に値をセットする
-    //■第１引数：kernel
-    //値をセットするカーネル。
-    //■第２引数：arg_index
-    //引数のインデックス。左からひとつめの引数のインデックスを０とする。n個引数を持つカーネルは0..n-1のインデックスの引数を持つ。
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &buffer);
-
-    if (status != CL_SUCCESS)
-    {
+    // カーネルの引数をセット
+    // clSetKernelArg()カーネルの特定の引数に値をセットする。
+    // kernel    値をセットするカーネル。
+    // arg_index    引数のインデックス。
+    // arg_size    引数として渡すのデータのサイズ。
+    // arg_value    第２引数arg_indexで指定した引数にわたすデータへのポインタ。
+    //printf("15\n");
+    status=clSetKernelArg(kernel,0,sizeof(cl_mem),&buffer);
+    if(status!=CL_SUCCESS){
       printf("Couldn't set kernel arg.");
-      return 1;
+      return 15;
     }
-
-    //【10】：CPU ⇒ GPU のデータ転送
-    //cl_int clEnqueueReadBuffer(cl_command_queue command_queue,//コマンドキュー
-    //      cl_mem buffer,//バッファオブジェクト
-    //            cl_bool blocking_read,//ブロッキングモードを指定(CL_TRUE or CL_FALSE)
-    //                  size_t offset,//データの開始位置のオフセット
-    //                        size_t cb,//データのバイトサイズ
-    //                              void *ptr,//読み出し先。CPU側の格納用配列データのアドレス
-    //                                    cl_uint num_events_in_wait_list,//イベントのリスト。とりあえず「0」でいい
-    //                                          const cl_event *event_wait_list,//イベントリストの数。とりあえず「NULL」でいい
-    //                                                cl_event *event)//とりあえずNULLでいい
-    status = clEnqueueWriteBuffer(cmd_queue, buffer, CL_TRUE, 0, sizeof(inProgress),
- 	                                &inProgress, 0, NULL, NULL);
-
-    if (status != CL_SUCCESS)
-    {
+		//メモリバッファへの書き込み
+    //printf("16\n");
+    status=clEnqueueWriteBuffer(cmd_queue,buffer,CL_TRUE,0,sizeof(inProgress),&inProgress,0,NULL,NULL);
+		/**
+    for(int i=0;i<num_devices;i++){
+      status=clEnqueueWriteBuffer(cmd_queue[i],buffer,CL_TRUE,0,sizeof(inProgress),&inProgress,0,NULL,NULL);
+    }
+		*/
+    if(status!=CL_SUCCESS){
       printf("Couldn't enque write buffer command.");
-      return 1;
+      return 16;
     }
-
-    size_t globalSizes[] = { spread };
-    //【11】：カーネルの実行
-    //カーネル(GPU側の関数)の実行には、「clEnqueueNDRangeKernel関数」を使う。
-    //
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, 0, globalSizes, NULL, 0, NULL, NULL);
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, 0, globalSizes, NULL, 0, NULL, NULL);
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, 0, globalSizes, NULL, 0, NULL, NULL);
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, 0, globalSizes, NULL, 0, NULL, NULL);
-    status = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, 0, globalSizes, NULL, 0, NULL, NULL);
-
-    if (status != CL_SUCCESS)
-    {
+		//カーネルの実行
+    size_t globalSizes[]={ spread*spread };
+    //printf("17\n");
+   	status=clEnqueueNDRangeKernel(cmd_queue,kernel,1,0,globalSizes,NULL,0,NULL,NULL);
+		/**
+    for(int i=0;i<num_devices;i++){
+   	  status=clEnqueueNDRangeKernel(cmd_queue[i],kernel,1,0,globalSizes,NULL,0,NULL,&profile_event);
+    }
+		*/
+    if(status!=CL_SUCCESS){
       printf("Couldn't enque kernel execution command.");
-      return 1;
+      return 17;
     }
-
-    //【12】：GPU ⇒ CPU のデータ転送
-    status = clEnqueueReadBuffer(cmd_queue, buffer, CL_TRUE, 0,
-                                 sizeof(inProgress), inProgress, 0, NULL, NULL);
-
-    if (status != CL_SUCCESS)
-    {
+		//結果を読み込み
+    //printf("18\n");
+   	status=clEnqueueReadBuffer(cmd_queue,buffer,CL_TRUE,0,sizeof(inProgress),inProgress,0,NULL,NULL);
+		/**
+    for(int i=0;i<num_devices;i++){
+   	  status=clEnqueueReadBuffer(cmd_queue[i],buffer,CL_TRUE,0,sizeof(inProgress),inProgress,0,NULL,NULL);
+    }
+		*/
+    if(status!=CL_SUCCESS){
       printf("Couldn't enque read command.");
-      return 1;
+      return 18;
     }
-    //【13】： 実行が終わるまで待つ
-    status = clFinish(cmd_queue);
-
-    if (status != CL_SUCCESS)
-    {
+		//実行が終わるまで待機
+    //printf("19\n");
+    status=clFinish(cmd_queue);
+		/**
+    for(int i=0;i<num_devices;i++){
+      status=clFinish(cmd_queue[i]);
+    }
+		*/
+    if(status!=CL_SUCCESS){
       printf("Couldn't finish command queue.");
-      return 1;
+      return 19;
     }
-  }
+  }//end while
 
-  for (int i = 0; i < spread; i++)
-    printf("%d: %llu\n", inProgress[i].id, inProgress[i].solutions);
+	//結果の印字
+  long lGTotal=0;
+  for(int i=0; i < spread; i++){
+  for(int j=0; j < spread; j++){
+    printf("%d: %llu\n",inProgress[i*spread+j].id,inProgress[i*spread+j].lTotal);
+    lGTotal+=inProgress[i*spread+j].lTotal;
+	}
+	}
+  printf("lGTotal:%ld\n",lGTotal);
 
   free(devices);
-
+  clReleaseProgram(program);
+  clReleaseKernel(kernel);
+  //clReleaseCommandQueue(cmd_queue[0]);
+  clReleaseCommandQueue(cmd_queue);
+  clReleaseContext(context);
   return 0;
 }
