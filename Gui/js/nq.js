dbec5eb5a01ad2ffdadad9395b51016646e59245
wYoshi <>
2018-07-12 15:57:40 +0900

    js

--- Gui/js/nq.js
+++ Gui/js/nq.js
@@ -1,153 +1,174 @@
-var startTime = new Date();
-
-function nq(len, target){
-  var html = '<table id="table" class="table table-bordered table-sm"></table>';
-  $(target).append(html);
-  for(var x = 0; x < len; x++) {
-    $(target+' table').append('<tr class="tr-'+x+'"></tr>');
-    for(var y = 0; y < len; y++) {
-      $(target+' table').find('.tr-'+x).append('<td class="td-'+x+y+'" data-val="'+x+','+y+'"></td>').html();
-      if(x == 0) { SELECT.push([x, y]); }
-      ARR.push([x,y])
-    }
-  }
-  $(target).append(html);
-}
-
+/*--------------------------------------------
+共通変数
+---------------------------------------------*/
 var SIZE = $('#num').val();
-var ARR = [];
-var SELECT = [];
-var UPPER = 1;
 var COUNT = 0;
-var CHECK = true;
-var RES = [];
-var TIMER;
-var MAX = SIZE ^ SIZE;
 
-function init(){
-  $('#output').html('');
-  $('#res').html('');
-  $('#text > *').html('');
-  SIZE = $('#num').val();
-  ARR = [];
-  SELECT = [];
-  UPPER = 1;
-  COUNT = 0;
-  CHECK = true;
-  RES = [];
-
-  nq(SIZE, '#output');
-}
-
-$(function(){ init(); })
-
-function selects(){
-  $('#count-text').text(`総当たり：${COUNT} 回目`)
-  CHECK = true;
-  $('#output .queen').removeClass('queen');
-  for(i in SELECT){
-    i = Number(i);
-    var orgX = SELECT[i][0]
-    var orgY = SELECT[i][1]
-    $(`#output .td-${SELECT[i].join('')}`).addClass('queen');
-
-    for(l = 0; l < i; l++){
-      //横のチェック
-      if( orgX == SELECT[l][0] ) { CHECK = false; break; }
-      //縦のチェック
-      if( orgY == SELECT[l][1] ) { CHECK = false; break; }
-      //体格のチェック
-      for(k = 0; k < i; k++){
-        if( SELECT[k][0] + i - k == orgX) { CHECK = false; break; }
-        if( SELECT[k][0] - i + k == orgX) { CHECK = false; break; }
+//JS追加変数
+var TIMER = false;
+var startTime = new Date();
+var SELECT = [];
+var STEP = 1;
+/*--------------------------------------------
+設定
+---------------------------------------------*/
+class init {
+  //値を初期化
+  initValue(){
+    startTime = new Date();
+    $('#output').html('');
+    $('#res').html('');
+    $('#text > *').html('');
+    SIZE = $('#num').val();
+    SELECT = [];
+    COUNT = 0;
+  }
+  //テーブルを設定
+  makeTable(target){
+    var ARR = [];
+    var html = '<table id="table" class="table table-bordered table-sm"></table>';
+    $(target).append(html);
+    for(var x = 0; x < SIZE; x++) {
+      $(target+' table').append('<tr class="tr-'+x+'"></tr>');
+      for(var y = 0; y < SIZE; y++) {
+        $(target+' table').find('.tr-'+x).append('<td class="td-'+x+y+'" data-val="'+x+','+y+'"></td>').html();
+        if(x == 0) { SELECT.push([x, y]); }
+        ARR.push([x,y])
       }
     }
   }
-  if(CHECK) {
-    RES.push(SELECT);
-    if($(`#res .id${COUNT}`).length > 0){
-      $(`#res .id${COUNT}`).remove();
-      RES.pop();
-    }
-    $('#output table').clone().addClass(`id${COUNT}`).appendTo('#res')
-    $('#find-text').text(`${RES.length} 個発見`);
+}
 
-    if(SIZE % 2 == 1){
-      $('#res tr:nth-child('+Math.ceil(SIZE / 2)+')').addClass('c_g');
-      $('#res td:nth-child('+Math.ceil(SIZE / 2)+')').addClass('c_g');
+/*--------------------------------------------
+クイーンを配置
+---------------------------------------------*/
+class setQueen {
+  conflictQueen(){
+    for(var i = 0; i < SELECT.length; i++){
+      for(var l = 0; l < i; l++){
+        //縦横のチェック
+        if( SELECT[i][0] == SELECT[l][0] || SELECT[i][1] == SELECT[l][1] ) { return false; }
+        //対角のチェック
+        for(var k = 0; k < i; k++){
+          if( Number(SELECT[k][0]+i-k) == SELECT[i][0]) { return false; }
+          if( Number(SELECT[k][0]-i+k) == SELECT[i][0]) { return false; }
+        }
+      }
     }
-    for(var i = 0; i < SIZE / 2+1; i++) { $('#res td:nth-child('+i+')').addClass('c_r'); }
-    if($('#isstop').prop('checked')){
-      $('#stop').trigger('click');
+    return true;
+  }
+  selects(){
+    $('#count-text').text(`総当たり：${COUNT+1} 回目`)
+    $('#output .queen').removeClass('queen');
+    $(`#res .id${COUNT+1}`).remove();
+    for(var i = 0; i < SELECT.length; i++){
+      $(`#output .td-${SELECT[i].join('')}`).addClass('queen');
+    }
+    if( this.conflictQueen() ) {
+      if($(`#res .id${COUNT}`).length > 0){ $(`#res .id${COUNT}`).remove(); }
+      $('#output table').clone().addClass(`id${COUNT}`).appendTo('#res')
+      if(SIZE % 2 == 1){
+        $('#res tr:nth-child('+Math.ceil(SIZE / 2)+')').addClass('c_g');
+        $('#res td:nth-child('+Math.ceil(SIZE / 2)+')').addClass('c_g');
+      }
+      if($('#isstop').prop('checked')){
+        $('#stop').trigger('click');
+      }
     }
   }
 }
 
-
-function move(n){
-  if(SELECT[SIZE-n][0] < SIZE - 1) {
-    for(i = 0; i < n; i++){ if(typeof SELECT[SIZE-i] != 'undefined') { SELECT[SIZE-i] = [0, SIZE-i]; } }
-    SELECT[SIZE-n] = [++SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
-    if(n != 1) { UPPER = 1; }
-    COUNT++;
-  } else {
-    for(i = 0; i < n; i++){ if(typeof SELECT[SIZE-i] != 'undefined'){ SELECT[SIZE-i] = [0, SIZE-i]; } }
-    SELECT[SIZE-n] = [++SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
-    UPPER = UPPER + 1;
-    move(UPPER);
+/*--------------------------------------------
+NQueen1
+ブルートフォース
+---------------------------------------------*/
+class nQueen01 {
+  //1ステップ進める
+  increaseStep(n){
+    if(SELECT[SIZE-n][0] < SIZE - 1) {
+      for(var i = 0; i < n; i++){ SELECT[SIZE-i] = [0, SIZE-i]; }
+      SELECT.splice(SIZE);
+      SELECT[SIZE-n] = [++SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
+      if(n != 1) {  STEP = 1; }
+    } else {
+      for(var i = 0; i < n; i++){  SELECT[SIZE-i] = [0, SIZE-i]; }
+      SELECT.splice(SIZE);
+      SELECT[SIZE-n] = [++SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
+      ++STEP;
+      this.increaseStep(STEP);
+    }
+    _setQueen.selects();
+  }
+  //1ステップ戻る
+  decreaseStep(n){
+    if(SELECT[SIZE-n][0] > 0) {
+      for(var i = 0; i < n; i++){
+        SELECT[SIZE-i] = [SIZE-1, SIZE-i];
+      }
+      SELECT[SIZE-n] = [--SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
+      if(n != 1) { STEP = 1; }
+    } else {
+      for(var  i = 0; i < n; i++){
+        SELECT[SIZE-i] = [0, SIZE-i];
+      }
+      SELECT[SIZE-n] = [--SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
+      ++STEP;
+      if(STEP > SIZE) { return; }
+      this.decreaseStep(STEP);
+    }
+    _setQueen.selects();
   }
 }
 
-function moveM(n){
-  if(SELECT[SIZE-n][0] > 0) {
-    for(i = 0; i < n; i++){ if(typeof SELECT[SIZE-i] != 'undefined') { SELECT[SIZE-i] = [SIZE-1, SIZE-i]; } }
-    SELECT[SIZE-n] = [--SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
-    if(n != 1) { UPPER = 1; }
-    COUNT--;
-  } else {
-    for(i = 0; i < n; i++){ if(typeof SELECT[SIZE-i] != 'undefined'){ SELECT[SIZE-i] = [0, SIZE-i]; } }
-    SELECT[SIZE-n] = [--SELECT[SIZE-n][0], SELECT[SIZE-n][1]];
-    UPPER = UPPER + 1;
-    moveM(UPPER);
-  }
-}
 
+//実行
+const _init = new init()
+_init.initValue()
+_init.makeTable('#output'); //テーブルを作成
+
+const _setQueen = new setQueen(); //クイーンの配置
+
+const _nqueen = new nQueen01();  //どのnQueenで解くかを決める
+
+
+//1ステップ進める
 $('#btn-p').click(function(){
-  move(UPPER);
-  selects();
+  ++COUNT;
+  try { _nqueen.increaseStep(STEP); } catch(e) {}
 })
+//1ステップ戻る
 $('#btn-m').click(function(){
-  if(COUNT <= 0) { COUNT = 0; return false; }
-  moveM(UPPER);
-  selects();
+  --COUNT;
+  try { _nqueen.decreaseStep(STEP); } catch(e) {}
 })
-
-
+//自動でステップを進める
 $('#start').click(function(){
   $('#start').hide();
   $('#stop').show();
+  var max = SIZE ** SIZE;
   TIMER = setInterval(function(){
-    try {
+    if(COUNT < max) {
       $('#btn-p').trigger('click');
-    } catch(e){
-      // console.log(e)
-      console.log(COUNT)
+    } else {
       clearInterval(TIMER)
       $('#start').show();
       $('#stop').hide();
-      $('#res-text').text('解：'+RES.length);
+      $('#res-text').text('解：'+$('#res table').length);
       var endTime = new Date();
-      $('#res-time').text(`処理時間：${endTime - startTime} ms`);
+      $('#res-time').text(`処理時間：${Math.ceil((endTime - startTime) / 60)} s`);
     }
-  }, 1)
+  }, 0)
 })
-
+//一時停止を行う
 $('#stop').click(function(){
   $('#start').show();
   $('#stop').hide();
   clearInterval(TIMER);
 })
-
+//値に変更があった場合
 $('#num').keyup(function(){
-  init();
+  _init.initValue()
+  _init.makeTable('#output'); 
 })
+
+
