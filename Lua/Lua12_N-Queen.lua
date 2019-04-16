9afbcf6f1a73a107b04b12d5ee93d6188338b0d7
suzukiiichiro <suzuki.iichiro@gmail.com>
2019-04-16 14:54:22 +0900

    add

--- Lua/Lua12_N-Queen.lua
+++ Lua/Lua12_N-Queen.lua
@@ -43,46 +43,6 @@ NQueen={}; NQueen.new=function()
 	  return "00:00:"..sec
   end 
   --
-  function NQueen:rotate(bf,af,si)
-    for i=0,si,1 do
-      local t=0;
-      for j=0,si,1 do
-        --t=t|((bf[j]>>i)&1)<<(si-j-1); 
-        t=bit.bor(t,bit.lshift(bit.band(bit.rshift(bf[j],i),1),(si-j-1)));
-      end 
-      af[i]=t;
-    end 
-  end
-  --
-  function NQueen:rh(a,sz)
-    local tmp=0;
-    for i=0,sz,1 do
-      --if(a&(1<<i))then  
-      if bit.band(a,bit.lshift(1,i))~=0 then
-         --return tmp|=(1<<(sz-i)); 
-         return bit.bor(tmp,bit.lshift(1,(sz-i)));
-      end
-    end 
-    return tmp;
-  end
-  --
-  function NQueen:vMirror(bf,af,si)
-    local score ;
-    for i=0,si,1 do 
-      score=bf[i];
-      af[i]=self:rh(score,si-1);
-    end 
-  end
-  --
-  function NQueen:intncmp(lt,rt,si)
-    local rtn=0;
-    for k=0,si,1 do
-      rtn=lt[k]-rt[k];
-      if(rtn~=0)then break;end
-    end 
-    return rtn;
-  end
-  --
   function NQueen:rbits(byte,sz)
     local score=0;
     for i=sz,0,-1 do
@@ -91,56 +51,6 @@ NQueen={}; NQueen.new=function()
       end
     end
     return score;
-  end
-  --
-  function NQueen:_symmetryOps(si)
-    local nEquiv;
-    --// 回転・反転・対称チェックのためにboard配列をコピー
-    for i=0,si,1 do self.trial[i]=self.board[i]; end
-    --//時計回りに90度回転
-    self:rotate(self.trial,self.scratch,si);    
-    local k=self:intncmp(self.board,self.scratch,si);
-    if(k>0)then return; end
-    if(k==0)then nEquiv=2;
-    else
-      --//時計回りに180度回転
-      self:rotate(self.scratch,self.trial,si);  
-      k=self:intncmp(self.board,self.trial,si);
-      if(k>0)then return; end 
-      if(k==0)then nEquiv=4;
-      else
-        --//時計回りに270度回転
-        self:rotate(self.trial,self.scratch,si);
-        k=self:intncmp(self.board,self.scratch,si);
-        if(k>0) then return; end 
-        nEquiv=8;
-      end 
-    end 
-    --// 回転・反転・対称チェックのためにboard配列をコピー
-    for i=0,si,1 do
-      self.scratch[i]=self.board[i];
-    end
-    --//垂直反転
-    self:vMirror(self.scratch,self.trial,si);   
-    k=self:intncmp(self.board,self.trial,si);
-    if(k>0)then return; end 
-    if(nEquiv>2)then    --//-90度回転 対角鏡と同等       
-      self:rotate(self.trial,self.scratch,si);
-      k=self:intncmp(self.board,self.scratch,si);
-      if(k>0)then return; end 
-      if(nEquiv>4)then  --//-180度回転 水平鏡像と同等
-        self:rotate(self.scratch,self.trial,si);
-        k=self:intncmp(self.board,self.trial,si);
-        if(k>0)then return; end
-        --//-270度回転 反対角鏡と同等
-        self:rotate(self.trial,self.scratch,si);
-        k=self:intncmp(self.board,self.scratch,si);
-        if(k>0)then return; end 
-      end 
-    end 
-    if(nEquiv==2)then self.COUNT2=self.COUNT2+1;end 
-    if(nEquiv==4)then self.COUNT4=self.COUNT4+1;end 
-    if(nEquiv==8)then self.COUNT8=self.COUNT8+1;end
   end
   --
   function NQueen:symmetryOps()
@@ -225,6 +135,8 @@ NQueen={}; NQueen.new=function()
   function NQueen:backTrack2(min,left,down,right) 
     local BIT;
     local bitmap=bit.band(self.MASK,self:rbits(bit.bor(left,down,right ),self.size-1));
+    -- 枝刈りと最適化
+    -- if min==self.size then
     if min==self.size-1 then
       --self.TOTAL=self.TOTAL+1 ;
       if bitmap~=0 then
@@ -258,7 +170,7 @@ NQueen={}; NQueen.new=function()
         --self.aB[y],self.B=(-BM&BM),(-BM&BM);
           --BIT=bit.band(-bitmap,bitmap);
           --self.board[min]=BIT;
-        -- 最適化と枝刈り インラインに最適化
+        -- 枝刈りと最適化 インラインに最適化
         self.board[min],BIT=bit.band(-bitmap,bitmap),bit.band(-bitmap,bitmap);
         --BM=(BM~self.aB[y]);
         bitmap=bit.bxor(bitmap,BIT);
@@ -271,11 +183,11 @@ NQueen={}; NQueen.new=function()
   function NQueen:backTrack1(min,left,down,right) 
     local BIT;
     local bitmap=bit.band(self.MASK,self:rbits(bit.bor(left,down,right ),self.size-1));
-    -- 最適化と枝刈り
+    -- 枝刈りと最適化
     -- if min==self.size then
     if min==self.size-1 then
       --self.TOTAL=self.TOTAL+1 ;
-      -- 最適化と枝刈り
+      -- 枝刈りと最適化
       -- if bitmap==0 then
       if bitmap~=0 then
         self.board[min]=bitmap;
@@ -291,7 +203,7 @@ NQueen={}; NQueen.new=function()
         --self.aB[y],self.B=(-BM&BM),(-BM&BM)
             --BIT=bit.band(-bitmap,bitmap);
             --self.board[min]=BIT;
-        -- 最適化と枝刈り インラインに最適化
+        -- 枝刈りと最適化 インラインに最適化
         self.board[min],BIT=bit.band(-bitmap,bitmap),bit.band(-bitmap,bitmap);
         --BM=(BM~self.aB[y]);
         bitmap=bit.bxor(bitmap,BIT);
