8605ad66949f131d451f8868020a202bb163e525
suzukiiichiro <suzuki.iichiro@gmail.com>
2019-04-18 14:34:57 +0900

    add

--- Lua/Lua05_N-Queen.lua
+++ Lua/Lua05_N-Queen.lua
@@ -34,10 +34,8 @@
 NQueen={}; NQueen.new=function()
 	--
   local this={
-    SIZE=0;
-    nTOTAL=0;
-    nUNIQUE=0;
-    nEquiv=0;
+    TOTAL=0;
+    UNIQUE=0;
     colChk={};
     diagChk={};
     antiChk={};
@@ -59,120 +57,187 @@ NQueen={}; NQueen.new=function()
 	  return "00:00:"..sec;
   end 
 	--
-  function NQueen:intncmp(lt,rt,n)
-    local k,rtn=0,0; 
-    for k=0,n-1,1 do
-      rtn=lt[k]-rt[k];
-      if (rtn~=0)then break; end
+  function NQueen:intncmp(board,trial,size)
+    local rtn=0; 
+    for k=0,size,1 do
+      rtn=board[k]-trial[k];
+      if (rtn~=0)then 
+        break; 
+      end
     end
     return rtn;
   end
 	--
-  function NQueen:rotate(trial,scratch,n,neg)
-    local k=0;
-	  local incr=0;
-    if neg then k=0; else k=n-1; end
+  function NQueen:rotate(trial,scratch,size,neg)
+    local k;
+	  local incr;
+    if neg then k=0; else k=size-1; end
     if neg then incr=1; else incr=-1; end 
+
 	  local j=0;
-	  while j<n do
+	  while j<size do
       scratch[j]=trial[k];
 		  k=k+incr;
       j=j+1;
     end
-    if neg then k=n-1; else k=0; end 
+
+    if neg then k=size-1; else k=0; end 
+
 	  local j=0;
-	  while j<n do
+	  while j<size do
       trial[scratch[j]]=k;
-      j=j+1;
 	    k=k-incr;
+      j=j+1;
     end
   end
 	--
-  function NQueen:vMirror(check,n)
-    for j=0,n-1,1 do
-      check[j]=(n-1)-check[j];
+  function NQueen:vMirror(trial,size)
+    for j=0,size,1 do
+      trial[j]=(size-1)-trial[j];
     end
   end
 	--
-  function NQueen:symmetryOps()
-	  local trial={};   --//回転・反転の対称チェック
-	  local scratch={};
-    self.nEquiv=0; 	  
-    for k=0,self.size-1,1 do trial[k]=self.board[k]; end
+  function NQueen:symmetryOps(board,trial,scratch,size)
+    local nEquiv=0; 	  
+    --初期化
+    for i=0,size,1 do 
+      trial[i]=board[i]; 
+    end
     --//時計回りに90度回転
-    self:rotate(trial,scratch,self.size,nil);
-    local k=self:intncmp(self.board,trial,self.size);
+    self:rotate(trial,scratch,size,0);
+    local k=self:intncmp(board,trial,size);
     if(k>0)then return 0; end
-    if(k==0)then self.nEquiv=1; else
+    if(k==0)then 
+      nEquiv=1; 
+    else
       --//時計回りに180度回転
-      self:rotate(trial,scratch,self.size,nil);
-      k=self:intncmp(self.board,trial,self.size);
+      self:rotate(trial,scratch,size,0);
+      k=self:intncmp(board,trial,size);
       if(k>0) then return 0; end
-      if(k==0)then self.nEquiv=2; else
+      if(k==0)then 
+        nEquiv=2; 
+      else
         --//時計回りに270度回転
-        self:rotate(trial,scratch,self.size,nil);
-        k=self:intncmp(self.board,trial,self.size);
-        if(k>0) then return 0; end
-        self.nEquiv=4;
+        self:rotate(trial,scratch,size,0);
+        k=self:intncmp(board,trial,size);
+        if(k>0) then 
+          return 0; 
+        end
+        nEquiv=4;
       end
     end  
-    for k=0,self.size-1,1 do trial[k]=self.board[k]; end
+    --初期化
+    for i=0,size,1 do 
+      trial[i]=board[i]; 
+    end
     --//垂直反転 
-    self:vMirror(trial,self.size);	
-    k=self:intncmp(self.board,trial,self.size);
-    if(k>0) then return 0; end
+    self:vMirror(trial,size);	
+    k=self:intncmp(board,trial,size);
+    if(k>0) then 
+      return 0; 
+    end
     --// 4回転とは異なる場合
-    if (self.nEquiv>1) then
+    if (nEquiv>1) then
 			-- 90度回転 対角鏡と同等
-      self:rotate(trial,scratch,self.size,true);
-      k=self:intncmp(self.board,trial,self.size);
-      if(k>0) then return 0; end
+      self:rotate(trial,scratch,size,1);
+      k=self:intncmp(board,trial,size);
+      if(k>0) then 
+        return 0; 
+      end
       --// 2回転とは異なる場合
-      if(self.nEquiv>2)then
+      if(nEquiv>2)then
 			  -- 180度回転 水平鏡像と同等
-        self:rotate(trial,scratch,self.size,true);
-        k=self:intncmp(self.board,trial,self.size);
-        if(k>0) then return 0; end
+        self:rotate(trial,scratch,size,1);
+        k=self:intncmp(board,trial,size);
+        if(k>0) then 
+          return 0; 
+        end
 			  -- 270度回転 反対角鏡と同等
-        self:rotate(trial,scratch,self.size,true);
-        k=self:intncmp(self.board,trial,self.size);
-        if(k>0) then return 0; end
+        self:rotate(trial,scratch,size,1);
+        k=self:intncmp(board,trial,size);
+        if(k>0) then 
+          return 0; 
+        end
       end 
     end
-    return self.nEquiv * 2;
+    return nEquiv * 2;
   end
 	--
-  function NQueen:NQueens(row)
-    local lim=0;
-    if row==self.size then
-	    local tst=self:symmetryOps();--//回転・反転・対称の解析
+  function NQueen:NQueens(board,row,size)
+    local tmp;
+    local trial={};
+    local scratch={};
+    --枝刈り
+    -- if row==size then
+    if row==size-1 then
+      --枝刈り
+      if(self.diagChk[row-board[row]+size-1]==true
+        or self.antiChk[row+board[row]]==true) then
+        return;
+      end
+	    local tst=self:symmetryOps(board,trial,scratch,size);--//回転・反転・対称の解析
 	    if(tst~=0) then
-	      self.nUnique=self.nUnique+1;
-	      self.nTotal=self.nTotal+tst;
+	      self.UNIQUE=self.UNIQUE+1;
+        self.TOTAL=self.TOTAL+tst;
 	    end
     else
       --枝刈り
-		  if row~=0 then 
-        lim=self.size; 
-      else 
-        lim=(self.size+1)/2; 
-      end
-      -- for i=row+1,lim,1 do
-      for i=0,self.size-1,1 do
-        self.board[row]=i; 
-        if	self.colChk[i]==nil 
-			  and self.antiChk[row+i]==nil 
-			  and self.diagChk[row-i+(self.size-1)]==nil then
-          self.colChk[i],
-          self.antiChk[row+i],
-          self.diagChk[row-i+(self.size-1)]=true,true,true;
-          self:NQueens(row+1);
-          self.colChk[i],
-          self.antiChk[row+i],
-          self.diagChk[row-i+(self.size-1)]=nil,nil,nil;
+      local lim ;
+		  if row~=0 then lim=size; else lim=(size+1)/2; end
+      --for i=0,size-1,1 do
+        for i=row,lim,1 do
+        --board[row]=i; 
+          -- 交換
+          tmp=board[i];
+          board[i]=board[row];
+          board[row]=tmp;
+
+        if	self.antiChk[row+board[row]]==nil
+			  and self.diagChk[row-board[row]+(size-1)]==nil then
+          self.antiChk[row+board[row]],
+          self.diagChk[row-board[row]+(size-1)]=true,true;
+          self:NQueens(board,row+1,size);
+          self.antiChk[row+board[row]],
+          self.diagChk[row-board[row]+(size-1)]=nil,nil;
         end
       end
+      -- 交換
+      tmp=board[row];
+      for i=row+1,size,1 do
+        board[i-1]=board[i];
+      end
+      board[size-1]=tmp;
     end
+--    local lim=0;
+--    if row==self.size then
+--	    local tst=self:symmetryOps();--//回転・反転・対称の解析
+--	    if(tst~=0) then
+--	      self.nUnique=self.nUnique+1;
+--	      self.nTotal=self.nTotal+tst;
+--	    end
+--    else
+--      --枝刈り
+--		  if row~=0 then 
+--        lim=self.size; 
+--      else 
+--        lim=(self.size+1)/2; 
+--      end
+--      -- for i=row+1,lim,1 do
+--      for i=0,self.size-1,1 do
+--        self.board[row]=i; 
+--        if	self.colChk[i]==nil 
+--			  and self.antiChk[row+i]==nil 
+--			  and self.diagChk[row-i+(self.size-1)]==nil then
+--          self.colChk[i],
+--          self.antiChk[row+i],
+--          self.diagChk[row-i+(self.size-1)]=true,true,true;
+--          self:NQueens(row+1);
+--          self.colChk[i],
+--          self.antiChk[row+i],
+--          self.diagChk[row-i+(self.size-1)]=nil,nil,nil;
+--        end
+--      end
+--    end
 --    -- BASH05版の移植
 --    if min~=0 then lim=self.size; else lim=(self.size+1)/2; end
 --    --枝刈り
@@ -203,17 +268,16 @@ NQueen={}; NQueen.new=function()
   function NQueen:NQueen()
 	  local max=17 ; -- //最大N
     print(" N:            Total       Unique    hh:mm:ss");
-    for si=2,max,1 do
-      self.size=si; --初期化
-		  self.nUnique,self.nTotal=0,0;
-		  self.board,self.colChk,self.diagChk,self.antiChk={},{},{},{};
+    for size=2,max,1 do
+		  self.UNIQUE=0;
+      self.TOTAL=0;
       s=os.time();  --計測開始
-      for k=0,self.size-1,1 do
-        self.board[k]=k;
+      for j=0,size,1 do
+        self.board[j]=j;
       end
-      self:NQueens(0);
+      self:NQueens(self.board,0,size);
       print(string.format("%2d:%17d%13d%12s",
-												si,self.nTotal,self.nUnique,
+												size,self.TOTAL,self.UNIQUE,
 												self:secstotime(os.difftime(os.time(),s)))); 
     end
   end
