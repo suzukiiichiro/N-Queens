3df6de04fd4c1daac28519d1df0bb327e8715110
suzukiiichiro <suzuki.iichiro@gmail.com>
2019-07-24 16:52:56 +0900

    add

--- Python/py13_4_multiprocess_nqueen.py
+++ Python/py13_4_multiprocess_nqueen.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 """ py13_4_multiprocess_nqueen.py """
+
 from datetime import datetime
 from multiprocessing import Pool as ThreadPool
 #
@@ -21,7 +23,7 @@ from multiprocessing import Pool as ThreadPool
 #  処理中は全てのCPUの負荷が１００％になっていることを確認して下さい。
 #
 #
-# py12_nqueen.py 
+# py12_nqueen.py
 #  N:        Total       Unique        hh:mm:ss.ms
 #  4:            2            1         0:00:00.000
 #  5:           10            2         0:00:00.000
@@ -72,7 +74,7 @@ from multiprocessing import Pool as ThreadPool
 # マルチスレッドにて py13_3_multithread_join_nqueen.py
 # コア一つを使い回しているにすぎない。遅い
 # joinを処理末で実行。本来のマルチスレッド
-# Nの数分スレッドが起動しそれぞれ並列処理 
+# Nの数分スレッドが起動しそれぞれ並列処理
 #
 #  N:        Total       Unique        hh:mm:ss.ms
 #  4:            2            1         0:00:00.001
@@ -108,16 +110,16 @@ from multiprocessing import Pool as ThreadPool
 #
 # グローバル変数
 #
-class Nqueen():
+class Nqueen(): # pylint: disable=R0902
     """ nqueen() """
-    MAX = 16 # N = 15
-    ABOARD = [0 for i in range(MAX)]
-    TOPBIT = 0
-    ENDBIT = 0
-    SIDEMASK = 0
-    LASTMASK = 0
-    BOUND1 = 0
-    BOUND2 = 0
+    # MAX = 16 # N = 15
+    # self.aboard = [0 for i in range(MAX)]
+    # self.topbit = 0
+    # self.endbit = 0
+    # self.sidemask = 0
+    # self.lastmask = 0
+    # self.bound1 = 0
+    # self.bound2 = 0
     # MASK = 0
     #
     # 初期化
@@ -130,117 +132,123 @@ class Nqueen():
         self.gttotal = [0] * self.size      #総合計
         self.gtunique = [0] * self.size     #総合計
 
-        self.aboard = [[i for i in range(2*size-1) ] for j in range(self.size)]
+        self.aboard = [[i for i in range(2*size-1)] for j in range(self.size)]
         self.mask = (1<<size)-1
-        self.COUNT2 = 0
-        self.COUNT4 = 0
-        self.COUNT8 = 0
-    # 
+        self.count2 = 0
+        self.count4 = 0
+        self.count8 = 0
+        self.bound1 = 0
+        self.bound2 = 0
+        self.sidemask = 0
+        self.lastmask = 0
+        self.topbit = 0
+        self.endbit = 0
+    #
     # 解法
     def solve(self):
         """ solve() """
         pool = ThreadPool(self.size)
         self.gttotal = list(pool.map(self.nqueen, range(self.size)))
-        to=0
-        uni=0
+        total = 0
+        unique = 0
         #
         # ここをみて。あともう少しなんだけどな
         print("gttotal:%s" % self.gttotal)
-        for t,u in self.gttotal:
-            to+=t
-            uni+=u
+        for _t, _u in self.gttotal:
+            total += _t
+            unique += _u
         pool.close()
         pool.join()
-        return to, uni
+        return total, unique
     #
     # ユニーク値を出力
     def getunique(self):
         """ getunique() """
-        return self.COUNT2+self.COUNT4+self.COUNT8
+        return self.count2+self.count4+self.count8
     #
     # 合計を出力
     def gettotal(self):
         """ gettotal() """
-        return self.COUNT2*2+self.COUNT4*4+self.COUNT8*8
+        return self.count2*2+self.count4*4+self.count8*8
     #
     # 対称解除法
     def symmetryops(self):      # pylint: disable=R0912,R0911,R0915
         """ symmetryops() """
-        # global COUNT2  # pylint: disable=W0603
-        # global COUNT4  # pylint: disable=W0603
-        # global COUNT8  # pylint: disable=W0603
-        global ABOARD  # pylint: disable=W0603
-        global TOPBIT  # pylint: disable=W0603
-        global ENDBIT  # pylint: disable=W0603
+        # global count2  # pylint: disable=W0603
+        # global count4  # pylint: disable=W0603
+        # global count8  # pylint: disable=W0603
+        # global self.aboard  # pylint: disable=W0603
+        # global self.topbit  # pylint: disable=W0603
+        # global self.endbit  # pylint: disable=W0603
         own = 0
         ptn = 0
         you = 0
         bit = 0
         # 90度回転
-        if ABOARD[BOUND2] == 1:
+        if self.aboard[self.bound2] == 1:
             own = 1
             ptn = 2
             while own <= self.size-1:
                 bit = 1
                 you = self.size-1
-                while (ABOARD[you] != ptn) and (ABOARD[own] >= bit):
+                while (self.aboard[you] != ptn) and (self.aboard[own] >= bit):
                     bit <<= 1
                     you -= 1
-                if ABOARD[own] > bit:
+                if self.aboard[own] > bit:
                     return
-                if ABOARD[own] < bit:
+                if self.aboard[own] < bit:
                     break
                 own += 1
                 ptn <<= 1
             #90度回転して同型なら180度/270度回転も同型である */
             if own > self.size-1:
-                self.COUNT2 += 1
+                self.count2 += 1
                 return
         #180度回転
-        if ABOARD[self.size-1] == ENDBIT:
+        if self.aboard[self.size-1] == self.endbit:
             own = 1
             you = self.size-1-1
             while own <= self.size-1:
                 bit = 1
-                ptn = TOPBIT
-                while (ABOARD[you] != ptn) and (ABOARD[own] >= bit):
+                ptn = self.topbit
+                while (self.aboard[you] != ptn) and (self.aboard[own] >= bit):
                     bit <<= 1
                     ptn >>= 1
-                if ABOARD[own] > bit:
+                if self.aboard[own] > bit:
                     return
-                if ABOARD[own] < bit:
+                if self.aboard[own] < bit:
                     break
                 own += 1
                 you -= 1
             # 90度回転が同型でなくても180度回転が同型である事もある */
             if own > self.size-1:
-                self.COUNT4 += 1
+                self.count4 += 1
                 return
         #270度回転
-        if ABOARD[BOUND1] == TOPBIT:
+        if self.aboard[self.bound1] == self.topbit:
             own = 1
-            ptn = TOPBIT>>1
+            ptn = self.topbit>>1
             while own <= self.size-1:
                 bit = 1
                 you = 0
-                while (ABOARD[you] != ptn) and (ABOARD[own] >= bit):
+                while (self.aboard[you] != ptn) and (self.aboard[own] >= bit):
                     bit <<= 1
                     you += 1
-                if ABOARD[own] > bit:
+                if self.aboard[own] > bit:
                     return
-                if ABOARD[own] < bit:
+                if self.aboard[own] < bit:
                     break
                 own += 1
                 ptn >>= 1
-        self.COUNT8 += 1
+        self.count8 += 1
     #
     # BackTrack2
     def backtrack2(self, row, left, down, right): # pylint: disable=R0913
         """ backtrack2() """
-        global ABOARD       # pylint: disable=W0603
-        global LASTMASK     # pylint: disable=W0603
-        global BOUND1       # pylint: disable=W0603
-        global BOUND2       # pylint: disable=W0603
+        # global self.aboard       # pylint: disable=W0603
+        # global self.lastmask     # pylint: disable=W0603
+        # global self.bound1       # pylint: disable=W0603
+        # global self.bound2       # pylint: disable=W0603
         bit = 0
         bitmap = self.mask&~(left|down|right)
         #枝刈り
@@ -248,23 +256,23 @@ class Nqueen():
         if row == self.size-1:
             if bitmap:
                 #枝刈り
-                if (bitmap&LASTMASK) == 0:
-                    ABOARD[row] = bitmap
+                if (bitmap&self.lastmask) == 0:
+                    self.aboard[row] = bitmap
                     #symmetryOps_bitmap(size)
                     self.symmetryops()
             #else:
-            #  ABOARD[row] = bitmap
+            #  self.aboard[row] = bitmap
             #  symmetryOps_bitmap(size)
         else:
             #枝刈り 上部サイド枝刈り
-            if row < BOUND1:
-                bitmap &= ~SIDEMASK
+            if row < self.bound1:
+                bitmap &= ~self.sidemask
             #枝刈り 下部サイド枝刈り
-            elif row == BOUND2:
-                if down&SIDEMASK == 0:
+            elif row == self.bound2:
+                if down&self.sidemask == 0:
                     return
-                if down&SIDEMASK != SIDEMASK:
-                    bitmap &= SIDEMASK
+                if down&self.sidemask != self.sidemask:
+                    bitmap &= self.sidemask
             # 枝刈り
             if row != 0:
                 lim = self.size
@@ -274,32 +282,32 @@ class Nqueen():
             for i in range(row, lim):       # pylint: disable=W0612
                 while bitmap:
                     bit = (-bitmap&bitmap)
-                    ABOARD[row] = bit
-                    bitmap ^= ABOARD[row]
+                    self.aboard[row] = bit
+                    bitmap ^= self.aboard[row]
                     self.backtrack2(row+1, (left|bit)<<1, down|bit, (right|bit)>>1)
     #
     # BackTrack1
     def backtrack1(self, row, left, down, right):  # pylint: disable=R0913
         """ backtrack1() """
-        global ABOARD       # pylint: disable=W0603
-        # global COUNT8       # pylint: disable=W0603
-        global BOUND1       # pylint: disable=W0603
+        # global self.aboard       # pylint: disable=W0603,W0601
+        # global count8       # pylint: disable=W0603,W0601
+        # global self.bound1       # pylint: disable=W0603,W0601
         bit = 0
         bitmap = self.mask&~(left|down|right)
         #枝刈り
         #if row =  = size:
         if row == self.size-1:
             if bitmap:
-                ABOARD[row] = bitmap
-                #枝刈りにてCOUNT8に加算
-                self.COUNT8 += 1
+                self.aboard[row] = bitmap
+                #枝刈りにてcount8に加算
+                self.count8 += 1
             #else:
-            #  ABOARD[row] = bitmap
+            #  self.aboard[row] = bitmap
             #  symmetryOps_bitmap(size)
         else:
             #枝刈り 鏡像についても主対角線鏡像のみを判定すればよい
             # ２行目、２列目を数値とみなし、２行目＜２列目という条件を課せばよい
-            if row < BOUND1:
+            if row < self.bound1:
                 bitmap &= ~2 # bm| = 2 bm^ = 2 (bm& = ~2と同等)
             # 枝刈り
             if row != 0:
@@ -307,32 +315,33 @@ class Nqueen():
             else:
                 lim = (self.size+1)//2 # 割り算の結果を整数にするには //
             # 枝刈り
-            for i in range(row, lim):   # pylint: disable=W0612 
+            for i in range(row, lim):   # pylint: disable=W0612
                 while bitmap:
                     bit = (-bitmap&bitmap)
-                    ABOARD[row] = bit
-                    bitmap ^= ABOARD[row]
+                    self.aboard[row] = bit
+                    bitmap ^= self.aboard[row]
                     self.backtrack1(row+1, (left|bit)<<1, down|bit, (right|bit)>>1)
       #
     # メインメソッド
-    def nqueen(self, thr_index, row=0, depth=0):
+    #def nqueen(self, thr_index, row=0, depth=0):
+    def nqueen(self, thr_index):
         """ nqueen() """
-        global ABOARD
-        ABOARD = self.aboard[thr_index]
-        size = self.size
-        start = 0 if (row > 0) else int(thr_index * (size / self._nthreads))
-        end = size - 1 if ((row > 0) or (thr_index == self._nthreads - 1)) else int((thr_index + 1) * (size / self._nthreads) - 1)
-        global TOPBIT         # pylint: disable=W0603
-        global ENDBIT         # pylint: disable=W0603
-        global SIDEMASK       # pylint: disable=W0603
-        global LASTMASK       # pylint: disable=W0603
-        global BOUND1         # pylint: disable=W0603
-        global BOUND2         # pylint: disable=W0603
+        # global self.aboard         # pylint: disable=W0603,W0601
+        # self.aboard = self.aboard[thr_index]
+        # size = self.size
+        # start = 0 if (row > 0) else int(thr_index * (size / self._nthreads))
+        # end = size - 1 if ((row > 0) or (thr_index == self._nthreads - 1)) else int((thr_index + 1) * (size / self._nthreads) - 1) # pylint: disable=C0301
+        # global self.topbit         # pylint: disable=W0603,W0601
+        # global self.endbit         # pylint: disable=W0603,W0601
+        # global self.sidemask       # pylint: disable=W0603,W0601
+        # global self.lastmask       # pylint: disable=W0603,W0601
+        # global self.bound1         # pylint: disable=W0603,W0601
+        # global self.bound2         # pylint: disable=W0603,W0601
         bit = 0
-        ABOARD[0] = 1
-        TOPBIT = 1<<(size-1)
-        BOUND1 = thr_index
-        # 
+        self.aboard[0] = 1
+        self.topbit = 1<<(self.size-1)
+        self.bound1 = thr_index
+        #
         # 148+ をコメントアウトして
         # ここを入れ替えると数は合う
         # ロジックはあっているはず。合計はもちろん違うけど
@@ -341,46 +350,46 @@ class Nqueen():
         #  8:          736           96         0:00:00.119
         #
         # print(thr_index) # N=8の時は 0,1,2,3,4,5,6,7
-        # for BOUND1 in range(2, size-1):
-        if BOUND1 > 1 and BOUND1 < size - 1:
-            ABOARD[1] = bit = (1<<BOUND1)
+        for self.bound1 in range(2, self.size-1):
+        # if self.bound1 > 1 and self.bound1 < self.size - 1:
+            self.aboard[1] = bit = (1<<self.bound1)
             self.backtrack1(2, (2|bit)<<1, (1|bit), (bit>>1))
-        SIDEMASK = LASTMASK = (TOPBIT|1)
-        ENDBIT = (TOPBIT>>1)
-        BOUND2 = size-2
+        self.sidemask = self.lastmask = (self.topbit|1)
+        self.endbit = (self.topbit>>1)
+        self.bound2 = self.size-2
         #
         # 148+ をコメントアウトして
         # ここを入れ替えると数は合う
         # ロジックはあっているはず
-        # for BOUND1 in range(1, BOUND2):
-        if BOUND1 > 0 and BOUND2 < size - 1 and BOUND1 < BOUND2:
-            ABOARD[0] = bit = (1<<BOUND1)
+        for self.bound1 in range(1, self.bound2):
+        # if self.bound1 > 0 and self.bound2 < self.size - 1 and self.bound1 < self.bound2:
+            self.aboard[0] = bit = (1<<self.bound1)
             self.backtrack2(1, bit<<1, bit, bit>>1)
-            LASTMASK |= LASTMASK>>1|LASTMASK<<1
-            ENDBIT >>= 1
-            BOUND2 -= 1
-        if depth == 0:
-            return self.gettotal(),self.getunique()
-        return self.gettotal(),self.getunique()
+            self.lastmask |= self.lastmask>>1|self.lastmask<<1
+            self.endbit >>= 1
+            self.bound2 -= 1
+        # if depth == 0:
+            # return self.gettotal(), self.getunique()
+        return self.gettotal(), self.getunique()
 #
 # メインメソッド
 def main():
     """ main() """
-    # global COUNT2     # pylint: disable=W0603
-    # global COUNT4     # pylint: disable=W0603
-    # global COUNT8     # pylint: disable=W0603
-    # global ABOARD     # pylint: disable=W0603
+    # global count2     # pylint: disable=W0603,W0601
+    # global count4     # pylint: disable=W0603,W0601
+    # global count8     # pylint: disable=W0603,W0601
+    # global self.aboard     # pylint: disable=W0603,W0601
     nmin = 4                          # Nの最小値（スタートの値）を格納
-    nmax = 16 
+    nmax = 16
     print(" N:        Total       Unique        hh:mm:ss.ms")
     for i in range(nmin, nmax):
-        # COUNT2 = COUNT4 = COUNT8 = 0
+        # count2 = count4 = count8 = 0
         # mask = (1<<i)-1
         # for j in range(i):
-        #     ABOARD[j] = j              # 盤を初期化
+        #     self.aboard[j] = j              # 盤を初期化
         start_time = datetime.now()
         nqueen_obj = Nqueen(i)
-        total,unique = nqueen_obj.solve()
+        total, unique = nqueen_obj.solve()
         # nqueen(i, mask)
         time_elapsed = datetime.now()-start_time
         _text = '{}'.format(time_elapsed)
