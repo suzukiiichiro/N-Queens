83a42979ce1224a60023aacdf774c13549721ca7
suzukiiichiro <suzuki.iichiro@gmail.com>
2020-03-05 13:07:18 +0900

    作成途中

--- CUDA/GCC08NR.c
+++ CUDA/GCC08NR.c
@@ -47,6 +47,7 @@ bash-3.2$ gcc -Wall -W -O3 -g -ftrapv -std=c99 -pthread GCC08NR.c && ./a.out -c
 //
 #define MAX 27
 //変数宣言
+int BOUND1,BOUND2,TOPBIT,ENDBIT,SIDEMASK,LASTMASK;
 int down[2*MAX-1]; 	//down:flagA 縦 配置フラグ
 int right[2*MAX-1]; //right:flagB 斜め配置フラグ
 int left[2*MAX-1]; 	//left:flagC 斜め配置フラグ
@@ -62,7 +63,8 @@ void vMirror_bitmap(int bf[],int af[],int si);
 int intncmp(int lt[],int rt[],int n);
 void symmetryOps_bitmap(int si);
 void TimeFormat(clock_t utime,char *form);
-void NQueenR(int size,int mask,int row,int left,int down,int right);
+// void NQueenR(int size,int mask,int row,int left,int down,int right);
+void NQueenR(int size,int mask,int row,int left,int down,int right,int ex1,int ex2);
 void NQueen(int size,int mask);
 void print(int size);
 //hh:mm:ss.ms形式に処理時間を出力
@@ -188,124 +190,76 @@ void NQueen(int size,int mask){
   register int row=0;
   register int bit;
   register int bitmap;
-  int odd=size&1;
+  int odd=size&1; //奇数:1 偶数:0 
   int sizeE=size-1;
   /* センチネルを設定-スタックの終わりを示します*/
   aStack[0]=-1;
-  /**
-    注：サイズが奇数の場合、（サイズ＆1）は真。
-    サイズが奇数の場合は2xをループする必要があります
-    */
   for(int i=0;i<(1+odd);++i){
-    /**
-      クリティカルループ
-      この部分を最適化する必要はありません。
-      */
     bitmap=0;
     if(0==i){
-      /*中央を除くボードの半分を処理します
-        カラム。ボードが5 x 5の場合、最初の行は00011になります。
-        クイーンを中央の列に配置することについてはまだです。
-        */
-      /* ２で割る */
-      int half=size>>1;
-      /*サイズの半分のビットマップで右端の1を埋めます
-        サイズが7の場合、その半分は3です（残りは破棄します）
-        ビットマップはバイナリで111に設定されます。
-        */
-      bitmap=(1<<half)-1;
-      pnStack=aStack+1;/* スタックポインタ */
       aBoard[0]=0;
       down[0]=left[0]=right[0]=0;
+      int half=size>>1;
+      bitmap=(1<<half)-1;
+      pnStack=aStack+1;
     }else{
-      /*（奇数サイズのボードの）中央の列を処理します。
-        中央の列ビットを1に設定してから設定します
-        したがって、最初の行（1つの要素）と次の半分を処理しています。
-        ボードが5 x 5の場合、最初の行は00100になり、次の行は00011です。
-        */
-      bitmap=1<<(size>>1);
-      row=1; /*すでに 0 */
-      /* 最初の行にはクイーンが1つだけあります（中央の列）*/
       aBoard[0]=bitmap;
       down[0]=left[0]=right[0]=0;
-      /* 次の行を実行します。半分だけビットを設定します
-         「Y軸」で結果を反転します
-         */
+      bitmap=1<<(size>>1);
+      row=1;
       down[1]=bitmap;
       right[1]=(bitmap>>1);
       left[1]=(bitmap<<1);
-      pnStack=aStack+1; // スタックポインタ
-      /* この行は-1つの要素のみで完了 */
+      pnStack=aStack+1;
       *pnStack++=0;
-      /* ビットマップ-1は、単一の1の左側すべて1です */
       bitmap=(bitmap-1)>>1;
     }
-    // クリティカルループ
     while(true){
-      /*
-         bit = bitmap ^（bitmap＆（bitmap -1））;
-         最初の（最小のsig） "1"ビットを取得しますが、それは遅くなります。
-         */
-      /* これは、2の補数アーキテクチャを想定しています */
       bit=-((signed)bitmap) & bitmap;
-      if(0==bitmap){
-        /* 前を取得スタックからのビットマップ */
-        bitmap=*--pnStack;
-        /* センチネルがヒットした場合... */
-        if(pnStack==aStack){
-          break ;
+      if(bitmap){
+        bitmap^=aBoard[row]=bit=(-bitmap&bitmap); 
+        if(row==sizeE){
+          symmetryOps_bitmap(size); /* 対称解除法の追加 */
+          bitmap=*--pnStack;
+          --row;
+          continue;
+        }else{
+          int n=row++;
+          down[row]=down[n]|bit;
+          right[row]=(right[n]|bit)>>1;
+          left[row]=(left[n]|bit)<<1;
+          *pnStack++=bitmap;
+          bitmap=mask&~(down[row]|right[row]|left[row]);
+          continue;
         }
-        --row;
-        continue;
-      }
-      /* このビットをオフにして、再試行しないようにします */
-      bitmap&=~bit;
-      /* 結果を保存 */
-      aBoard[row]=bit;
-      /* 処理する行がまだあるか？ */
-      if(row<sizeE){
-        int n=row++;
-        down[row]=down[n]|bit;
-        right[row]=(right[n]|bit)>>1;
-        left[row]=(left[n]|bit)<<1;
-        *pnStack++=bitmap;
-        /* 同じ女王の位置を考慮することはできません
-           列、同じ正の対角線、または同じ負の対角線
-           すでにボード上のクイーン。
-           */
-        bitmap=mask&~(down[row]|right[row]|left[row]);
-        continue;
       }else{
-        /* 処理する行はもうありません。解決策が見つかりました。
-           ボードの位置としてソリューションを印刷するために、
-           printtableへの呼び出しをコメントアウトします
-           printtable（size、aBoard、TOTAL + 1）; */
-        //++TOTAL;
-        symmetryOps_bitmap(size); /* 対称解除法の追加 */
         bitmap=*--pnStack;
+        if(pnStack==aStack){ break ; }
         --row;
         continue;
       }
     }
   }
-  /* 鏡像をカウントするために、ソリューションを2倍します */
-  //TOTAL*=2;
 }
 //CPUR 再帰版　ロジックメソッド
-void NQueenR(int size,int mask,int row,int left,int down,int right){
+//void NQueenR(int size,int mask,int row,int left,int down,int right){
+void NQueenR(int size,int mask,int row,int left,int down,int right,int ex1,int ex2){
+  int odd=size&1;
   int bit;
-  int bitmap=mask&~(left|down|right);
+  int bitmap;
+  int bitmaps;
+  /** 偶数と奇数の分岐 */
+  // bitmap=(mask&~(left|down|right));
+  bitmap=(mask&~(left|down|right|ex1));
   if(row==size){
-    if(!bitmap){
-      aBoard[row]=bitmap;
-      symmetryOps_bitmap(size);
-    }
+    TOTAL++;
+    // symmetryOps_bitmap(size);
   }else{
     while(bitmap){
-      // bit=(-bitmap&bitmap);
-      // bitmap=(bitmap^bit);
       bitmap^=aBoard[row]=bit=(-bitmap&bitmap);
-      NQueenR(size,mask,row+1,(left|bit)<<1,down|bit,(right|bit)>>1);
+      //NQueenR(size,mask,row+1,(left|bit)<<1,down|bit,(right|bit)>>1);
+      NQueenR(size,mask,row+1,(left|bit)<<1,down|bit,(right|bit)>>1,ex2,0);
+      ex2=0;
     }
   }
 }
@@ -313,7 +267,7 @@ void NQueenR(int size,int mask,int row,int left,int down,int right){
 int main(int argc,char** argv){
   bool cpu=false,cpur=false;
   int argstart=2;
-  /** 起動パラメータの処理 */
+  /* 起動パラメータの処理 */
   if(argc>=2&&argv[1][0]=='-'){
     if(argv[1][1]=='c'||argv[1][1]=='C'){cpu=true;}
     else if(argv[1][1]=='r'||argv[1][1]=='R'){cpur=true;}
@@ -335,11 +289,13 @@ int main(int argc,char** argv){
   int min=4;
   int targetN=17;
   int mask;
+  int excl;
   for(int i=min;i<=targetN;i++){
-    //TOTAL=0; UNIQUE=0;
+    TOTAL=0; UNIQUE=0;
     COUNT2=COUNT4=COUNT8=0;
     mask=(1<<i)-1;
     st=clock();
+    excl=(1<<((i/2)^0))-1;
     if(cpu){
       //初期化は不要です
       /** 非再帰は-1で初期化 */
@@ -354,10 +310,12 @@ int main(int argc,char** argv){
       // for(int j=0;j<=targetN;j++){
       // 	aBoard[j]=j;
       // }
-      NQueenR(i,mask,0,0,0,0);
+      //奇数と偶数の判別
+      NQueenR(i,mask,0,0,0,0,excl,i%2 ? excl : 0);
     }
     TimeFormat(clock()-st,t);
-    printf("%2d:%13ld%16ld%s\n",i,getTotal(),getUnique(),t);
+    // printf("%2d:%13ld%16ld%s\n",i,getTotal(),getUnique(),t);
+    printf("%2d:%13ld%16ld%s\n",i,TOTAL*2,getUnique(),t);
   }
   return 0;
 }
