892f7a2c2276de721e203e835a360986b79d6a83
horiuchi <ani@horiuchinoiMac.local>
2021-03-05 15:54:43 +0900

    a

--- 07CUDA/GCC06_N-Queen.c
+++ 07CUDA/GCC06_N-Queen.c
@@ -1,52 +1,3 @@
-/**
- Cで学ぶアルゴリズムとデータ構造
- ステップバイステップでＮ−クイーン問題を最適化
- 一般社団法人  共同通信社  情報技術局  鈴木  維一郎(suzuki.iichiro@kyodonews.jp)
-
- 実行
- $ gcc -Wall -W -O3 -g -ftrapv -std=c99 GCC06.c && ./a.out [-c|-r]
-
-
-６．CPUR 再帰 バックトラック＋ビットマップ
- 
-
-bash-3.2$ gcc -Wall -W -O3 -g -ftrapv -std=c99 -pthread GCC06.c && ./a.out -r
-６．CPUR 再帰 バックトラック＋ビットマップ
- N:        Total       Unique        hh:mm:ss.ms
- 4:            2               0            0.00
- 5:           10               0            0.00
- 6:            4               0            0.00
- 7:           40               0            0.00
- 8:           92               0            0.00
- 9:          352               0            0.00
-10:          724               0            0.00
-11:         2680               0            0.00
-12:        14200               0            0.01
-13:        73712               0            0.04
-14:       365596               0            0.23
-15:      2279184               0            1.40
-16:     14772512               0            9.37
-17:     95815104               0         1:05.71
-
-
-bash-3.2$ gcc -Wall -W -O3 -g -ftrapv -std=c99 GCC06.c && ./a.out -c
-６．CPU 非再帰 バックトラック＋ビットマップ
- N:        Total       Unique        hh:mm:ss.ms
- 4:            2               0            0.00
- 5:           10               0            0.00
- 6:            4               0            0.00
- 7:           40               0            0.00
- 8:           92               0            0.00
- 9:          352               0            0.00
-10:          724               0            0.00
-11:         2680               0            0.00
-12:        14200               0            0.01
-13:        73712               0            0.04
-14:       365596               0            0.24
-15:      2279184               0            1.47
-16:     14772512               0            9.75
-17:     95815104               0         1:08.46
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -88,7 +39,15 @@ void TimeFormat(clock_t utime,char *form){
 }
 //
 //CPU 非再帰版 ロジックメソッド
-void solve_nqueen(int size,int mask, int row,int* left,int* down,int* right,int* bitmap){
+void solve_nqueen(int size,int mask, int row,int h_left,int h_down,int h_right){
+	unsigned int left[size];
+    unsigned int down[size];
+	unsigned int right[size];
+    unsigned int bitmap[size];
+	left[row]=h_left;
+	down[row]=h_down;
+	right[row]=h_right;
+	bitmap[row]=mask&~(left[row]|down[row]|right[row]);
     unsigned int bit;
     unsigned int sizeE=size-1;
     int mark=row;
@@ -117,32 +76,31 @@ void solve_nqueen(int size,int mask, int row,int* left,int* down,int* right,int*
 }
 void NQueen(int size,int mask){
   register int sizeE=size-1;
-  register int bitmap[size];
-  register int down[size],right[size],left[size];
   register int bit;
   if(size<=0||size>32){return;}
-  int row=0;
   bit=0;
-  bitmap[0]=mask;
-  down[0]=left[0]=right[0]=0;
+  //bitmap[0]=mask;
+  //down[0]=left[0]=right[0]=0;
   //偶数、奇数共通
   for(int col=0;col<size/2;col++){//右側半分だけクイーンを置く
     bit=(1<<col);//
-    down[1]=bit;//再帰の場合は down,left,right,bitmapは現在の行だけで良いが
-    left[1]=bit<<1;//非再帰の場合は全行情報を配列に入れて行の上がり下がりをする
-    right[1]=bit>>1;
-    bitmap[1]=mask&~(left[1]|down[1]|right[1]);
-    solve_nqueen(size,mask,1,left,down,right,bitmap);
+    //down[1]=bit;//再帰の場合は down,left,right,bitmapは現在の行だけで良いが
+    //eft[1]=bit<<1;//非再帰の場合は全行情報を配列に入れて行の上がり下がりをする
+    //right[1]=bit>>1;
+    //bitmap[1]=mask&~(left[1]|down[1]|right[1]);
+    //solve_nqueen(size,mask,1,left,down,right);
+    solve_nqueen(size,mask,1,bit<<1,bit,bit>>1);
   }
   TOTAL*=2;//ミラーなのでTOTALを２倍する
   //奇数の場合はさらに中央にクイーンを置く
   if(size%2==1){
     bit=(1<<(sizeE)/2);
-    down[1]=bit;
-    left[1]=bit<<1;
-    right[1]=bit>>1;
-    bitmap[1]=mask&~(left[1]|down[1]|right[1]);
-    solve_nqueen(size,mask,1,left,down,right,bitmap);
+    //down[1]=bit;
+    //left[1]=bit<<1;
+    //right[1]=bit>>1;
+    //bitmap[1]=mask&~(left[1]|down[1]|right[1]);
+    //solve_nqueen(size,mask,1,left,down,right);
+    solve_nqueen(size,mask,1,bit<<1,bit,bit>>1);
   }  
 }
 //
@@ -215,8 +173,10 @@ int main(int argc,char** argv) {
     printf("%s\n"," N:        Total       Unique        hh:mm:ss.ms");
     clock_t st;          //速度計測用
     char t[20];          //hh:mm:ss.msを格納
-    int min=4;
-    int targetN=17;
+    //int min=4;
+    //int targetN=17;
+    int min=7;
+    int targetN=7;
     int mask;
     for(int i=min;i<=targetN;i++){
       TOTAL=0;
