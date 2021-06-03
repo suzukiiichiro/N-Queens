5b956c909acbb10c02b94eb146d3ae17c1a60b86
堀内幸太郎 <user@horiuchikoutarou-no-MacBook.local>
2021-06-03 14:07:23 +0900

    a

--- q27/src/cpp/coronal2.cpp
+++ q27/src/cpp/coronal2.cpp
@@ -62,8 +62,10 @@ namespace {
 
   public:
     Explorer(bool const &full) : cnt(full? new uint64_t[4] : 0) {
-      for(Symmetry  s : Symmetry::RANGE)  pre[s] = 0;
-      for(Symmetry  s : Symmetry::RANGE)  cnt[s] = 0;
+      for(Symmetry  s : Symmetry::RANGE){  
+        pre[s] = 0;
+        cnt[s] = 0;
+      }
     }
     ~Explorer() {}
 
