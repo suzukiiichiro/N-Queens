8d6dc2808a840ed2800ffd51eaf3117ff8b7c77c
suzukiiichiro <suzuki.iichiro@gmail.com>
2023-05-02 13:46:15 +0900

    add

--- 08BashBit/07Bash_carryChain.sh
+++ 08BashBit/07Bash_carryChain.sh
@@ -27,14 +27,15 @@ declare -a pres_b;
 declare -a B_x;
 #
 : '';
-function solve_nqueenr()
+function solve()
 {
   local -i row="$1";
   local -i left="$2";
   local -i down="$3";
   local -i right="$4";
   if (( (down+1)==0 ));then 
-    return 1; 
+    #return 1; 
+    return ; 
   fi
   while (( (row&1)!=0 ));do
     ((row>>=1))
@@ -43,42 +44,45 @@ function solve_nqueenr()
   done
   ((row>>=1));
   local -i bit=0;
-  local -i bitmap=0;
-  for (( bitmap=~(left|down|right);bitmap!=0;bitmap^=bit ));do
-    (( bit=bitmap&-bitmap ));
-    #(( total+=solve_nqueenr "$row" "(left|bit)<<1" "(down|bit)" "(right|bit)>>1" )); 
-    solve_nqueenr "$row" "(left|bit)<<1" "(down|bit)" "(right|bit)>>1"  ; 
+  local -i bitmap=$(( ~(left|down|right) ));
+  while (( bitmap!=0 ));do
+    bit=$(( bitmap&-bitmap ));
+    bitmap=$(( bitmap^bit ));
+    solve "$row" "(left|bit)<<1" "(down|bit)" "(right|bit)>>1"  ; 
     total+=$?;
   done
+  return $total;
 }
 #
 : 'クイーンの効きをチェック';
-function board_placement()
+declare -i bpFlag=0; # 
+function placement()
 {
   local -i dimx="$1";
   local -i dimy="$2";
-  # read -p "dimx:$dimx dimy:$dimy";
-  local -i flag=1;
-  # echo "dimx:$dimx dimy:$dimy";
-  if (( B_x[dimx]==dimy ));then # 同じ場所に置くかチェック
-    # 同じ場所に置くのはOK
+  flag=0;       # グローバル変数
+  if (( B_x[$dimx]==$dimy ));then   # 同じ場所の配置を許す
     flag=1;
-    # return 1;
-    # read -p "flag1";
+    return $?; 
   fi
-  B_x[$dimx]="$dimy";             # dimxは行 dimyは列 
+  B_x[$dimx]="$dimy";               # dimxは行 dimyは列 
   row=$((1<<dimx));
   down=$((1<<dimy));
-  left=$((1<<(size-1-dimx+dimy))); # size-1-x+yは右上から左下 
-  right=$((1<<(dimx+dimy)));       # x+yは左上から右下
-  if (( (B["row"]&row)||(B["down"]&down)||(B["left"]&left)||(B["right"]&right) ));then
+  left=$((1<<(size-1-dimx+dimy)));  #右上から左下 
+  right=$((1<<(dimx+dimy)));        # x+yは左上から右下
+  if (( (B["row"]&row)||
+        (B["down"]&down)||
+        (B["left"]&left)||
+        (B["right"]&right) ));then
     flag=0;
-    return 0 ;
+    return $?;
   fi 
   B["row"]=$((B["row"]|row));
   B["down"]=$((B["down"]|down));
   B["left"]=$((B["left"]|left));
   B["right"]=$((B["right"]|right));
+  flag=1;
+
   [[ $flag -eq 0 ]]
   return $?;
 }
@@ -106,13 +110,12 @@ function carryChain()
   done
   #
   #
+  #
+  # 1 上２行にクイーンを置く 上１行は２分の１だけ実行
+  #
   # 90度回転
   # wB=( $B[@] );
   for i in ${!B[@]};do wB[$i]=${B[$i]} ; done
-
-  # 1
-  # 上２行にクイーンを置く
-  # 上１行は２分の１だけ実行
   # q=7なら (7/2)*(7-4)=12
   # 1行目は0,1,2で,2行目0,1,2,3,4,5,6 で
   # 利き筋を置かないと13パターンになる
@@ -134,16 +137,39 @@ function carryChain()
     #
     # 上２行　0行目,1行目にQを配置
     # ０行目にQを配置
-    board_placement "0" "$((pres_a[w]))" ;
-    # echo $?; # OK
-    # １行目にQを配置
-    board_placement "1" "$((pres_b[w]))" ;
-    # echo $?; # OK
+    local -i pna;
     #
+    # echo の場合
+    #pna=$( placement "0" "$((pres_a[w]))" );
+    #echo "pna: $bpFlag";
+    : ' pna:1
+        pna:1
+        pna:1
+        pna:1
+        pna:1 ';
+    #
+    # return で返却する場合
+    placement "0" "$((pres_a[w]))";
+    echo "pna: $?";
+    #
+    # グローバルフラグの場合
+    #placement "0" "$((pres_a[w]))";
+    # echo "pna: $bpFlag";
+    # １行目にQを配置
+    placement "1" "$((pres_b[w]))";
+    #echo "pna:$bpFlag";
+    : ' pna:1
+        pna:1
+        pna:1
+        pna:1
+        pna:1 ';
 
 
 
 
+    #
+    # ２ 左２列にクイーンを置く
+    #
     # 90度回転
     local -A nB=( 
       ["row"]="0"
@@ -153,21 +179,25 @@ function carryChain()
     );
     #nB=( ${B[@]} );
     for i in ${!B[@]};do nB[$i]=${B[$i]}; done
-    # ２ 左２列にクイーンを置く
     local -i n;
     for ((n=w;n<(size-2)*(size-1)-w;n++));do 
       #B=( $nB[@] );
       for i in ${!nB[@]};do B[$i]=${nB[$i]}; done
       #
       # Qを配置
-      board_placement "$((pres_a[n]))" "$((size-1))"; 
+      placement "$((pres_a[n]))" "$((size-1))"; 
+      # 0000011000000000001100011000000000001000
       if (( $?==0 ));then continue; fi
-      board_placement "$((pres_b[n]))" "$((size-2))";
+      # if (( bpFlag==0 ));then continue; fi
+      placement "$((pres_b[n]))" "$((size-2))";
       if (( $?==0 ));then continue; fi
+      # if (( bpFlag==0 ));then continue; fi
+
+
+
       #
-
-
-
+      # 3 下２行に置く
+      #
       # 90度回転
       local -A eB=( 
         ["row"]="0"
@@ -177,18 +207,21 @@ function carryChain()
       );
       #eB=( ${B[@]} );
       for i in ${!B[@]};do eB[$i]=${B[$i]}; done
-      # 3 下２行に置く
       local -i e;
       for ((e=w;e<(size-2)*(size-1)-w;e++));do 
         #B=( ${eB[@]} );
         for i in ${!eB[@]};do B[$i]=${eB[$i]}; done
-        board_placement "$((size-1))" "$((size-1-pres_a[e]))"; 
+        placement "$((size-1))" "$((size-1-pres_a[e]))"; 
         if (( $?==0 ));then continue; fi
-        board_placement "$((size-2))" "$((size-1-pres_b[e]))"; 
+        # if (( bpFlag==0 ));then continue; fi
+        placement "$((size-2))" "$((size-1-pres_b[e]))"; 
         if (( $?==0 ));then continue; fi
+        # if (( bpFlag==0 ));then continue; fi
 
 
 
+        #
+        # 4 右２列に置く
         #
         # 90度回転
         local -A sB=( 
@@ -200,17 +233,18 @@ function carryChain()
         #sB=( ${B[@]} );
         for i in ${!B[@]};do sB[$i]=${B[$i]}; done
         #
-        # 4 右２列に置く
         local -i s;
         for ((s=w;s<(size-2)*(size-1)-w;s++));do
           #
           # B=( ${sB[@]} );
           for i in ${!sB[@]};do B[$i]=${sB[$i]}; done
           #
-          board_placement "$((size-1-pres_a[s]))" "0";
+          placement "$((size-1-pres_a[s]))" "0";
           if (( $?==0 ));then continue; fi
-          board_placement "$((size-1-pres_b[s]))" "1"; 
+          # if (( bpFlag==0 ));then continue; fi
+          placement "$((size-1-pres_b[s]))" "1"; 
           if (( $?==0 ));then continue; fi
+          # if (( bpFlag==0 ));then continue; fi
 
 
 
@@ -234,8 +268,8 @@ function carryChain()
             # 上下左右２行２列配置完了
             # w=n=e=sであれば90度回転で同じ可能性
             # この場合はミラーの2
-            solve_nqueenr "$((B[row]>>2))" "$((B[left]>>4))" "$(( ((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1 ))" "$(( (B[right]>>4)<<($size-5) ))";
-            COUNT2+=$total; total=0;
+            solve "$((B[row]>>2))" "$((B[left]>>4))" "$(( ((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1 ))" "$(( (B[right]>>4)<<($size-5) ))";
+            COUNT2+=$?; total=0;
             continue;
           fi
           # e==wは180度回転して同じ
@@ -244,12 +278,12 @@ function carryChain()
             if((n>s));then continue; fi
             # この場合は4
             # 上下左右２行２列配置完了
-            solve_nqueenr "B[row]>>2" "B[left]>>4" "((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1" "(B[right]>>4)<<($size-5)";
+            solve "B[row]>>2" "B[left]>>4" "((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1" "(B[right]>>4)<<($size-5)";
             COUNT4+=$total;total=0;
             continue;
           fi
           # 上下左右２行２列配置完了"
-          solve_nqueenr "B[row]>>2" "B[left]>>4" "((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1" "(B[right]>>4)<<($size-5)";
+          solve "B[row]>>2" "B[left]>>4" "((((B[down]>>2)|(~0<<($size-4)))+1)<<($size-5))-1" "(B[right]>>4)<<($size-5)";
           COUNT8+=$total;total=0;
           continue;
         done
