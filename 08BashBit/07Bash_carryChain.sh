d4d874dea9091702884d37af4ddf29d416aef8c0
suzukiiichiro <suzuki.iichiro@gmail.com>
2023-05-08 14:41:43 +0900

    add

--- 08BashBit/07Bash_carryChain.sh
+++ 08BashBit/07Bash_carryChain.sh
@@ -62,52 +62,45 @@ function solve()
 }
 #
 : 'クイーンの効きをチェック';
-declare -i bpFlag=0; # 
 function placement()
 {
   local -i dimx="$1";
   local -i dimy="$2";
-  local -i flag=0;       
-  if (( B_x[$dimx]==$dimy ));then   # 同じ場所の配置を許す
-    flag=1;
-    return $?; 
+  # local -i flag=0;       
+  if (( B_x["$dimx"]=="$dimy" ));then   # 同じ場所の配置を許す
+    # flag=1;
+    # return $?; 
+    return 1;
   fi
-  B_x[$dimx]="$dimy";               # dimxは行 dimyは列 
-  row=$((1<<dimx));
-  down=$((1<<dimy));
-  left=$((1<<(size-1-dimx+dimy)));  #右上から左下 
-  right=$((1<<(dimx+dimy)));        # x+yは左上から右下
-  if (( (B[row]&row)||
-        (B[down]&down)||
-        (B[left]&left)||
-        (B[right]&right) ));then
-    flag=0;
-    return $?;
-  fi 
-  # 連想配列への代入方法が違う可能性がある。
-  : '
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  B.row:
-  ';
-   B[row]=$((B[row]|row));
-  # B=( [row]=$((B[row]|row)) );
-  B[down]=$((B[down]|down));
-  B[left]=$((B[left]|left));
-  B[right]=$((B[right]|right));
+  B_x["$dimx"]="$dimy";               # dimxは行 dimyは列 
+  row="$((1<<dimx))";
+  down="$((1<<dimy))";
+  left="$((1<<(size-1-dimx+dimy)))";  #右上から左下 
+  right="$((1<<(dimx+dimy)))";        # x+yは左上から右下
   # 確認
   # for key_B in "${!B[@]}";do
   #   echo "B.$key_B:${B[$key_B]}";
   # done
-  flag=1;
-  [[ $flag -eq 0 ]]
-  return $?;
+  if (( (B["row"] & $row)||
+        (B["down"] & $down)||
+        (B["left"] & $left)||
+        (B["right"] & $right) ));then
+    # flag=0;
+    # return $?;
+    return 0;
+  fi 
+  B["row"]=$((B[row]|row));
+  B["down"]=$((B[down]|down));
+  B["left"]=$((B[left]|left));
+  B["right"]=$((B[right]|right));
+  # 確認
+  # for key_B in "${!B[@]}";do
+  #   echo "B.$key_B:${B[$key_B]}";
+  # done
+  # flag=1;
+  # [[ $flag -eq 0 ]]
+  # return $?;
+  return 1;
 }
 : 'キャリーチェーン';
 function carryChain()
@@ -240,19 +233,15 @@ function carryChain()
       #
       # Qを配置
       placement "$((pres_a[n]))" "$((size-1))"; 
-      #echo -n "$?";
+      echo -n "$?";
       : 'Cの結果
       0000011000000000001100011000000000001000
          bashの結果
-      0000010000000000001000000000000000000000
+      0110111010110101011101011010100101011010
       ';
-      if (( $?==1 ));then continue; fi
-      # if (( bpFlag==0 ));then continue; fi
+      if (( $?==0 ));then continue; fi
       placement "$((pres_b[n]))" "$((size-2))";
       if (( $?==0 ));then continue; fi
-      # if (( bpFlag==0 ));then continue; fi
-
-
       #
       # 3 下２行に置く
       #
@@ -266,43 +255,44 @@ function carryChain()
       # for key_eB in ${!eB[@]};do 
       #   echo "eB.$key_eB:${eB["$key_eB"]}";
       # done
-
-
       for ((e=w;e<(size-2)*(size-1)-w;e++));do 
         #B=( ${eB[@]} );
-        for i in ${!eB[@]};do B[$i]=${eB[$i]}; done
+        for key_eB in ${!eB[@]};do 
+          B["$key_eB"]="${eB[$key_eB]}"; 
+        done
+        # 確認
+        # for key_eB in ${!B[@]};do 
+        #   echo "B.key_eB:${B[$key_eB]}"; 
+        # done
         placement "$((size-1))" "$((size-1-pres_a[e]))"; 
         if (( $?==0 ));then continue; fi
-echo "HOE";
-        # if (( bpFlag==0 ));then continue; fi
         placement "$((size-2))" "$((size-1-pres_b[e]))"; 
         if (( $?==0 ));then continue; fi
-        # if (( bpFlag==0 ));then continue; fi
-
-
         #
         # 4 右２列に置く
         #
         # 90度回転
-        local -A sB=( ["row"]="0" ["left"]="0" ["down"]="0" ["right"]="0");
         #sB=( ${B[@]} );
-        for i in ${!B[@]};do sB[$i]=${B[$i]}; done
-        #
-        local -i s;
+        for key_B in ${!B[@]};do 
+          sB["$key_B"]="${B[$key_B]}"; 
+        done
+        # 確認
+        # for key_sB in ${!sB[@]};do 
+        #   echo "${sB["$key_sB"]}"; 
+        # done
         for ((s=w;s<(size-2)*(size-1)-w;s++));do
-          #
           # B=( ${sB[@]} );
-          local -i i;
-          for i in ${!sB[@]};do B[$i]=${sB[$i]}; done
-          #
+          for key_sB in ${!sB[@]};do 
+            B["$key_sB"]="${sB[$key_sB]}"; 
+          done
+          # 確認
+          # for key_B in ${!B[@]};do 
+          #   echo "${B["$key_B"]}"; 
+          # done
           placement "$((size-1-pres_a[s]))" "0";
           if (( $?==0 ));then continue; fi
-          # if (( bpFlag==0 ));then continue; fi
           placement "$((size-1-pres_b[s]))" "1"; 
           if (( $?==0 ));then continue; fi
-          # if (( bpFlag==0 ));then continue; fi
-
-
           #
           # 対象解除法
           #
