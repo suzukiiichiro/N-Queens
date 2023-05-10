2c481d809739551bbd71a469cd42b36b8197c007
suzukiiichiro <suzuki.iichiro@gmail.com>
2023-05-10 15:58:16 +0900

    add

--- 08BashBit/07Bash_carryChain.sh
+++ 08BashBit/07Bash_carryChain.sh
@@ -8,23 +8,18 @@ declare -i COUNT4=0;
 declare -i COUNT8=0;
 declare -a pres_a;
 declare -a pres_b;
-declare -A x;
-declare -A B;
-declare -i w;
-declare -i n;
-declare -i e;
-declare -i s;
+declare -A B; # B=(row left down right X[@])
+declare -A X; # dimx=(0 0 0 0 0)
 #
 : 'ボード外側２列を除く内側のクイーン配置処理';
 function solve()
 {
-  local -i total=0;
   local -i row="$1";
   local -i left="$2";
   local -i down="$3";
   local -i right="$4";
   # 配置完了の確認 # bh=-1 1111111111 すべての列にクイーンを置けると-1になる
-  if (( (down+1)==0 ));then return 1; fi
+  (( (down+1)==0 ))&& return 1;
   # 新たなQを配置 colは置き換える。row 右端にクイーンがすでに置かれていたらクイー
   # ンを置かずに１行下に移動する。rowを右端から１ビットずつ削っていく。ここでは
   # rowはすでにクイーンが置かれているかどうかだけで使う
@@ -36,6 +31,7 @@ function solve()
   (( row>>=1 ));      # １行下に移動する
   local -i bit=0;
   local -i bitmap=0;
+  local -i total=0;
   for (( bitmap=~(left|down|right);bitmap!=0;bitmap^=bit ));do
     (( bit=bitmap&-bitmap ));
     solve "$row" "$(( (left|bit)<<1 ))" "$(( (down|bit) ))" "$(( (right|bit)>>1 ))"  ; 
@@ -57,10 +53,10 @@ function solveQueen()
 : 'クイーンの効きをチェック';
 function placement()
 {
-  local -i dimx="$1"; # dimxは行 dimyは列
+  local -i dimx="$1";     # dimxは行 dimyは列
   local -i dimy="$2";
   local -a t_x=(${B[x]}); # 同じ場所の配置を許す
-  if (( t_x[dimx]==dimy ));then return 1; fi
+  (( t_x[dimx]==dimy ))&& return 1;
   t_x[$dimx]="$dimy" B[x]=${t_x[@]}; # Bに反映  
   if (( (B[row] & 1<<dimx)||
         (B[down] & 1<<dimy)||
@@ -82,29 +78,29 @@ function carryChainSymmetry()
   local -i ww=$(( (size-2)*(size-1)-1-w ));
   local -i w2=$(( (size-2)*(size-1)-1 ));
   # 対角線上の反転が小さいかどうか確認する
-  if (( (s==ww)&&(n<(w2-e)) ));then return; fi
+  (( (s==ww)&&(n<(w2-e)) ))&& return;
   # 垂直方向の中心に対する反転が小さいかを確認
-  if (( (e==ww)&&(n>(w2-n)) ));then return; fi
+  (( (e==ww)&&(n>(w2-n)) ))&& return;
   # 斜め下方向への反転が小さいかをチェックする
-  if (( (n==ww)&&(e>(w2-s)) ));then return; fi
+  (( (n==ww)&&(e>(w2-s)) ))&& return ;
   # n,e,s==w の場合は最小値を確認する。
-  if ((s==w));then
-    # : '右回転で同じ場合は、
-    # w=n=e=sでなければ値が小さいのでskip
-    # w=n=e=sであれば90度回転で同じ可能性 ';
-    if(( (n!=w)||(e!=w) ));then return; fi
+  # : '右回転で同じ場合は、
+  # w=n=e=sでなければ値が小さいのでskip
+  # w=n=e=sであれば90度回転で同じ可能性 ';
+  ((s==w))&&{
+    (( (n!=w)||(e!=w) ))&& return;
     solveQueen;
     COUNT2+=$?; 
     return ;
-  fi
+  }
   # : 'e==wは180度回転して同じ
   # 180度回転して同じ時n>=sの時はsmaller?  ';
-  if (( (e==w)&&(n>=s) ));then
-    if((n>s));then return;  fi
+  (( (e==w)&&(n>=s) ))&&{
+    ((n>s))&& return ;
     solveQueen;
     COUNT4+=$?;
     return ;
-  fi
+  }
   solveQueen;
   COUNT8+=$?;
   return ;
@@ -114,76 +110,60 @@ function carryChainSymmetry()
 function buildChain()
 {
   # Bの初期化
-  B=(["row"]="0" ["down"]="0" ["left"]="0" ["right"]="0" ["x"]=${x[@]});
+  B=(["row"]="0" ["down"]="0" ["left"]="0" ["right"]="0" ["x"]=${X[@]});
   #
   # １ 上２行にクイーンを置く 上１行は２分の１だけ実行 90度回転
-  #--------------
   # wB=( $B[@] );
   local -A wB=B; # bashの連想配列は↓が必要
   for key_B in ${!B[@]};do wB["$key_B"]="${B[$key_B]}" ; done
-  #--------------
   for ((w=0;w<=(size/2)*(size-3);w++));do
-    #--------------
     # B=( $wB[@] );
     B=wB;  # bashの連想配列は↓が必要
     for key_wB in ${!wB[@]};do B["$key_wB"]="${wB[$key_wB]}" ; done
-    #--------------
-    for ((bx_i=0;bx_i<size;bx_i++));do x[$bx_i]=-1; done
-    B=(["row"]="0" ["down"]="0" ["left"]="0" ["right"]="0" ["x"]=${x[@]});
+    for ((bx_i=0;bx_i<size;bx_i++));do X[$bx_i]=-1; done
+    B=(["row"]="0" ["down"]="0" ["left"]="0" ["right"]="0" ["x"]=${X[@]});
     placement "0" "$((pres_a[w]))"; # １　０行目と１行目にクイーンを配置
     placement "1" "$((pres_b[w]))";
     #
     # ２ 90度回転
-    #--------------
     # nB=( ${B[@]} );
     local -A nB=B;  # bashの連想配列は↓が必要
     for key_B in "${!B[@]}";do nB["$key_B"]="${B[$key_B]}"; done
-    #--------------
     local -i mirror=$(( (size-2)*(size-1)-w ));
     for ((n=w;n<mirror;n++));do 
-      #--------------
       # B=( $nB[@] );
       B=nB;  # bashの連想配列は↓が必要
       for key_nB in ${!nB[@]};do B["$key_nB"]="${nB[$key_nB]}"; done
-      #--------------
       placement "$((pres_a[n]))" "$((size-1))"; 
-      if (( $?==0 ));then continue; fi
+      [[ $? -eq 0 ]] && continue;
       placement "$((pres_b[n]))" "$((size-2))";
-      if (( $?==0 ));then continue; fi
+      [[ $? -eq 0 ]] && continue;
       #
       # ３ 90度回転
-      #--------------
       # eB=( ${B[@]} );
       local -A eB=B;  # bashの連想配列は↓が必要
       for key_B in ${!B[@]};do eB["$key_B"]="${B[$key_B]}"; done
-      #--------------
       for ((e=w;e<mirror;e++));do 
-        #--------------
         #B=( ${eB[@]} );
         B=eB; # bashの連想配列は↓が必要
         for key_eB in ${!eB[@]};do B["$key_eB"]="${eB[$key_eB]}"; done
-        #--------------
         placement "$((size-1))" "$((size-1-pres_a[e]))"; 
-        if (( $?==0 ));then continue; fi
+        [[ $? -eq 0 ]] && continue;
         placement "$((size-2))" "$((size-1-pres_b[e]))"; 
-        if (( $?==0 ));then continue; fi
+        [[ $? -eq 0 ]] && continue;
         #
         # ４ 90度回転
-        #--------------
         #sB=( ${B[@]} );
         local -A sB=B; # bashの連想配列は↓が必要
         for key_B in ${!B[@]};do sB["$key_B"]="${B[$key_B]}"; done
-        #--------------
         for ((s=w;s<mirror;s++));do
-          #--------------
           #B=( ${sB[@]} );
           B=sB; # bashの連想配列は↓が必要
           for key_sB in ${!sB[@]};do B["$key_sB"]="${sB[$key_sB]}"; done
-          #--------------
           placement "$((size-1-pres_a[s]))" "0";
-          if (( $?==0 ));then continue; fi
+          [[ $? -eq 0 ]] && continue;
           placement "$((size-1-pres_b[s]))" "1"; 
-          if (( $?==0 ));then continue; fi
+          [[ $? -eq 0 ]] && continue;
           #
           #  対象解除法
           carryChainSymmetry; 
@@ -198,8 +178,7 @@ function buildChain()
 function initChain()
 {
   local -i idx=0;
-  local -i a=0;
-  local -i b=0;
+  local -i a=b=0;
   for ((a=0;a<size;a++));do
     for ((b=0;b<size;b++));do
       if (( ( (a>=b)&&((a-b)<=1) )||
