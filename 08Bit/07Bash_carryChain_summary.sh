126a2093e3b147f389e4b974154545336bc16797
suzukiiichiro <suzuki.iichiro@gmail.com>
2023-05-23 16:49:36 +0900

    add

--- 08Bit/07Bash_carryChain_summary.sh
+++ 08Bit/07Bash_carryChain_summary.sh
@@ -204,6 +204,125 @@ declare -a B;
 
 #
 : 'ボードレイアウトを出力 ビットマップ対応版';
+function printRecordCarryChain()
+{
+  local -a board=(${B[4]}); # 同じ場所の配置を許す
+  ((TOTAL++));
+  size="$1";
+  flag="$2"; # bitmap版は1 それ以外は 0
+  echo "$TOTAL";
+  sEcho=" ";  
+  : 'ビットマップ版
+     ビットマップ版からは、左から数えます
+     上下反転左右対称なので、これまでの上から数える手法と
+     rowを下にたどって左から数える方法と解の数に変わりはありません。
+     0 2 4 1 3 
+    +-+-+-+-+-+
+    |O| | | | | 0
+    +-+-+-+-+-+
+    | | |O| | | 2
+    +-+-+-+-+-+
+    | | | | |O| 4
+    +-+-+-+-+-+
+    | |O| | | | 1
+    +-+-+-+-+-+
+    | | | |O| | 3
+    +-+-+-+-+-+
+  ';
+  if ((flag));then
+    local -i i=0;
+    local -i j=0;
+    for ((i=0;i<size;i++));do
+      for ((j=0;j<size;j++));do
+       if (( board[i]&1<<j ));then
+          sEcho="${sEcho}$((j)) ";
+       fi 
+      done
+    done
+  else 
+  : 'ビットマップ版以外
+     (ブルートフォース、バックトラック、配置フラグ)
+     上から数えます
+     0 2 4 1 3 
+    +-+-+-+-+-+
+    |O| | | | |
+    +-+-+-+-+-+
+    | | | |O| |
+    +-+-+-+-+-+
+    | |O| | | |
+    +-+-+-+-+-+
+    | | | | |O|
+    +-+-+-+-+-+
+    | | |O| | |
+    +-+-+-+-+-+
+     ';
+    local -i i=0;
+    for((i=0;i<size;i++)){
+      sEcho="${sEcho}${board[i]} ";
+    }
+  fi
+  echo "$sEcho";
+  echo -n "+";
+  local -i i=0;
+  for((i=0;i<size;i++)){
+    echo -n "-";
+    if((i<(size-1)));then
+      echo -n "+";
+    fi
+  }
+  echo "+";
+  local -i i=0;
+  local -i j=0;
+  for((i=0;i<size;i++)){
+    echo -n "|";
+    for((j=0;j<size;j++)){
+      if ((flag));then
+        if(( board[i]!=-1));then
+          if (( board[i]&1<<j ));then
+            echo -n "Q";
+          else
+            echo -n " ";
+          fi
+        else
+          echo -n " ";
+        fi
+      else
+        if((i==board[j]));then
+          echo -n "Q";
+        else
+          echo -n " ";
+        fi
+      fi
+      if((j<(size-1)));then
+        echo -n "|";
+      fi
+    }
+  echo "|";
+  if((i<(size-1)));then
+    echo -n "+";
+    local -i j=0;
+    for((j=0;j<size;j++)){
+      echo -n "-";
+      if((j<(size-1)));then
+        echo -n "+";
+      fi
+    }
+  echo "+";
+  fi
+  }
+  echo -n "+";
+  local -i i=0;
+  for((i=0;i<size;i++)){
+    echo -n "-";
+    if((i<(size-1)));then
+      echo -n "+";
+    fi
+  }  
+  echo "+";
+  echo "";
+}
+#
+: 'ボードレイアウトを出力 ビットマップ対応版';
 function printRecord()
 {
   ((TOTAL++));
@@ -522,7 +641,8 @@ function carryChainSymmetry()
     process "$size" "2";  #COUNT8
     #
     # ボードレイアウト出力 # 出力 1:bitmap版 0:それ以外
-    ((DISPLAY==1))&& printRecordCarryChain "$size" "1";
+    ((DISPLAY==1))&& { printRecordCarryChain "$size" "0";
+    read -p ""; }
     return;
   }
   # n,e,s==w の場合は最小値を確認する。
@@ -533,7 +653,8 @@ function carryChainSymmetry()
     (( (n!=w)||(e!=w) ))&& return;
     process "$size" "0" # COUNT2
     # ボードレイアウト出力 # 出力 1:bitmap版 0:それ以外
-    ((DISPLAY==1))&& printRecordCarryChain "$size" "1";
+    ((DISPLAY==1))&& { printRecordCarryChain "$size" "0";
+    read -p ""; }
     return ;
   }
   # : 'e==wは180度回転して同じ
@@ -542,12 +663,14 @@ function carryChainSymmetry()
     ((n>s))&& return ;
     process "$size" "1" # COUNT4
     # ボードレイアウト出力 # 出力 1:bitmap版 0:それ以外
-    ((DISPLAY==1))&& printRecordCarryChain "$size" "1";
+    ((DISPLAY==1))&& { printRecordCarryChain "$size" "0";
+    read -p ""; }
     return ;
   }
   process "$size" "2" ; #COUNT8
   # ボードレイアウト出力 # 出力 1:bitmap版 0:それ以外
-  ((DISPLAY==1))&& printRecordCarryChain "$size" "1";
+  ((DISPLAY==1))&& { printRecordCarryChain "$size" "0";
+  read -p ""; }
   return ;
 }
 #
@@ -565,10 +688,16 @@ function buildChain()
   for ((w=0;w<=(size/2)*(size-3);++w));do
     B=("${wB[@]}");
     # Bの初期化 #0:row 1:left 2:down 3:right 4:dimx
-    #for ((bx_i=0;bx_i<size;bx_i++));do X[$bx_i]=-1; done
+    #for ((bx_i=0;bx_i<size;bx_i++));do 
+    #  X[$bx_i]=-1; 
+    #  board[$bx_i]=-1;
+    #done
     # i++ よりも ++i のほうが断然高速です。
-    for ((bx_i=0;bx_i<size;++bx_i));do X[$bx_i]=-1; done
-    B=([0]=0 [1]=0 [2]=0 [3]=0 [4]=${X[@]});
+    for ((bx_i=0;bx_i<size;++bx_i));do 
+      X[$bx_i]=-1; 
+      board[$bx_i]=-1;
+    done
+    B=([0]=0 [1]=0 [2]=0 [3]=0 [4]=${X[@]} [5]=${board[@]});
     placement "$size" "0" "$((pres_a[w]))"; # １　０行目と１行目にクイーンを配置
     [[ $? -eq 0 ]] && continue;
     placement "$size" "1" "$((pres_b[w]))";
@@ -641,11 +770,6 @@ function initChain()
 function carryChain()
 {
   local -i size="$1";
-  # 初期化
-  pres_a=pres_b=0;
-  TOTAL=UNIQUE=0;
-  COUNTER[0]=COUNTER[1]=COUNTER[2]=0;
-  # 処理
   initChain "$size";  # チェーンの初期化
   buildChain "$size"; # チェーンのビルド
   # 集計
@@ -1414,8 +1538,8 @@ function bluteForce_R()
 function NQ()
 {
   local selectName="$1";
-  local -i max=17;
   local -i min=4;
+  local -i max=17;
   local -i N="$min";
   local -i mask=0;
   local -i bit=0
@@ -1431,6 +1555,7 @@ function NQ()
     COUNT2=COUNT4=COUNT8=0;
     MASK=SIDEMASK=LASTMASK=0;
     TOPBIT=ENDBIT=BOUND1=BOUND2=0;
+    COUNTER[0]=COUNTER[1]=COUNTER[2]=0;    # カウンター配列
     mask=$(( (1<<N)-1 ));
     startTime=$(date +%s);# 計測開始時間
 
@@ -1486,7 +1611,8 @@ case "$selectNo" in
       echo; 
       case "$select" in
         y|Y) NQ carryChain; break; ;;
-        n|N) NQ carryChain_NR; break; ;;
+        #n|N) NQ carryChain_NR; break; ;;
+        n|N) NQ carryChain; break; ;;
       esac
     done
     ;;
