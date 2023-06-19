b46e936e6af0a19bfe6fa7865f21b4b7da3ad88a
suzukiiichiro <suzuki.iichiro@gmail.com>
2023-06-19 15:45:43 +0900

    add

--- 10Bit_Python/10Python_class.py
+++ 10Bit_Python/10Python_class.py
@@ -53,9 +53,6 @@ class nQueens(): # pylint:disable=RO902
   def __init__(self,size): # pylint:disable=R0913
     super(nQueens,self).__init__()
     self.size=size
-    self.B=Board(size)
-    self.pres_a=[0]*930
-    self.pres_b=[0]*930
     self.COUNTER=[0]*3
     self.carryChain()
   #
@@ -77,11 +74,11 @@ class nQueens(): # pylint:disable=RO902
     return total
   #
   # キャリーチェーン　solve()を呼び出して再起を開始する
-  def process(self,sym):
-    self.COUNTER[sym]+=self.solve(self.B.row>>2,self.B.left>>4,((((self.B.down>>2)|(~0<<(self.size-4)))+1)<<(self.size-5))-1,(self.B.right>>4)<<(self.size-5))
+  def process(self,B,sym):
+    self.COUNTER[sym]+=self.solve(B.row>>2,B.left>>4,((((B.down>>2)|(~0<<(self.size-4)))+1)<<(self.size-5))-1,(B.right>>4)<<(self.size-5))
   #
   # キャリーチェーン　対象解除
-  def carryChainSymmetry(self,n,w,s,e):
+  def carryChainSymmetry(self,B,n,w,s,e):
     # n,e,s=(N-2)*(N-1)-1-w の場合は最小値を確認する。
     ww=(self.size-2)*(self.size-1)-1-w
     w2=(self.size-2)*(self.size-1)-1
@@ -93,8 +90,8 @@ class nQueens(): # pylint:disable=RO902
     if n==ww and e>(w2-s): return
     # 【枝刈り】１行目が角の場合
     # １．回転対称チェックせずにCOUNT8にする
-    if not self.B.X[0]:
-      self.process(2) # COUNT8
+    if not B.X[0]:
+      self.process(B,2) # COUNT8
       return
     # n,e,s==w の場合は最小値を確認する。
     # : '右回転で同じ場合は、
@@ -102,114 +99,100 @@ class nQueens(): # pylint:disable=RO902
     # w=n=e=sであれば90度回転で同じ可能性 ';
     if s==w:
       if n!=w or e!=w: return
-      self.process(0) # COUNT2
+      self.process(B,0) # COUNT2
       return
     # : 'e==wは180度回転して同じ
     # 180度回転して同じ時n>=sの時はsmaller?  ';
     if e==w and n>=s:
       if n>s: return
-      self.process(1) # COUNT4
+      self.process(B,1) # COUNT4
       return
-    self.process(2)   # COUNT8
+    self.process(B,2)   # COUNT8
     return
   #
   # キャリーチェーン 効きのチェック dimxは行 dimyは列
-  def placement(self,dimx,dimy):
-    if self.B.X[dimx]==dimy:
+  def placement(self,B,dimx,dimy):
+    if B.X[dimx]==dimy:
       return 1
-    if self.B.X[0]:
-      if self.B.X[0]!=-1:
-        if(
-          (dimx<self.B.X[0] or dimx>=self.size-self.B.X[0]) and 
-          (dimy==0 or dimy==self.size-1) 
-        ):
-          return 0
-        if(
-          (dimx==self.size-1) and 
-          (dimy<=self.B.X[0] or dimy>=self.size-self.B.X[0])
-        ):
-          return 0
+    if B.X[0]:
+      if B.X[0]!=-1:
+        if((dimx<B.X[0] or dimx>=self.size-B.X[0]) and 
+          (dimy==0 or dimy==self.size-1)): return 0
+        if((dimx==self.size-1) and 
+          (dimy<=B.X[0] or dimy>=self.size-B.X[0])):return 0
     else:
-      if self.B.X[1]!=-1:
-        if self.B.X[1]>=dimx and dimy==1:
-          return 0
-    if( (self.B.row & 1<<dimx) or 
-        (self.B.left & 1<<(self.size-1-dimx+dimy)) or
-        (self.B.down & 1<<dimy) or
-        (self.B.right & 1<<(dimx+dimy)) 
-    ): 
-      return 0
-    self.B.row|=1<<dimx
-    self.B.left|=1<<(self.size-1-dimx+dimy)
-    self.B.down|=1<<dimy
-    self.B.right|=1<<(dimx+dimy)
-    self.B.X[dimx]=dimy
+      if B.X[1]!=-1:
+        if B.X[1]>=dimx and dimy==1: return 0
+    if( (B.row & 1<<dimx) or 
+        (B.left & 1<<(self.size-1-dimx+dimy)) or
+        (B.down & 1<<dimy) or
+        (B.right & 1<<(dimx+dimy))): return 0
+    B.row|=1<<dimx
+    B.left|=1<<(self.size-1-dimx+dimy)
+    B.down|=1<<dimy
+    B.right|=1<<(dimx+dimy)
+    B.X[dimx]=dimy
     return 1
   #
   # チェーンのビルド
-  def buildChain(self):
-    # global B
-    # global pres_a
-    # global pres_b
-
-    # B=np.array([0,0,0,0,[-1]*size]) # Bの初期化
-    #wB=sB=eB=nB=np.array([[0 for i i,pres_a,pres_en range(size)] for j in range(size)])
-    wB=sB=eB=nB=copy.deepcopy(self.B)
-    # wB=np.array(copy.deepcopy(self.B))
+  def buildChain(self,pres_a,pres_b):
+    #
+    # Boardクラスのインスタンス
+    B=Board(self.size)
+    # 
+    wB=copy.deepcopy(B)
     for w in range( (self.size//2)*(self.size-3) +1):
-      #self.B=np.array(wB.copy())
-      self.B=copy.deepcopy(wB)
-      #B=np.array([0,0,0,0,[-1]*size]) # Bの初期化
-      # B=[[0 for i in range(size)] for j in range(size)]
-      # B[4]=[-1]*size       # X を -1 でsize分を初期化
+      B=copy.deepcopy(wB)
       # １．０行目と１行目にクイーンを配置
-      if self.placement(0,self.pres_a[w])==0:
+      if self.placement(B,0,pres_a[w])==0:
         continue
-      if self.placement(1,self.pres_b[w])==0:
+      if self.placement(B,1,pres_b[w])==0:
         continue
       # ２．９０度回転
-      nB=copy.deepcopy(self.B)
+      nB=copy.deepcopy(B)
       mirror=(self.size-2)*(self.size-1)-w
       for n in range(w,mirror,1):
-        self.B=copy.deepcopy(nB)
-        if self.placement(self.pres_a[n],self.size-1)==0:
+        B=copy.deepcopy(nB)
+        if self.placement(B,pres_a[n],self.size-1)==0:
           continue
-        if self.placement(self.pres_b[n],self.size-2)==0:
+        if self.placement(B,pres_b[n],self.size-2)==0:
           continue
         # ３．９０度回転
-        eB=copy.deepcopy(self.B)
+        eB=copy.deepcopy(B)
         for e in range(w,mirror,1):
-          self.B=copy.deepcopy(eB)
-          if self.placement(self.size-1,self.size-1-self.pres_a[e])==0:
+          B=copy.deepcopy(eB)
+          if self.placement(B,self.size-1,self.size-1-pres_a[e])==0:
             continue
-          if self.placement(self.size-2,self.size-1-self.pres_b[e])==0:
+          if self.placement(B,self.size-2,self.size-1-pres_b[e])==0:
             continue
           # ４．９０度回転
-          # sB=np.array(copy.deepcopy(self.B))
+          sB=copy.deepcopy(B)
           for s in range(w,mirror,1):
-            self.B=copy.deepcopy(sB)
-            if self.placement(self.size-1-self.pres_a[s],0)==0:
+            B=copy.deepcopy(sB)
+            if self.placement(B,self.size-1-pres_a[s],0)==0:
               continue
-            if self.placement(self.size-1-self.pres_b[s],1)==0:
+            if self.placement(B,self.size-1-pres_b[s],1)==0:
               continue
             # 対象解除法
-            self.carryChainSymmetry(n,w,s,e)
+            self.carryChainSymmetry(B,n,w,s,e)
   #
   # チェーンの初期化
-  def initChain(self):
+  def initChain(self,pres_a,pres_b):
     idx=0
     for a in range(self.size):
       for b in range(self.size):
         if (a>=b and (a-b)<=1) or (b>a and (b-a<=1)):
           continue
-        self.pres_a[idx]=a
-        self.pres_b[idx]=b
+        pres_a[idx]=a
+        pres_b[idx]=b
         idx+=1
   #
   # キャリーチェーン
   def carryChain(self):
-    self.initChain()     # チェーンの初期化
-    self.buildChain()    # チェーンのビルド
+    pres_a=[0]*930
+    pres_b=[0]*930
+    self.initChain(pres_a,pres_b)     # チェーンの初期化
+    self.buildChain(pres_a,pres_b)    # チェーンのビルド
   #
   # ユニーク数の集計
   def getUnique(self):
@@ -222,8 +205,8 @@ class nQueens(): # pylint:disable=RO902
 # メイン
 def main():
   nmin = 5
-  nmax = 6
-  print("キャリーチェーン シングルスレッド")
+  nmax = 21
+  print("キャリーチェーン クラスの導入")
   print(" N:        Total       Unique        hh:mm:ss.ms")
   for size in range(nmin, nmax,1):
     start_time = datetime.now()
