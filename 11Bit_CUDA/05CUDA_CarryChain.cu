473011372318d9bf91d0dbaec115b766b0a215de
堀内幸太郎 <ani@horiuchikoutarounoMacBook-puro.local>
2023-07-27 10:16:51 +0900

    a

--- 11Bit_CUDA/05CUDA_CarryChain.cu
+++ 11Bit_CUDA/05CUDA_CarryChain.cu
@@ -84,24 +84,21 @@ typedef struct
 /**
   CPU 
 */
-// 再帰 ボード外側２列を除く内側のクイーン配置処理
-uint64_t solve(uint64_t row,uint64_t left,uint64_t down,uint64_t right)
+//再帰、非再帰共通
+// チェーンのリストを作成
+void listChain()
 {
-  if(down+1==0){ return  1; }
-  while((row&1)!=0) { 
-    row>>=1;
-    left<<=1;
-    right>>=1;
+  unsigned int idx=0;
+  for(unsigned int a=0;a<(unsigned)g.size;++a){
+    for(unsigned int b=0;b<(unsigned)g.size;++b){
+      if(((a>=b)&&(a-b)<=1)||((b>a)&&(b-a)<=1)){ continue; }
+      g.pres_a[idx]=a;
+      g.pres_b[idx]=b;
+      ++idx;
+    }
   }
-  row>>=1;
-  uint64_t total=0;
-  for(uint64_t carryChain=~(left|down|right);carryChain!=0;){
-    uint64_t const bit=carryChain&-carryChain;
-    total+=solve(row,(left|bit)<<1,down|bit,(right|bit)>>1);
-    carryChain^=bit;
-  }
-  return total;
-} 
+}
+
 // クイーンの効きをチェック
 bool placement(void* args)
 {
@@ -131,11 +128,88 @@ bool placement(void* args)
   l->B.row|=row; l->B.down|=down; l->B.left|=left; l->B.right|=right;
   return true;
 }
+
+
+//非再帰ここから
+//非再帰ここから
+
+uint64_t solve(int size,int current,uint64_t row,uint64_t left,uint64_t down,uint64_t right)
+{
+  uint64_t row_a[MAX];
+  uint64_t right_a[MAX];
+  uint64_t left_a[MAX];
+  uint64_t down_a[MAX];
+  uint64_t bitmap_a[MAX];
+  for (int i=0;i<size;i++){
+    row_a[i]=0;
+    left_a[i]=0;
+    down_a[i]=0;
+    right_a[i]=0;
+    bitmap_a[i]=0;
+  }
+  row_a[current]=row;
+  left_a[current]=left;
+  down_a[current]=down;
+  right_a[current]=right;
+  uint64_t bitmap=bitmap_a[current]=~(left_a[current]|down_a[current]|right_a[current]);
+  uint64_t total=0;
+  uint64_t bit;
+
+  while(current>-1){
+    if((bitmap!=0||row&1)&&current<size){
+      if(!(down+1)){
+        total++;
+        current--;
+        row=row_a[current];
+        left=left_a[current];
+        right=right_a[current];
+        down=down_a[current];
+        bitmap=bitmap_a[current];
+        continue;
+      }else if(row&1){
+        while( row&1 ){
+          row>>=1;
+          left<<=1;
+          right>>=1;
+        }
+        bitmap=~(left|down|right);  //再帰に必要な変数は必ず定義する必要があります。
+        continue;
+      }else{
+        bit=-bitmap&bitmap;
+        bitmap=bitmap^bit;
+        if(current<size){
+          row_a[current]=row;
+          left_a[current]=left;
+          down_a[current]=down;
+          right_a[current]=right;
+          bitmap_a[current]=bitmap;
+          current++;
+        }
+        row>>=1;      //１行下に移動する
+        left=(left|bit)<<1;
+        down=down|bit;
+        right=(right|bit)>>1;
+        bitmap=~(left|down|right);  //再帰に必要な変数は必ず定義する必要があります。
+      }
+    }else{
+      current--;
+      row=row_a[current];
+      left=left_a[current];
+      right=right_a[current];
+      down=down_a[current];
+      bitmap=bitmap_a[current];
+    }
+  }
+  return total;
+}
+
+
+
 //対称解除法
 void carryChain_symmetry(void* args)
 {
   Local *l=(Local *)args;
-  // 対称解除法 
+  // 対称解除法
   unsigned const int ww=(g.size-2)*(g.size-1)-1-l->w;
   unsigned const int w2=(g.size-2)*(g.size-1)-1;
   // # 対角線上の反転が小さいかどうか確認する
@@ -145,28 +219,29 @@ void carryChain_symmetry(void* args)
   // # 斜め下方向への反転が小さいかをチェックする
   if((l->n==ww)&&(l->e>(w2-l->s))){ return; }
   // 枝刈り １行目が角の場合回転対称チェックせずCOUNT8にする
-  if(l->B.x[0]==0){ 
-    l->COUNTER[l->COUNT8]+=solve(l->B.row>>2,
+  if(l->B.x[0]==0){
+    l->COUNTER[l->COUNT8]+=solve(g.size,0,l->B.row>>2,
     l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
     return ;
   }
   // n,e,s==w の場合は最小値を確認する。右回転で同じ場合は、
   // w=n=e=sでなければ値が小さいのでskip  w=n=e=sであれば90度回転で同じ可能性
-  if(l->s==l->w){ if((l->n!=l->w)||(l->e!=l->w)){ return; } 
-    l->COUNTER[l->COUNT2]+=solve(l->B.row>>2,
+  if(l->s==l->w){ if((l->n!=l->w)||(l->e!=l->w)){ return; }
+    l->COUNTER[l->COUNT2]+=solve(g.size,0,l->B.row>>2,
     l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
     return;
   }
   // e==wは180度回転して同じ 180度回転して同じ時n>=sの時はsmaller?
-  if((l->e==l->w)&&(l->n>=l->s)){ if(l->n>l->s){ return; } 
-    l->COUNTER[l->COUNT4]+=solve(l->B.row>>2,
+  if((l->e==l->w)&&(l->n>=l->s)){ if(l->n>l->s){ return; }
+    l->COUNTER[l->COUNT4]+=solve(g.size,0,l->B.row>>2,
     l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
     return;
   }
-  l->COUNTER[l->COUNT8]+=solve(l->B.row>>2,
+  l->COUNTER[l->COUNT8]+=solve(g.size,0,l->B.row>>2,
   l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
   return;
 }
+
 // pthread run()
 void thread_run(void* args)
 {
@@ -174,48 +249,50 @@ void thread_run(void* args)
 
   // memcpy(&l->B,&l->wB,sizeof(Board));       // B=wB;
   l->B=l->wB;
-  l->dimx=0; l->dimy=g.pres_a[l->w]; 
-  //if(!placement(l)){ continue; } 
-  if(!placement(l)){ return; } 
-  l->dimx=1; l->dimy=g.pres_b[l->w]; 
-  // if(!placement(l)){ continue; } 
-  if(!placement(l)){ return; } 
+  l->dimx=0; l->dimy=g.pres_a[l->w];
+  //if(!placement(l)){ continue; }
+  if(!placement(l)){ return; }
+  l->dimx=1; l->dimy=g.pres_b[l->w];
+  // if(!placement(l)){ continue; }
+  if(!placement(l)){ return; }
   //２ 左２行に置く
   // memcpy(&l->nB,&l->B,sizeof(Board));       // nB=B;
   l->nB=l->B;
   for(l->n=l->w;l->n<(g.size-2)*(g.size-1)-l->w;++l->n){
     // memcpy(&l->B,&l->nB,sizeof(Board));     // B=nB;
     l->B=l->nB;
-    l->dimx=g.pres_a[l->n]; l->dimy=g.size-1; 
-    if(!placement(l)){ continue; } 
-    l->dimx=g.pres_b[l->n]; l->dimy=g.size-2; 
-    if(!placement(l)){ continue; } 
+    l->dimx=g.pres_a[l->n]; l->dimy=g.size-1;
+    if(!placement(l)){ continue; }
+    l->dimx=g.pres_b[l->n]; l->dimy=g.size-2;
+    if(!placement(l)){ continue; }
     // ３ 下２行に置く
     // memcpy(&l->eB,&l->B,sizeof(Board));     // eB=B;
     l->eB=l->B;
     for(l->e=l->w;l->e<(g.size-2)*(g.size-1)-l->w;++l->e){
       // memcpy(&l->B,&l->eB,sizeof(Board));   // B=eB;
       l->B=l->eB;
-      l->dimx=g.size-1; l->dimy=g.size-1-g.pres_a[l->e]; 
-      if(!placement(l)){ continue; } 
-      l->dimx=g.size-2; l->dimy=g.size-1-g.pres_b[l->e]; 
-      if(!placement(l)){ continue; } 
+      l->dimx=g.size-1; l->dimy=g.size-1-g.pres_a[l->e];
+      if(!placement(l)){ continue; }
+      l->dimx=g.size-2; l->dimy=g.size-1-g.pres_b[l->e];
+      if(!placement(l)){ continue; }
       // ４ 右２列に置く
       // memcpy(&l->sB,&l->B,sizeof(Board));   // sB=B;
       l->sB=l->B;
       for(l->s=l->w;l->s<(g.size-2)*(g.size-1)-l->w;++l->s){
         // memcpy(&l->B,&l->sB,sizeof(Board)); // B=sB;
         l->B=l->sB;
-        l->dimx=g.size-1-g.pres_a[l->s]; l->dimy=0; 
-        if(!placement(l)){ continue; } 
-        l->dimx=g.size-1-g.pres_b[l->s]; l->dimy=1; 
-        if(!placement(l)){ continue; } 
+        l->dimx=g.size-1-g.pres_a[l->s]; l->dimy=0;
+        if(!placement(l)){ continue; }
+        l->dimx=g.size-1-g.pres_b[l->s]; l->dimy=1;
+        if(!placement(l)){ continue; }
         // 対称解除法
         carryChain_symmetry(l);
       } //w
     } //e
   } //n
 }
+
+
 // チェーンのビルド
 void buildChain()
 {
@@ -244,24 +321,158 @@ void buildChain()
           l->COUNTER[l->COUNT4]*4+
           l->COUNTER[l->COUNT8]*8;
 }
-// チェーンのリストを作成
-void listChain()
-{
-  unsigned int idx=0;
-  for(unsigned int a=0;a<(unsigned)g.size;++a){
-    for(unsigned int b=0;b<(unsigned)g.size;++b){
-      if(((a>=b)&&(a-b)<=1)||((b>a)&&(b-a)<=1)){ continue; }
-      g.pres_a[idx]=a;
-      g.pres_b[idx]=b;
-      ++idx;
-    }
-  }
-}
+
 // キャリーチェーン
 void carryChain()
 {
   listChain();  //チェーンのリストを作成
   buildChain(); // チェーンのビルド
+  // calcChain(&l);  // 集計
+}
+
+
+
+
+//非再帰ここまで
+// 再帰 ボード外側２列を除く内側のクイーン配置処理
+uint64_t solveR(uint64_t row,uint64_t left,uint64_t down,uint64_t right)
+{
+  if(down+1==0){ return  1; }
+  while((row&1)!=0) { 
+    row>>=1;
+    left<<=1;
+    right>>=1;
+  }
+  row>>=1;
+  uint64_t total=0;
+  for(uint64_t carryChain=~(left|down|right);carryChain!=0;){
+    uint64_t const bit=carryChain&-carryChain;
+    total+=solveR(row,(left|bit)<<1,down|bit,(right|bit)>>1);
+    carryChain^=bit;
+  }
+  return total;
+} 
+
+
+//対称解除法
+void carryChain_symmetryR(void* args)
+{
+  Local *l=(Local *)args;
+  // 対称解除法
+  unsigned const int ww=(g.size-2)*(g.size-1)-1-l->w;
+  unsigned const int w2=(g.size-2)*(g.size-1)-1;
+  // # 対角線上の反転が小さいかどうか確認する
+  if((l->s==ww)&&(l->n<(w2-l->e))){ return ; }
+  // # 垂直方向の中心に対する反転が小さいかを確認
+  if((l->e==ww)&&(l->n>(w2-l->n))){ return; }
+  // # 斜め下方向への反転が小さいかをチェックする
+  if((l->n==ww)&&(l->e>(w2-l->s))){ return; }
+  // 枝刈り １行目が角の場合回転対称チェックせずCOUNT8にする
+  if(l->B.x[0]==0){
+    l->COUNTER[l->COUNT8]+=solveR(l->B.row>>2,
+    l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
+    return ;
+  }
+  // n,e,s==w の場合は最小値を確認する。右回転で同じ場合は、
+  // w=n=e=sでなければ値が小さいのでskip  w=n=e=sであれば90度回転で同じ可能性
+  if(l->s==l->w){ if((l->n!=l->w)||(l->e!=l->w)){ return; }
+    l->COUNTER[l->COUNT2]+=solveR(l->B.row>>2,
+    l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
+    return;
+  }
+  // e==wは180度回転して同じ 180度回転して同じ時n>=sの時はsmaller?
+  if((l->e==l->w)&&(l->n>=l->s)){ if(l->n>l->s){ return; }
+    l->COUNTER[l->COUNT4]+=solveR(l->B.row>>2,
+    l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
+    return;
+  }
+  l->COUNTER[l->COUNT8]+=solveR(l->B.row>>2,
+  l->B.left>>4,((((l->B.down>>2)|(~0<<(g.size-4)))+1)<<(g.size-5))-1,(l->B.right>>4)<<(g.size-5));
+  return;
+}
+
+// pthread run()
+void thread_runR(void* args)
+{
+  Local *l=(Local *)args;
+
+  // memcpy(&l->B,&l->wB,sizeof(Board));       // B=wB;
+  l->B=l->wB;
+  l->dimx=0; l->dimy=g.pres_a[l->w];
+  //if(!placement(l)){ continue; }
+  if(!placement(l)){ return; }
+  l->dimx=1; l->dimy=g.pres_b[l->w];
+  // if(!placement(l)){ continue; }
+  if(!placement(l)){ return; }
+  //２ 左２行に置く
+  // memcpy(&l->nB,&l->B,sizeof(Board));       // nB=B;
+  l->nB=l->B;
+  for(l->n=l->w;l->n<(g.size-2)*(g.size-1)-l->w;++l->n){
+    // memcpy(&l->B,&l->nB,sizeof(Board));     // B=nB;
+    l->B=l->nB;
+    l->dimx=g.pres_a[l->n]; l->dimy=g.size-1;
+    if(!placement(l)){ continue; }
+    l->dimx=g.pres_b[l->n]; l->dimy=g.size-2;
+    if(!placement(l)){ continue; }
+    // ３ 下２行に置く
+    // memcpy(&l->eB,&l->B,sizeof(Board));     // eB=B;
+    l->eB=l->B;
+    for(l->e=l->w;l->e<(g.size-2)*(g.size-1)-l->w;++l->e){
+      // memcpy(&l->B,&l->eB,sizeof(Board));   // B=eB;
+      l->B=l->eB;
+      l->dimx=g.size-1; l->dimy=g.size-1-g.pres_a[l->e];
+      if(!placement(l)){ continue; }
+      l->dimx=g.size-2; l->dimy=g.size-1-g.pres_b[l->e];
+      if(!placement(l)){ continue; }
+      // ４ 右２列に置く
+      // memcpy(&l->sB,&l->B,sizeof(Board));   // sB=B;
+      l->sB=l->B;
+      for(l->s=l->w;l->s<(g.size-2)*(g.size-1)-l->w;++l->s){
+        // memcpy(&l->B,&l->sB,sizeof(Board)); // B=sB;
+        l->B=l->sB;
+        l->dimx=g.size-1-g.pres_a[l->s]; l->dimy=0;
+        if(!placement(l)){ continue; }
+        l->dimx=g.size-1-g.pres_b[l->s]; l->dimy=1;
+        if(!placement(l)){ continue; }
+        // 対称解除法
+        carryChain_symmetryR(l);
+      } //w
+    } //e
+  } //n
+}
+// チェーンのビルド
+void buildChainR()
+{
+  Local l[(g.size/2)*(g.size-3)];
+
+  // カウンターの初期化
+  l->COUNT2=0; l->COUNT4=1; l->COUNT8=2;
+  l->COUNTER[l->COUNT2]=l->COUNTER[l->COUNT4]=l->COUNTER[l->COUNT8]=0;
+  // Board の初期化 nB,eB,sB,wB;
+  l->B.row=l->B.down=l->B.left=l->B.right=0;
+  // Board x[]の初期化
+  for(unsigned int i=0;i<g.size;++i){ l->B.x[i]=-1; }
+  //１ 上２行に置く
+  // memcpy(&l->wB,&l->B,sizeof(Board));         // wB=B;
+  l->wB=l->B;
+  for(l->w=0;l->w<=(unsigned)(g.size/2)*(g.size-3);++l->w){
+    thread_runR(&l);
+  } //w
+  /**
+   * 集計
+   */
+  UNIQUE= l->COUNTER[l->COUNT2]+
+          l->COUNTER[l->COUNT4]+
+          l->COUNTER[l->COUNT8];
+  TOTAL=  l->COUNTER[l->COUNT2]*2+
+          l->COUNTER[l->COUNT4]*4+
+          l->COUNTER[l->COUNT8]*8;
+}
+// キャリーチェーン
+void carryChainR()
+{
+  listChain();  //チェーンのリストを作成
+  buildChainR(); // チェーンのビルド
   // calcChain(&l);  // 集計
 }
 
@@ -319,7 +530,7 @@ int main(int argc,char** argv)
       gettimeofday(&t0, NULL);//計測開始
       if(cpur){ //再帰
         g.size=size;
-        carryChain();
+        carryChainR();
       }
       if(cpu){ //非再帰
         g.size=size;
