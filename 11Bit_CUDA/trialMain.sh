b9d6fe2c56dc2313aa3d43c92e44e46ce886d4a1
EC2 Default User <ec2-user@ip-172-31-13-29.us-west-2.compute.internal>
2023-11-22 05:05:21 +0000

    add

--- 11Bit_CUDA/trialMain.sh
+++ 11Bit_CUDA/trialMain.sh
@@ -13,9 +13,13 @@
 #
 # アタッチする
 # screen -r 
-
-sourceFile="04CUDA_Symmetry_BitBoard.cu";  # ソースファイル
-nvcc -O3 -arch=sm_61 -m64 -ptx -prec-div=false $sourceFile && ./a.out -n ;
+clear;
+rm -fr a.out;
+date;
+#nvcc -O3 -arch=sm_61 -m64 -ptx -prec-div=false $sourceFile && ./a.out -n
+#nvcc -O3 -arch=sm_61 -m64 -ptx -prec-div=false 04CUDA_Symmetry_BitBoard.cu && ./a.out -n
+nvcc -O3 -arch=sm_61 -m64 04CUDA_Symmetry_BitBoard.cu && ./a.out -n
+#./a.out -n 2>&1 | tee -a OUT
 # gcc -O3 -W -Wall -mtune=native -march=native -arch=sm_61 04CUDA_Symmetry_BitBoard.cu && POCL_DEBUG=all ./a.out -n 
 
 # デタッチする（離れる）
