b487f8f9f39a8a1b050d303f9ea2ed4a61167164
suzuki <suzuki@localhost.localdomain>
2025-02-13 15:00:39 +0900

    add

--- 11Bit_CUDA/00GCC_CarryChain.c
+++ 11Bit_CUDA/00GCC_CarryChain.c
@@ -1,3 +1,25 @@
+/**
+bash-5.1$ gcc -W -Wall -O3 00GCC_CarryChain.c && ./a.out
+
+キャリーチェーン
+ N:        Total       Unique        hh:mm:ss.ms
+ 4:            2               1            0.00
+ 5:           10               2            0.00
+ 6:            4               1            0.00
+ 7:           40               6            0.00
+ 8:           92              12            0.00
+ 9:          352              46            0.00
+10:          724              92            0.00
+11:         2680             341            0.00
+12:        14200            1788            0.01
+13:        73712            9237            0.05
+14:       365596           45771            0.21
+15:      2279184          285095            0.97
+16:     16314044         2040171            5.52
+17:    167611052        20954665           38.22
+18:   2368560040       296093363         5:30.60
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,18 +28,19 @@
 #define THREAD_NUM		96
 #define MAX 27
 //
-typedef unsigned long long uint64;
+//typedef unsigned long long uint;
+typedef uint uint;
 typedef struct{
-  uint64 bv;
-  uint64 down;
-  uint64 left;
-  uint64 right;
+  uint bv;
+  uint down;
+  uint left;
+  uint right;
   int x[MAX];
 }Board ;
 Board B;
-unsigned int COUNT8=2;
-unsigned int COUNT4=1;
-unsigned int COUNT2=0;
+uint COUNT8=2;
+uint COUNT4=1;
+uint COUNT2=0;
 long cnt[3];
 long pre[3];
 long TOTAL=0;
@@ -42,10 +65,12 @@ void TimeFormat(clock_t utime,char *form)
   else{ sprintf(form,"           %5.2f",ss); }
 }
 /**
- *
+ * solve
  */
-long solve_nqueenr(uint64 bv,uint64 left,uint64 down,uint64 right)
+long solve(uint bv,uint left,uint down,uint right)
 {
+  uint s=0;
+  uint bit;
   if(down+1==0){ return  1;}
   while((bv&1)!=0) { 
     bv>>=1;
@@ -53,36 +78,31 @@ long solve_nqueenr(uint64 bv,uint64 left,uint64 down,uint64 right)
     right>>=1;
   }
   bv>>=1;
-  long s=0;
-  uint64 bit;
-  for(uint64 bitmap=~(left|down|right);bitmap!=0;bitmap^=bit){
+  for(uint bitmap=~(left|down|right);bitmap!=0;bitmap^=bit){
     bit=bitmap&-bitmap;
-    s+=solve_nqueenr(bv,(left|bit)<<1,down|bit,(right|bit)>>1);
+    s+=solve(bv,(left|bit)<<1,down|bit,(right|bit)>>1);
   }
   return s;
 }
 /**
- *
+ * process
  */
-void process(int si,Board B,int sym)
+void process(int size,Board B,int sym)
 {
   pre[sym]++;
-  cnt[sym] += solve_nqueenr(B.bv >> 2,
-      B.left>>4,
-      ((((B.down>>2)|(~0<<(si-4)))+1)<<(si-5))-1,
-      (B.right>>4)<<(si-5));
+  cnt[sym]+=solve(B.bv>>2,B.left>>4,((((B.down>>2)|(~0<<(size-4)))+1)<<(size-5))-1,(B.right>>4)<<(size-5));
 }
 /**
- *
+ * placement
  */
-bool board_placement(int si,int x,int y)
+bool placement(int size,int x,int y)
 {
   if(B.x[x]==y){ return true;  }
   B.x[x]=y;
-  uint64 bv=1<<x;
-  uint64 down=1<<y;
-  uint64 left=1<<(si-1-x+y);
-  uint64 right=1<<(x+y);
+  uint bv=1<<x;
+  uint down=1<<y;
+  uint left=1<<(size-1-x+y);
+  uint right=1<<(x+y);
   if((B.bv&bv)||(B.down&down)||(B.left&left)||(B.right&right)){ return false; }
   B.bv |=bv;
   B.down |=down;
@@ -91,18 +111,16 @@ bool board_placement(int si,int x,int y)
   return true;
 }
 /**
- *
+ * nqueens
  */
-void NQueenR(int size)
+void nqueens(int size)
 {
   int pres_a[930];
   int pres_b[930];
   int idx=0;
   for(int a=0;a<size;a++){
     for(int b=0;b<size;b++){
-      if((a>=b&&(a-b)<=1)||(b>a&&(b-a)<=1)){
-        continue;
-      }     
+      if((a>=b&&(a-b)<=1)||(b>a&&(b-a)<=1)){ continue; }
       pres_a[idx]=a;
       pres_b[idx]=b;
       idx++;
@@ -121,24 +139,24 @@ void NQueenR(int size)
     printf("\r");
     fflush(stdout);
     //プログレス
-    board_placement(size,0,pres_a[w]);
-    board_placement(size,1,pres_b[w]);
+    placement(size,0,pres_a[w]);
+    placement(size,1,pres_b[w]);
     Board nB=B;
     int lsize=(size-2)*(size-1)-w;
     for(int n=w;n<lsize;n++){
       B=nB;
-      if(board_placement(size,pres_a[n],size-1)==false){ continue; }
-      if(board_placement(size,pres_b[n],size-2)==false){ continue; }
+      if(placement(size,pres_a[n],size-1)==false){ continue; }
+      if(placement(size,pres_b[n],size-2)==false){ continue; }
       Board eB=B;
       for(int e=w;e<lsize;e++){
         B=eB;
-        if(board_placement(size,size-1,size-1-pres_a[e])==false){ continue; }
-        if(board_placement(size,size-2,size-1-pres_b[e])==false){ continue; }
+        if(placement(size,size-1,size-1-pres_a[e])==false){ continue; }
+        if(placement(size,size-2,size-1-pres_b[e])==false){ continue; }
         Board sB=B;
         for(int s=w;s<lsize;s++){
           B=sB;
-          if(board_placement(size,size-1-pres_a[s],0)==false){ continue; }
-          if(board_placement(size,size-1-pres_b[s],1)==false){ continue; }
+          if(placement(size,size-1-pres_a[s],0)==false){ continue; }
+          if(placement(size,size-1-pres_b[s],1)==false){ continue; }
           int ww=(size-2)*(size-1)-1-w;
           int w2=(size-2)*(size-1)-1;
           if((s==ww)&&(n<(w2-e))){ continue; }
@@ -157,23 +175,24 @@ void NQueenR(int size)
 //メインメソッド
 int main(int argc,char** argv)
 {
-  printf("\n\n７．CPU 非再帰 バックトラック＋ビットマップ＋対称解除法\n");
+  printf("\n\nキャリーチェーン\n");
   printf("%s\n"," N:        Total       Unique        hh:mm:ss.ms");
-  clock_t st;           //速度計測用
-  char t[20];           //hh:mm:ss.msを格納
-  int min=4;int targetN=17;
+  clock_t st;
+  char t[20];
+  int min=4;
+  int targetN=18;
   int mask;
-  for(int i=min;i<=targetN;i++){
+  for(int size=min;size<=targetN;size++){
     TOTAL=0; UNIQUE=0;
     for(int j=0;j<=2;j++){
       pre[j]=0;
       cnt[j]=0;
     }
-    mask=(1<<i)-1;
+    mask=(1<<size)-1;
     st=clock();
-    NQueenR(i); 
-    TimeFormat(clock()-st,t); 
-    printf("%2d:%13ld%16ld%s\n",i,TOTAL,UNIQUE,t);
+    nqueens(size);
+    TimeFormat(clock()-st,t);
+    printf("%2d:%13ld%16ld%s\n",size,TOTAL,UNIQUE,t);
   }
   return 0;
 }
