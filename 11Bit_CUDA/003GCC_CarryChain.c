df6951d01c0ca01ea60f497329112f74409c1d1b
suzuki <suzuki@localhost.localdomain>
2025-02-13 20:32:16 +0900

    add

--- 11Bit_CUDA/003GCC_CarryChain.c
+++ 11Bit_CUDA/003GCC_CarryChain.c
@@ -16,7 +16,7 @@ bash-5.1$ g++ -W -Wall -O3 00GCC_CarryChain.c && ./a.out
 15:      2279184      285095      00:00:00:00.93
 16:     14772512     1847425      00:00:00:05.46
 17:     95815104    11979381      00:00:00:36.85
-18:    657378384    82181924      00:00:04:21.49
+18:    657378384    82181924      00:00:04:18.49
 
 bash-5.1$ g++ -W -Wall -O3 00GCC_NodeLayer.c && ./a.out
 ノードレイヤー
@@ -42,78 +42,79 @@ bash-5.1$ gcc -W -Wall -O3 01CUDA_Bit_Symmetry.c && ./a.out
 #include <sys/time.h>
 #define MAX 27
 //
-typedef long long uint64_t;
+typedef unsigned long ulong;
+typedef unsigned int uint;
 typedef struct{
-  uint64_t bv;
-  uint64_t down;
-  uint64_t left;
-  uint64_t right;
-  uint64_t x[MAX];
+  ulong row;
+  ulong down;
+  ulong left;
+  ulong right;
+  ulong x[MAX];
 }Board ;
-Board B;
-uint64_t COUNT8=2;
-uint64_t COUNT4=1;
-uint64_t COUNT2=0;
-uint64_t cnt[3];
-uint64_t pre[3];
-uint64_t TOTAL=0;
-uint64_t UNIQUE=0;
+uint COUNT8=2;
+uint COUNT4=1;
+uint COUNT2=0;
+ulong cnt[3];
+ulong pre[3];
+ulong TOTAL=0;
+ulong UNIQUE=0;
 /**
  * solve
  */
-uint64_t solve(uint64_t bv,uint64_t left,uint64_t down,uint64_t right)
+ulong solve(ulong row,ulong left,ulong down,ulong right)
 {
-  uint64_t s=0;
-  uint64_t bit;
+  ulong s=0;
+  ulong bit;
+  ulong bitmap=0;
   if(down+1==0){ return  1;}
-  while((bv&1)!=0) {
-    bv>>=1;
+  while((row&1)!=0){
+    row>>=1;
     left<<=1;
     right>>=1;
   }
-  bv>>=1;
-  for(uint64_t bitmap=~(left|down|right);bitmap!=0;bitmap^=bit){
+  row>>=1;
+  for(bitmap=~(left|down|right);bitmap!=0;bitmap^=bit){
     bit=bitmap&-bitmap;
-    s+=solve(bv,(left|bit)<<1,down|bit,(right|bit)>>1);
+    s+=solve(row,(left|bit)<<1,down|bit,(right|bit)>>1);
   }
   return s;
 }
 /**
  * process
  */
-void process(int size,Board B,int sym)
+void process(uint size,Board* B,int sym)
 {
   pre[sym]++;
-  cnt[sym]+=solve(B.bv>>2,B.left>>4,((((B.down>>2)|(~0<<(size-4)))+1)<<(size-5))-1,(B.right>>4)<<(size-5));
+  cnt[sym]+=solve(B->row>>2,B->left>>4,((((B->down>>2)|(~0<<(size-4)))+1)<<(size-5))-1,(B->right>>4)<<(size-5));
 }
 /**
  * placement
  */
-bool placement(int size,int x,int y)
+bool placement(uint size,ulong x,ulong y,Board* B)
 {
-  if(B.x[x]==y){ return true;  }
-  B.x[x]=y;
-  uint64_t bv=1<<x;
-  uint64_t down=1<<y;
-  uint64_t left=1<<(size-1-x+y);
-  uint64_t right=1<<(x+y);
-  if((B.bv&bv)||(B.down&down)||(B.left&left)||(B.right&right)){ return false; }
-  B.bv |=bv;
-  B.down |=down;
-  B.left |=left;
-  B.right |=right;
+  if(B->x[x]==y){ return true;  }
+  B->x[x]=y;
+  ulong row=1<<x;
+  ulong down=1<<y;
+  ulong left=1<<(size-1-x+y);
+  ulong right=1<<(x+y);
+  if((B->row&row)||(B->down&down)||(B->left&left)||(B->right&right)){ return false; }
+  B->row |=row;
+  B->down |=down;
+  B->left |=left;
+  B->right |=right;
   return true;
 }
 /**
  * nqueens
  */
-void nqueens(int size)
+void nqueens(uint size)
 {
-  uint64_t pres_a[930];
-  uint64_t pres_b[930];
-  uint64_t idx=0;
-  for(int a=0;a<size;a++){
-    for(int b=0;b<size;b++){
+  uint pres_a[930];
+  uint pres_b[930];
+  uint idx=0;
+  for(uint a=0;a<size;a++){
+    for(uint b=0;b<size;b++){
       if((a>=b&&(a-b)<=1)||(b>a&&(b-a)<=1)){ continue; }
       pres_a[idx]=a;
       pres_b[idx]=b;
@@ -123,51 +124,58 @@ void nqueens(int size)
   //プログレス
   // printf("\t\t  First side bound: (%d,%d)/(%d,%d)",(unsigned)pres_a[(size/2)*(size-3)  ],(unsigned)pres_b[(size/2)*(size-3)  ],(unsigned)pres_a[(size/2)*(size-3)+1],(unsigned)pres_b[(size/2)*(size-3)+1]);
   //プログレス
-  Board wB=B;
-  for(int w=0;w<=(size/2)*(size-3);w++){
-    B=wB;
-    B.bv=B.down=B.left=B.right=0;
-    for(int i=0;i<size;i++){ B.x[i]=-1; }
+  // Board wB=B;
+  Board wB,B;
+  memcpy(&wB,&B,sizeof(Board));
+  for(uint w=0;w<=(size/2)*(size-3);w++){
+    // B=wB;
+    memcpy(&B,&wB,sizeof(Board));
+    B.row=B.down=B.left=B.right=0;
+    for(uint i=0;i<size;i++){ B.x[i]=-1; }
     //プログレス
     // printf("\r(%d/%d)",w,((size/2)*(size-3)));
     // printf("\r");
     // fflush(stdout);
     //プログレス
-    placement(size,0,pres_a[w]);
-    placement(size,1,pres_b[w]);
+    placement(size,0,pres_a[w],&B);
+    placement(size,1,pres_b[w],&B);
     // Board nB=B;
     Board nB;
     memcpy(&nB,&B,sizeof(Board));
-    int lsize=(size-2)*(size-1)-w;
-    for(int n=w;n<lsize;n++){
+    uint lsize=(size-2)*(size-1)-w;
+    for(uint n=w;n<lsize;n++){
       // B=nB;
       memcpy(&B,&nB,sizeof(Board));
-      if(placement(size,pres_a[n],size-1)==false){ continue; }
-      if(placement(size,pres_b[n],size-2)==false){ continue; }
+      if(placement(size,pres_a[n],size-1,&B)==false){ continue; }
+      if(placement(size,pres_b[n],size-2,&B)==false){ continue; }
       // Board eB=B;
       Board eB;
       memcpy(&eB,&B,sizeof(Board));
-      for(int e=w;e<lsize;e++){
+      for(uint e=w;e<lsize;e++){
         // B=eB;
         memcpy(&B,&eB,sizeof(Board));
-        if(placement(size,size-1,size-1-pres_a[e])==false){ continue; }
-        if(placement(size,size-2,size-1-pres_b[e])==false){ continue; }
+        if(placement(size,size-1,size-1-pres_a[e],&B)==false){ continue; }
+        if(placement(size,size-2,size-1-pres_b[e],&B)==false){ continue; }
         // Board sB=B;
         Board sB;
         memcpy(&sB,&B,sizeof(Board));
-        for(int s=w;s<lsize;s++){
+        for(uint s=w;s<lsize;s++){
           // B=sB;
           memcpy(&B,&sB,sizeof(Board));
-          if(placement(size,size-1-pres_a[s],0)==false){ continue; }
-          if(placement(size,size-1-pres_b[s],1)==false){ continue; }
-          int ww=(size-2)*(size-1)-1-w;
-          int w2=(size-2)*(size-1)-1;
+          if(placement(size,size-1-pres_a[s],0,&B)==false){ continue; }
+          if(placement(size,size-1-pres_b[s],1,&B)==false){ continue; }
+          uint ww=(size-2)*(size-1)-1-w;
+          uint w2=(size-2)*(size-1)-1;
           if((s==ww)&&(n<(w2-e))){ continue; }
           if((e==ww)&&(n>(w2-n))){ continue;}
           if((n==ww)&&(e>(w2-s))){ continue; }
-          if(s==w){ if((n!=w)||(e!=w)){ continue; } process(size,B,COUNT2);continue; }
-          if((e==w)&&(n>=s)){ if(n>s){ continue; } process(size,B,COUNT4);continue; }
-          process(size,B,COUNT8);continue;
+          if(s==w){ if((n!=w)||(e!=w)){ continue; }
+            process(size,&B,COUNT2);continue;
+          }
+          if((e==w)&&(n>=s)){ if(n>s){ continue; }
+            process(size,&B,COUNT4);continue;
+          }
+          process(size,&B,COUNT8);continue;
         }
       }
     }
@@ -182,12 +190,12 @@ int main(int argc,char** argv)
 {
   printf("%s\n","キャリーチェーン");
   printf("%s\n"," N:        Total      Unique      dd:hh:mm:ss.ms");
-  int min=4;
-  int targetN=18;
+  uint min=4;
+  uint targetN=18;
   struct timeval t0;
   struct timeval t1;
-  uint64_t ss,ms,dd,hh,mm;
-  for(int size=min;size<=targetN;size++){
+  uint ss,ms,dd,hh,mm;
+  for(uint size=min;size<=targetN;size++){
     TOTAL=0; UNIQUE=0;
     for(int j=0;j<=2;j++){
       pre[j]=0;
@@ -208,7 +216,7 @@ int main(int argc,char** argv)
     hh=ss/3600;
     mm=(ss-hh*3600)/60;
     ss%=60;
-    printf("%2d:%13lld%12lld%8.2lld:%02lld:%02lld:%02lld.%02lld\n",size,TOTAL,UNIQUE,dd,hh,mm,ss,ms);
+    printf("%2d:%13ld%12ld%8.2d:%02d:%02d:%02d.%02d\n",size,TOTAL,UNIQUE,dd,hh,mm,ss,ms);
   }
   return 0;
 }
