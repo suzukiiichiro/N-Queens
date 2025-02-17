4be49d57d872eacd20672cfa3a59f36ad6080525
suzuki <suzuki@localhost.localdomain>
2025-02-17 17:36:17 +0900

    add

--- 11Bit_CUDA/003GCC_CarryChain_suzuki20250217.c
+++ 11Bit_CUDA/003GCC_CarryChain_suzuki20250217.c
@@ -1,110 +1,198 @@
+//日本語
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
-#define MAX 18
-
+#define MAX 24
+#define UINT64_C(c) c ## ULL
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned long long xlong;
 typedef struct{
-  unsigned long total; // long $B"*(B unsigned long
+  ulong total;
 } NQueens17;
-
-unsigned long solve(unsigned int row, unsigned int left, unsigned int down, unsigned int right){
-  unsigned long total = 0;
-  if ((down + 1) == 0) return 1;
-  while (row & 1) {
-    row >>= 1;
-    left <<= 1;
-    right >>= 1;
+/**
+ *
+ */
+xlong solve(uint row,uint left,uint down,uint right){
+  xlong total=0;
+  if((down+1)==0) return 1;
+  while(row&1){
+    row>>=1;
+    left<<=1;
+    right>>=1;
   }
   row >>= 1;
-  unsigned int bitmap = ~(left | down | right);
-  while (bitmap != 0) {
-    unsigned int bit = bitmap & -bitmap;
-    total += solve(row, (left | bit) << 1, down | bit, (right | bit) >> 1);
+  xlong bitmap=~(left|down|right);
+  while(bitmap!=0){
+    xlong bit=bitmap & -bitmap;
+    total+=solve(row,(left|bit)<<1,down|bit,(right|bit)>>1);
     bitmap ^= bit;
   }
   return total;
 }
-
-unsigned long process(int size, int sym, unsigned int B[]) {
-  return sym * solve(B[0] >> 2, B[1] >> 4, (((((unsigned)B[2] >> 2) | ~(0U) << (size - 4)) + 1) << (size - 5)) - 1, (B[3] >> 4) << (size - 5));
+/**
+ *
+ */
+xlong process(int size,int sym,uint B[]){
+  return sym*solve(
+    B[0]>>2,
+    B[1]>>4,
+    (((((unsigned)B[2]>>2)|~(0U)<<(size-4))+1)<<(size-5))-1,
+    (unsigned)(B[3]>>4)<<(size-5)
+  );
 }
-
-unsigned long Symmetry(int size, int n, int w, int s, int e, unsigned int B[], int B4[]) {
-  int ww = (size - 2) * (size - 1) - 1 - w;
-  int w2 = (size - 2) * (size - 1) - 1;
-  if (s == ww && n < (w2 - e)) return 0;
-  if (e == ww && n > (w2 - n)) return 0;
-  if (n == ww && e > (w2 - s)) return 0;
-  if (!B4[0]) return process(size, 8, B);
-  if (s == w) {
-    if (n != w || e != w) return 0;
-    return process(size, 2, B);
+/**
+ *
+ */
+ulong Symmetry(int size,int n,int w,int s,int e,uint B[],int B4[]){
+  int ww=(size-2)*(size-1)-1-w;
+  int w2=(size-2)*(size-1)-1;
+  if(s==ww&&n<(w2-e)) return 0;
+  if(e==ww&&n>(w2-n)) return 0;
+  if(n==ww&&e>(w2-s)) return 0;
+  if(!B4[0]) return process(size,8,B);
+  if(s==w){
+    if(n!=w||e!=w) return 0;
+    return process(size,2,B);
   }
-  if (e == w && n >= s) {
-    if (n > s) return 0;
-    return process(size, 4, B);
+  if(e==w&&n>=s){
+    if(n>s) return 0;
+    return process(size,4,B);
   }
-  return process(size, 8, B);
+  return process(size,8,B);
 }
-
-int placement(int size, int dimx, int dimy, unsigned int B[], int B4[]) {
-  if (B4[dimx] == dimy) return 1;
-  if (B4[0]) {
-    if ((B4[0] != -1 && ((dimx < B4[0] || dimx >= size - B4[0]) && (dimy == 0 || dimy == size - 1))) ||
-        ((dimx == size - 1) && (dimy <= B4[0] || dimy >= size - B4[0]))) return 0;
-  } else if ((B4[1] != -1) && (B4[1] >= dimx && dimy == 1)) return 0;
-  if ((B[0] & (1U << dimx)) || (B[1] & (1U << (size - 1 - dimx + dimy))) ||
-      (B[2] & (1U << dimy)) || (B[3] & (1U << (dimx + dimy)))) return 0;
-  B[0] |= 1U << dimx;
-  B[1] |= 1U << (size - 1 - dimx + dimy);
-  B[2] |= 1U << dimy;
-  B[3] |= 1U << (dimx + dimy);
-  B4[dimx] = dimy;
+/**
+ *
+ */
+int placement(int size,int dimx,int dimy,uint B[],int B4[]){
+  if(B4[dimx]==dimy) return 1;
+  if(B4[0]){
+    if((B4[0]!=-1&&((dimx<B4[0]||dimx>=size-B4[0]) &&
+      (dimy==0||dimy==size-1)))||
+      ((dimx==size-1)&&
+      (dimy<=B4[0]||dimy>=size-B4[0]))){ return 0;}
+  } else if((B4[1]!=-1)&&(B4[1]>=dimx&&dimy==1)){ return 0;}
+  if((B[0]&(1<<dimx))||
+    (B[1]&(1<<(size-1-dimx+dimy)))||
+    (B[2]&(1<<dimy))||
+    (B[3]&(1<<(dimx+dimy)))){ return 0;}
+  uint row=UINT64_C(1)<<dimx;
+  uint left=UINT64_C(1)<<(size-1-dimx+dimy);
+  uint down=UINT64_C(1)<<dimy;
+  uint right=UINT64_C(1)<<(dimx+dimy);
+  B4[dimx]=dimy;
+  if((B[0]&row)||(B[2]&down)||(B[1]&left)||(B[3]&right)){ return 0; }
+  B[0]|=row; B[2]|=down; B[1]|=left; B[3]|=right;
   return 1;
 }
-
-void deepcopy(unsigned int *src, unsigned int *dest, int size) {
-  memcpy(dest, src, size * sizeof(unsigned int));
+/**
+ *
+ */
+void deepcopy(uint *src,uint *dest,int size){
+  memcpy(dest,src,size*sizeof(uint));
 }
-
-unsigned long buildChain(int size, int pres_a[], int pres_b[]) {
-  unsigned long total = 0;
-  unsigned int B[4] = {0, 0, 0, 0};
-  int B4[MAX];
-  for (int i = 0; i < size; i++) B4[i] = -1;
-  int sizeE = size - 1;
-  int sizeEE = size - 2;
-  int range_size = (size / 2) * (size - 3) + 1;
-  for (int w = 0; w < range_size; w++) {
-    unsigned int wB[4];
-    int wB4[MAX];
-    deepcopy(B, wB, 4);
-    memcpy(wB4, B4, sizeof(B4));
-    if (!placement(size, 0, pres_a[w], wB, wB4) || !placement(size, 1, pres_b[w], wB, wB4)) continue;
-    for (int n = w; n < (sizeEE) * (sizeE) - w; n++) {
-      unsigned int nB[4];
-      int nB4[MAX];
-      deepcopy(wB, nB, 4);
-      memcpy(nB4, wB4, sizeof(B4));
-      if (!placement(size, pres_a[n], sizeE, nB, nB4) || !placement(size, pres_b[n], sizeEE, nB, nB4)) continue;
-      for (int e = w; e < (sizeEE) * (sizeE) - w; e++) {
-        unsigned int eB[4];
-        int eB4[MAX];
-        deepcopy(nB, eB, 4);
-        memcpy(eB4, nB4, sizeof(B4));
-        if (!placement(size, sizeE, sizeE - pres_a[e], eB, eB4) || !placement(size, sizeEE, sizeE - pres_b[e], eB, eB4)) continue;
-        for (int s = w; s < (sizeEE) * (sizeE) - w; s++) {
-          unsigned int sB[4];
-          int sB4[MAX];
-          deepcopy(eB, sB, 4);
-          memcpy(sB4, eB4, sizeof(B4));
-          if (!placement(size, sizeE - pres_a[s], 0, sB, sB4) || !placement(size, sizeE - pres_b[s], 1, sB, sB4)) continue;
-          total += Symmetry(size, n, w, s, e, sB, sB4);
+/**
+ *
+ */
+xlong buildChain(int size,int pres_a[],int pres_b[]){
+  xlong total=0;
+  uint B[4]={0,0,0,0};//row/left/down/right
+  uint B4[size];
+  for(int i=0;i<size;i++) B4[i]=-1;
+  int sizeE=size-1;
+  int sizeEE=size-2;
+  int range_size=(size/2)*(size-3)+1;
+  for(int w=0;w<range_size;w++){
+    uint wB[4];
+    uint wB4[size];
+    deepcopy(B,wB,4);
+    memcpy(wB4,B4,sizeof(B4));
+    if(!placement(size,0,pres_a[w],wB,wB4)||!placement(size,1,pres_b[w],wB,wB4)) continue;
+    for(int n=w;n<(sizeEE)*(sizeE)-w;n++){
+      uint nB[4];
+      uint nB4[size];
+      deepcopy(wB,nB,4);
+      memcpy(nB4,wB4,sizeof(B4));
+      if(!placement(size,pres_a[n],sizeE,nB,nB4)||!placement(size,pres_b[n],sizeEE,nB,nB4)) continue;
+      for(int e=w;e<(sizeEE)*(sizeE)-w;e++){
+        uint eB[4];
+        uint eB4[size];
+        deepcopy(nB,eB,4);
+        memcpy(eB4,nB4,sizeof(B4));
+        if(!placement(size,sizeE,sizeE-pres_a[e],eB,eB4)||!placement(size,sizeEE,sizeE-pres_b[e],eB,eB4)) continue;
+        for(int s=w;s<(sizeEE)*(sizeE)-w;s++){
+          uint sB[4];
+          uint sB4[size];
+          deepcopy(eB,sB,4);
+          memcpy(sB4,eB4,sizeof(B4));
+          if(!placement(size,sizeE-pres_a[s],0,sB,sB4)||!placement(size,sizeE-pres_b[s],1,sB,sB4)) continue;
+          total+=Symmetry(size,n,w,s,e,sB,sB4);
         }
       }
     }
   }
   return total;
+}
+/**
+ *
+ */
+void initChain(int size,int pres_a[],int pres_b[]){
+  int idx=0;
+  for(int a=0;a<size;a++){
+    for(int b=0;b<size;b++){
+      if(abs(a-b)<=1) continue;
+      pres_a[idx]=a;
+      pres_b[idx]=b;
+      idx++;
+    }
+  }
+}
+/*
+ *
+ */
+xlong carryChain(int size){
+  int pres_a[930]={0};
+  int pres_b[930]={0};
+  initChain(size,pres_a,pres_b);
+  return buildChain(size,pres_a,pres_b);
+}
+/**
+ *
+ */
+void mainNQueens17(){
+  int nmin=4;
+  int nmax=18;
+  struct timeval t0;
+  struct timeval t1;
+  int ss,ms,dd,hh,mm;
+  printf("%s\n","キャリーチェーン");
+  printf("%s\n"," N:        Total      Unique      dd:hh:mm:ss.ms");
+  for(int size=nmin;size<=nmax;size++){
+    gettimeofday(&t0,NULL);
+    xlong TOTAL=carryChain(size);
+    xlong UNIQUE=0;
+    gettimeofday(&t1,NULL);
+    if(t1.tv_usec<t0.tv_usec){
+      dd=(t1.tv_sec-t0.tv_sec-1)/86400;
+      ss=(t1.tv_sec-t0.tv_sec-1)%86400;
+      ms=(1000000+t1.tv_usec-t0.tv_usec+500)/10000;
+    }else{
+      dd=(t1.tv_sec-t0.tv_sec)/86400;
+      ss=(t1.tv_sec-t0.tv_sec)%86400;
+      ms=(t1.tv_usec-t0.tv_usec+500)/10000;
+    }
+    hh=ss/3600;
+    mm=(ss-hh*3600)/60;
+    ss%=60;
+    printf("%2d:%13llu%12llu%8.2d:%02d:%02d:%02d.%02d\n",size,TOTAL,UNIQUE,dd,hh,mm,ss,ms);
+  }
+}
+/**
+ *
+ */
+int main(){
+  mainNQueens17();
+  return 0;
 }
 
