34c5bf212af12ce0bc09609fdf85064575fa3033
suzuki <suzuki@localhost.localdomain>
2025-03-07 14:31:17 +0900

    add

--- 10Bit_Python/05Python_optimize.py
+++ 10Bit_Python/05Python_optimize.py
@@ -41,11 +41,10 @@ CentOS-5.1$ pypy 03Python_backTracking.py
 from datetime import datetime 
 # pypyを使う場合はコメントを解除
 # pypyで再帰が高速化できる
-import pypyjit
-pypyjit.set_param('max_unroll_recursion=-1')
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
 
 class NQueens05:
-  max:int
   total:int
   unique:int
   aboard:list[int]
@@ -55,25 +54,23 @@ class NQueens05:
   trial:list[int]
   scratch:list[int]
   def __init__(self):
-   self.max=16
-   self.total=0
-   self.unique=0
-   self.aboard=[0 for i in range(self.max)]
-   self.fa=[0 for i in range(2*self.max-1)]
-   self.fb=[0 for i in range(2*self.max-1)]
-   self.fc=[0 for i in range(2*self.max-1)]
-   self.trial=[0 for i in range(self.max)]
-   self.scratch=[0 for i in range(self.max)]
-
-  def rotate(self,chk:list[int],scr:list[int],_n:int,neg:int):
+    pass
+  def init(self,size:int)->None:
+    self.total=0
+    self.unique=0
+    self.aboard=[i for i in range(size)]
+    self.fa=[0 for i in range(2*size-1)]
+    self.fb=[0 for i in range(2*size-1)]
+    self.fc=[0 for i in range(2*size-1)]
+    self.trial=[0 for i in range(size)]
+    self.scratch=[0 for i in range(size)]
+  def rotate(self,chk:list[int],scr:list[int],_n:int,neg:int)->None:
     incr:int=0
     k:int=0
-    # k=0 if neg else _n-1
     if neg:
       k=0
     else:
       k=_n-1
-    # incr=1 if neg else -1
     if neg:
       incr=1
     else:
@@ -85,11 +82,9 @@ class NQueens05:
     for i in range(_n):
       chk[scr[i]]=k
       k-=incr
-
-  def vmirror(self,chk:list[int],neg:int):
+  def vmirror(self,chk:list[int],neg:int)->None:
     for i in range(neg):
       chk[i]=(neg-1)-chk[i]
-
   def intncmp(self,_lt:list[int],_rt:list[int],neg)->int:
     rtn:int=0
     for i in range(neg):
@@ -97,7 +92,6 @@ class NQueens05:
       if rtn!=0:
         break
     return rtn
-
   def symmetryops(self,size:int)->int:
     nequiv:int=0
     for i in range(size):
@@ -149,8 +143,7 @@ class NQueens05:
         if k>0:
           return 0
     return nequiv*2
-
-  def nqueens_rec(self,row:int,size:int):
+  def nqueens(self,row:int,size:int)->None:
     if row==size-1:
       if self.fb[row-self.aboard[row]+size-1] or self.fc[row+self.aboard[row]]:
         return
@@ -167,31 +160,23 @@ class NQueens05:
         self.aboard[row]=tmp
         if self.fb[row-self.aboard[row]+size-1]==0 and self.fc[row+self.aboard[row]]==0:
           self.fb[row-self.aboard[row]+size-1]=self.fc[row+self.aboard[row]]=1
-          self.nqueens_rec(row+1,size)
+          self.nqueens(row+1,size)
           self.fb[row-self.aboard[row]+size-1]=self.fc[row+self.aboard[row]]=0
       tmp=self.aboard[row]
       for i in range(row+1,size):
         self.aboard[i-1]=self.aboard[i]
       self.aboard[size-1]=tmp
-
-  def main(self):
-    nmin:int=4
-    nmax:int=16
+  def main(self)->None:
+    min:int=4
+    max:int=16
     print(" N:        Total       Unique         hh:mm:ss.ms")
-    for size in range(nmin,self.max):
-      self.total=0
-      self.unique=0
-      for j in range(size):
-        self.aboard[j]=j
+    for size in range(min,max):
+      self.init(size)
       start_time=datetime.now()
-      self.nqueens_rec(0,size)
+      self.nqueens(0,size)
       time_elapsed=datetime.now()-start_time
-      # _text='{}'.format(time_elapsed)
-      # text=_text[:-3]
-      # print("%2d:%13d%13d%20s" % (size,self.total,self.unique,text)); 
-      text = str(time_elapsed)[:-3]  
+      text = str(time_elapsed)[:-3]
       print(f"{size:2d}:{self.total:13d}{self.unique:13d}{text:>20s}")
-
 # 5.枝刈りと最適化
 # $ python <filename>
 # $ pypy <fileName>
