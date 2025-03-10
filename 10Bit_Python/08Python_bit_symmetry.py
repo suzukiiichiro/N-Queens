6900fec9849092a10c9315edadc194958a23d29c
suzuki <suzuki@localhost.localdomain>
2025-03-10 12:48:19 +0900

    add

--- 10Bit_Python/08Python_bit_symmetry.py
+++ 10Bit_Python/08Python_bit_symmetry.py
@@ -52,25 +52,28 @@ CentOS-5.1$ pypy 03Python_backTracking.py
 
 from datetime import datetime 
 # pypyを使う場合はコメントを解除
-import pypyjit
-pypyjit.set_param('max_unroll_recursion=-1')
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
 
 class NQueens08():
   total:int
   unique:int
+  board:list[int]
   bound1:int
   bound2:int
   topbit:int
   endbit:int
   sidemask:int
   lastmask:int
-  board:list[int]
   count2:int
   count4:int
   count8:int
   def __init__(self):
+    pass
+  def init(self,size:int):
     self.total=0
     self.unique=0
+    self.board=[0 for i in range(size)]
     self.bound1=0
     self.bound2=0
     self.topbit=0
@@ -219,27 +222,13 @@ class NQueens08():
     self.total=self.count2*2+self.count4*4+self.count8*8
   def main(self):
     nmin:int=4
-    nmax:int=16
+    nmax:int=18
     print(" N:        Total       Unique        hh:mm:ss.ms")
     for size in range(nmin, nmax):
-      self.total=0
-      self.unique=0
-      self.count2=0
-      self.count4=0
-      self.count8=0
-      self.bound1=0
-      self.bound2=0
-      self.topbit=0
-      self.endbit=0
-      self.sidemask=0
-      self.lastmask=0
-      self.board=[0 for i in range(size)]
+      self.init(size)
       start_time = datetime.now()
       self.NQueens(size)
       time_elapsed = datetime.now()-start_time
-      # _text = '{}'.format(time_elapsed)
-      # text = _text[:-3]
-      # print("%2d:%13d%13d%20s" % (size,self.total,self.unique, text))  
       text = str(time_elapsed)[:-3]  
       print(f"{size:2d}:{self.total:13d}{self.unique:13d}{text:>20s}")
 
