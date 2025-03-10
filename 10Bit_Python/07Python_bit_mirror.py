cd420fa485c5d29b1dd88e7d315a2fb9673674d6
suzuki <suzuki@localhost.localdomain>
2025-03-10 11:47:22 +0900

    add

--- 10Bit_Python/07Python_bit_mirror.py
+++ 10Bit_Python/07Python_bit_mirror.py
@@ -47,13 +47,15 @@ CentOS-5.1$ pypy 03Python_backTracking.py
 from datetime import datetime 
 # pypyを使う場合はコメントを解除
 # pypyで再帰が高速化できる
-import pypyjit
-pypyjit.set_param('max_unroll_recursion=-1')
+# import pypyjit
+# pypyjit.set_param('max_unroll_recursion=-1')
 
 class NQueens07():
   total:int
   unique:int
   def __init__(self):
+    pass
+  def init(self):
     self.total=0
     self.unique=0
   def mirror(self,size:int,row:int,left:int,down:int,right:int):
@@ -88,17 +90,13 @@ class NQueens07():
     self.total=self.total<<1; # 倍にする
   def main(self):
     nmin:int = 4
-    nmax:int = 16
+    nmax:int = 18
     print(" N:        Total       Unique        hh:mm:ss.ms")
     for size in range(nmin, nmax):
-      self.total=0
-      self.unique=0
+      self.init()
       start_time = datetime.now()
       self.NQueens(size,0,0,0,0)
       time_elapsed = datetime.now()-start_time
-      # _text = '{}'.format(time_elapsed)
-      # text = _text[:-3]
-      # print("%2d:%13d%13d%20s" % (i,self.total,self.unique, text))  
       text = str(time_elapsed)[:-3]  
       print(f"{size:2d}:{self.total:13d}{self.unique:13d}{text:>20s}")
 
