d926efa36995924908326dc2d48182b3fd1d875e
suzuki <suzuki@localhost.localdomain>
2025-03-11 13:16:05 +0900

    add

--- 10Bit_Python/11Python_NodeLayer.py
+++ 10Bit_Python/11Python_NodeLayer.py
@@ -89,6 +89,16 @@ class NQueens11:
         n&=(n-1)  # 右端の1を削除
         counter+=1
       return counter
+
+    # ノードレイヤーのバックトラック
+    # 解を導き出すために、ノードにより、クイーンの移動の候補を列挙する処理を行
+    # います。
+    # ノードレイヤーは、解を導き出すための処理に加えて、クイーンの移動候補を列
+    # 挙するための処理を行うため事実上、２回エイトクイーンを行っていることが、
+    # 速度低下の最大のボトルネックと言えます。
+    # また、この章ではＱが角にあるか・ないかを判定していません。
+    # よって枝刈りなどは行っていません。Nodelayerの対象解除で角判定と、枝刈り
+    # を行います。
     def kLayer_nodeLayer(self,size:int,nodes:list,k:int,left:int,down:int,right:int)->int:
       counter:int=0
       mask:int=(1<<size)-1
@@ -107,16 +117,31 @@ class NQueens11:
         bitmap^=bit
       return counter
     def bitmap_build_nodeLayer(self, size:int) ->int:
-        # ツリーの3番目のレイヤーにあるノードを生成
+        # ツリーの3番目のレイヤーにあるノード
+        # （それぞれ連続する3つの数字でエンコードされる）のベクトル
+        # レイヤー2以降はノードの数が均等なので対称性を利用できる。
+        # レイヤ4には十分なノードがある（N16の場合、9844）。
+        # ここではレイヤーを５に設定、Ｎに併せて増やしていく。
+        # Ｎが増えればレイヤーは枯渇します。
+        # Ｎが１６まではレイヤーは４で足りますが、以降、レイヤーは、５，６と
+        # 増やす必要があり、レイヤーが増えることによって、速度は加速度的に遅
+        # くなります。
+        # ノードレイヤーの考え方はスマートではありますが、Ｎの最大化と高速化を
+        # 求める場合は限界がまもなくおとずれるロジックです。
         nodes:list[int]=[]
+        #
+        # 第３引数の4は4行目までnqueenを実行し、それまでのleft,down,rightをnodes配列に格納するという意味になります。
         k:int=4  # 3番目のレイヤーを対象
         self.kLayer_nodeLayer(size,nodes,k,0,0,0)
-        # 必要なのはノードの半分だけで、各ノードは3つの整数で符号化
-        num_solutions:int=len(nodes)//6
+        # 必要なのはノードの半分だけで、各ノードは3つの整数で符号化される
+        # ３の整数というのは、一つのノードに`left` `down` `right` が格納されるからです。
+        # nodes配列は3個で１セットで`left` `dwon` `right`の情報を同じ配列に格納します
+        num_solutions:int=len(nodes)//3
+        # 以下は、スレッドごとに格納された解をforで回して集計します。
         total:int=0
         for i in range(num_solutions):
           total+=self.bitmap_solve_nodeLayer(size,nodes[3*i],nodes[3*i+1],nodes[3*i+2])
-        total *= 2
+        total *= 2 # 倍にする
         return total
 class NQueens11_NodeLayer:
   def main(self):
