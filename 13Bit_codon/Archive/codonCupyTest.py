2e32dade9cca3addce5b112217d165e21f9e00f3
anikotaro <anikotaro@gmail.com>
2025-10-06 05:58:21 +0000

    add

---
+++ 13Bit_codon/Archive/codonCupyTest.py
@@ -0,0 +1,13 @@
+import gpu
+
+@gpu.kernel
+def hello(a, b, c):
+    i = gpu.thread.x
+    c[i] = a[i] + b[i]
+
+a = [i for i in range(16)]
+b = [2*i for i in range(16)]
+c = [0 for _ in range(16)]
+
+hello(a, b, c, grid=1, block=16)
+print(c)
